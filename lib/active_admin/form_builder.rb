7c168059f77c56ee92bca4279115d488d665b476
Greg Bell <gregdbell@gmail.com>
2010-06-06 09:00:41 -0700

    Buffered form builder now works with all form methods

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -1,11 +1,10 @@
-require 'ostruct'
-
 module ActiveAdmin
   class FormBuilder < ::Formtastic::SemanticFormBuilder
     
     def initialize(*args)
+      @form_buffers = ["".html_safe]
+      @skip_form_buffer = false
       super(*args)
-      @form_buffers = [""]
     end
 
     # Sets up buffering for this form which allows
@@ -16,29 +15,44 @@ module ActiveAdmin
       method_names.each do |method_name|
         module_eval <<-EOF
           def #{method_name}(*args)
-            content = block_given? ? with_new_form_buffer { super } : super
-            @form_buffers.last << content
+            content = block_given? ? with_new_form_buffer('#{method_name}') { super } : super
+            return content if @skip_form_buffer
+            @form_buffers.last << content.html_safe
           end
         EOF
       end
     end
 
     buffer_output_for   :inputs,
-                        :input,
                         :buttons,
-                        :commit_button
+                        :commit_button,
+                        :submit,
+                        :label,
+                        :inputs_for_nested_attributes,
+                        :semantic_fields_for,
+                        :text_field,
+                        :text_area,
+                        :datetime_select,
+                        :time_select,
+                        :date_select,
+                        :radio_button,
+                        :file_field,
+                        :hidden_field
 
-    def semantic_fields_for(record_or_name_or_array, *args, &block)
-      opts = args.extract_options!
-      opts[:builder] ||= ::ActiveAdmin::FormBuilder
-      args.push(opts)
-      @form_buffers.last << super
+    # The input method returns a properly formatted string for
+    # its contents, so we want to skip the internal buffering
+    # while building up its contents
+    def input(*args)
+      @skip_form_buffer = true
+      content = super
+      @skip_form_buffer = false
+      @form_buffers.last << content.html_safe
     end
 
     private
 
-    def with_new_form_buffer
-      @form_buffers << ""
+    def with_new_form_buffer(name)
+      @form_buffers << "".html_safe
       return_value = yield
       @form_buffers.pop
       return_value
--- spec/active_admin/form_builder_spec.rb
+++ spec/active_admin/form_builder_spec.rb
@@ -1,5 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper') 
 include ActiveAdminIntegrationSpecHelper
 
 describe ActiveAdmin::FormBuilder do
@@ -21,7 +20,10 @@ describe ActiveAdmin::FormBuilder do
           f.input :title
           f.input :body
         end
+        f.label :title, "My Super Title"
+        f.text_field :title
         f.buttons do
+          f.submit "My Submit Button"
           f.commit_button "Submit Me"
           f.commit_button "Another Button"
         end
@@ -30,6 +32,9 @@ describe ActiveAdmin::FormBuilder do
     it "should generate a text input" do
       response.should have_tag("input", :attributes => { :type => "text",
                                                      :name => "post[title]" })
+    end
+    it "should generate a label using the default form methods" do
+      response.should have_tag("label", "My Super Title")
     end
     it "should generate a textarea" do
       response.should have_tag("textarea", :attributes => { :name => "post[body]" })
@@ -57,7 +62,7 @@ describe ActiveAdmin::FormBuilder do
     end
   end
 
-  context "with fields for" do
+  context "with semantic fields for" do
     before do
       build_form do |f|
         f.inputs do
@@ -76,4 +81,95 @@ describe ActiveAdmin::FormBuilder do
       response.body.scan("post_author_attributes_first_name_input").size.should == 1
     end
   end
+
+  context "with collection inputs" do
+    before do
+      User.create :first_name => "John", :last_name => "Doe"
+      User.create :first_name => "Jane", :last_name => "Doe"
+    end
+
+    describe "as select" do
+      before do
+        build_form do |f|
+          f.input :author
+        end
+      end
+      it "should create 2 options" do
+        response.body.scan(/\<option/).size.should == 3
+      end
+    end
+
+    describe "as radio buttons" do
+      before do
+        build_form do |f|
+          f.input :author, :as => :radio
+        end
+      end
+      it "should create 2 radio buttons" do
+        response.body.scan(/type=\"radio\"/).size.should == 2
+      end
+    end
+  end
+
+  context "with inputs 'for'" do
+    before do
+      build_form do |f|
+        f.inputs do
+          f.input :title
+          f.input :body
+        end
+        f.instance_eval do
+          @object.author = User.new
+        end
+        f.inputs :name => 'Author', :for => :author do |author|
+          author.inputs :first_name, :last_name
+        end
+      end
+    end
+    it "should generate a nested text input once" do
+      response.body.scan("post_author_attributes_first_name_input").size.should == 1
+    end
+    it "should add an author first name field" do
+      response.body.should have_tag("input", :attributes => { :name => "post[author_attributes][first_name]"})
+    end
+  end
+
+  context "with wrapper html" do
+    it "should set a class" do
+      build_form do |f|
+        f.input :title, :wrapper_html => { :class => "important" }
+      end
+      response.should have_tag("li", :attributes => {:class => "string optional important"})
+    end
+  end
+
+
+  # This checks that each input can be added via the standard
+  # rails method as well as using the form builder's #input method.
+  { 
+    "input :title, :as => :string"        => /id\=\"post_title\"/,
+    "text_field :title"                   => /id\=\"post_title\"/,
+    "input :title, :as => :text"          => /id\=\"post_title\"/,
+    "text_area :title"                    => /id\=\"post_title\"/,
+    "input :created_at, :as => :time"     => /id\=\"post_created_at_2i\"/,
+    "time_select :created_at"             => /id\=\"post_created_at_2i\"/,
+    "input :created_at, :as => :datetime" => /id\=\"post_created_at_2i\"/,
+    "datetime_select :created_at"         => /id\=\"post_created_at_2i\"/,
+    "input :created_at, :as => :date"     => /id\=\"post_created_at_2i\"/,
+    "date_select :created_at"             => /id\=\"post_created_at_2i\"/,
+    "radio_button :title, 'title'"        => /id\=\"post_title_title\"/,
+    "file_field :title"                   => /id\=\"post_title\"/,
+    "hidden_field :title"                 => /id\=\"post_title\"/,
+    "label :title"                        => /for\=\"post_title\"/,
+  }.each do |source, regex|
+   it "should properly buffer #{source}" do
+     build_form do |f|
+       f.instance_eval(source)
+       f.instance_eval(source)
+     end
+     response.body.scan(regex).size.should == 2
+   end
+  end
+      
+
 end
