a808d752a5fc7a9c5dba681f153c6af7bbd06b5f
Greg Bell <gregdbell@gmail.com>
2010-06-07 07:18:17 -0700

    Moved the TableBuilder::Column class into the TableBuilder file

--- lib/active_admin/table_builder.rb
+++ lib/active_admin/table_builder.rb
@@ -1,8 +1,6 @@
 module ActiveAdmin
   class TableBuilder < IndexBuilder
     
-    autoload :Column, 'active_admin/table_builder/column'
-    
     attr_reader :columns
     
     def initialize
@@ -48,6 +46,70 @@ module ActiveAdmin
         links += link_to "Delete", resource_path(resource), :method => :destroy, :confirm => "Are you sure you want to delete this?"
         links
       end
+    end
+
+    class Column
+      
+      attr_accessor :title, :data
+      
+      def initialize(*args, &block)
+        @options = default_options.merge(args.last.is_a?(::Hash) ? args.pop : {})
+        @title = pretty_title args[0]
+        @data  = args[1] || args[0]
+        @data = block if block
+      end
+
+      def sortable?
+        if @data.is_a?(Proc)
+          [String, Symbol].include?(@options[:sortable].class)
+        else
+          @options[:sortable]
+        end
+      end
+      
+      #
+      # Returns the key to be used for sorting this column
+      #
+      # Defaults to the column's method if its a symbol
+      #   column :username
+      #   # => Sort key will be set to 'username'
+      #
+      # You can set the sort key by passing a string or symbol
+      # to the sortable option:
+      #   column :username, :sortable => 'other_column_to_sort_on'
+      #
+      # If you pass a block to be rendered for this column, the column
+      # will not be sortable unless you pass a string to sortable to 
+      # sort the column on:
+      #
+      #   column('Username', :sortable => 'login'){ @user.pretty_name }
+      #   # => Sort key will be 'login'
+      #   
+      def sort_key
+        if @options[:sortable] == true || @options[:sortable] == false
+          @data.to_s
+        else
+          @options[:sortable].to_s
+        end
+      end
+
+      def conditional_block
+        @options[:if]
+      end
+      
+      private
+      
+      def pretty_title(raw)
+        raw.is_a?(Symbol) ? raw.to_s.split('_').collect{|s| s.capitalize }.join(' ') : raw
+      end
+
+      def default_options
+        {
+          :sortable => true,
+          :if => lambda{ true }
+        }
+      end
+      
     end
         
   end
