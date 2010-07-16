8bbb224efca38275cc3082a3298965677a571d52
Greg Bell <gregdbell@gmail.com>
2010-07-15 21:15:09 -0700

    Added some docs to the initializer which explain ActiveAdmin.site_title

--- lib/generators/active_admin/templates/active_admin.rb
+++ lib/generators/active_admin/templates/active_admin.rb
@@ -1,5 +1,15 @@
 ActiveAdmin.setup do |config|
 
+  # == Site Title
+  # Set the title that is displayed on the main layout
+  # for each of the active admin pages.
+  #
+  # If you don't set it here, it defaults to a friendly
+  # name of your Rails.application class name.
+  #
+  # config.site_title = "My Great Site"
+
+  # == Default Namespace
   # Set the default namespace each administration resource
   # will be added to. 
   #
@@ -11,7 +21,6 @@ ActiveAdmin.setup do |config|
   #
   # To set no namespace by default, use:
   #   config.default_namespace = false
-  #
   config.default_namespace = :admin
 
 end
