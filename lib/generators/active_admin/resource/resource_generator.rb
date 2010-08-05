6b2e9dc41ddf4c0ede76e8e8d0a91b39a20f696a
Greg Bell <greg.bell@versapay.com>
2010-08-04 17:13:29 -0700

    Namespaced the generators to be active_admin:install and active_admin:resource.
    Closes [#15] Namespace the generators to activeadmin

---
+++ lib/generators/active_admin/resource/resource_generator.rb
@@ -0,0 +1,16 @@
+module ActiveAdmin
+  module Generators
+    class ResourceGenerator < Rails::Generators::NamedBase
+      desc "Installs ActiveAdmin in a rails 3 application"
+
+      def self.source_root
+        @_active_admin_source_root ||= File.expand_path("../templates", __FILE__)
+      end
+
+      def generate_config_file
+        template "admin.rb", "app/active_admin/#{file_path.gsub('/', '_').pluralize}.rb"
+      end
+
+    end
+  end
+end
