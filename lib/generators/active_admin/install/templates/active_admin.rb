5e9f7a66190d008edd2e18c1b528231b8bc42229
Greg Bell <gregdbell@gmail.com>
2010-08-19 10:33:35 -0700

    Added some documentation to the initializer about setting a before filter for all resources.

--- lib/generators/active_admin/install/templates/active_admin.rb
+++ lib/generators/active_admin/install/templates/active_admin.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.setup do |config|
 
   # == Site Title
+  #
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
@@ -9,7 +10,9 @@ ActiveAdmin.setup do |config|
   #
   # config.site_title = "My Great Site"
 
+
   # == Default Namespace
+  #
   # Set the default namespace each administration resource
   # will be added to. 
   #
@@ -22,5 +25,14 @@ ActiveAdmin.setup do |config|
   # To set no namespace by default, use:
   #   config.default_namespace = false
   config.default_namespace = :admin
+
+
+  # == Controller Filters
+  #
+  # You can add before, after and around filters to all of your
+  # Active Admin resources from here. For example you may wish
+  # to authenticate users before each request.
+  #
+  # config.before_filter :authenticate_user!
 
 end
