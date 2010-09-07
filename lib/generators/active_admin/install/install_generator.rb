2439fa93fdbe0312613125485fd290e0d6b8076e
Greg Bell <gregdbell@gmail.com>
2010-09-07 07:22:54 -0700

    Installation now creates a 'app/admin/dashboards.rb' file for you to create dashboard sections in.

--- lib/generators/active_admin/install/install_generator.rb
+++ lib/generators/active_admin/install/install_generator.rb
@@ -13,6 +13,7 @@ module ActiveAdmin
 
       def setup_directory
         empty_directory "app/admin"
+        template 'dashboards.rb', 'app/admin/dashboards.rb'
       end
 
       def setup_routes
