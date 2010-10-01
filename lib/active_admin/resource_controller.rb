633fe44bfa663fb382f1435291923cfe40acb9ae
Greg Bell <gregdbell@gmail.com>
2010-10-01 07:58:03 -0700

    Got rid of duplicate methods which have already been moved to the sidebar module.

--- lib/active_admin/resource_controller.rb
+++ lib/active_admin/resource_controller.rb
@@ -205,16 +205,6 @@ module ActiveAdmin
     end
     helper_method :current_menu
 
-    def skip_sidebar!
-      @skip_sidebar = true
-    end
-    helper_method :skip_sidebar!
-
-    def skip_sidebar?
-      @skip_sidebar == true
-    end
-    helper_method :skip_sidebar?
-
     # Returns the renderer class to use for the given action.
     #
     # TODO: This needs to be wrapped into a default config as well
