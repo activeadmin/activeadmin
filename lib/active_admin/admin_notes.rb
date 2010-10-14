cf574097d38898e0041867735fea9d0ec13d7bb3
Greg Bell <gregdbell@gmail.com>
2010-10-14 06:37:02 -0700

    Moved all the admin note renders into their own file

--- lib/active_admin/admin_notes.rb
+++ lib/active_admin/admin_notes.rb
@@ -1,10 +1,11 @@
 module ActiveAdmin
   module AdminNotes
 
-    autoload :Note,             'active_admin/admin_notes/note'
-    autoload :NotesController,  'active_admin/admin_notes/notes_controller'
-    autoload :NotesForRenderer, 'active_admin/admin_notes/notes_for_renderer'
-    autoload :NoteRenderer,     'active_admin/admin_notes/note_renderer'
+    autoload :Note,                 'active_admin/admin_notes/note'
+    autoload :NotesController,      'active_admin/admin_notes/notes_controller'
+    autoload :NotesForRenderer,     'active_admin/admin_notes/renderers'
+    autoload :NoteRenderer,         'active_admin/admin_notes/renderers'
+    autoload :FormForRenderer,      'active_admin/admin_notes/renderers'
 
   end
 end
--- lib/active_admin/view_helpers/admin_notes_helper.rb
+++ lib/active_admin/view_helpers/admin_notes_helper.rb
@@ -11,22 +11,7 @@ module ActiveAdmin
       end
       
       def admin_note_form_for(resource)
-        loader = content_tag(:div, :class => "loading_indicator", :style => "display: none") do
-          image_tag("active_admin/loading.gif", :size => "16x16") +
-            content_tag(:span, "Adding note...")
-        end
-          
-        form = active_admin_form_for(ActiveAdmin::AdminNotes::Note.new, :as => :admin_note, :url => admin_admin_notes_path, :html => {:class => "inline_form"}) do |form|
-          form.inputs do
-            form.input :resource_type, :value => resource.class.to_s, :as => :hidden
-            form.input :resource_id, :value => resource.id, :as => :hidden
-            form.input :body, :input_html => {:size => "80x12"}, :label => false
-          end
-          form.buttons do
-            form.commit_button 'Add note'
-          end
-        end
-        loader + form
+        render ActiveAdmin::AdminNotes::FormForRenderer, resource
       end
 
     end
