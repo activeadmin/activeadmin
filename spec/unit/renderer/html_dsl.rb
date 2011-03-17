ec862515be45a2f7ad24049aa05af186a0e0cd67
Greg Bell <gregdbell@gmail.com>
2011-03-17 13:26:40 -0700

    More indentation issues

--- spec/unit/renderer/html_dsl.rb
+++ spec/unit/renderer/html_dsl.rb
@@ -5,41 +5,41 @@ describe ActiveAdmin::Renderer, "HTML DSL" do
   include ActiveAdmin::Renderer::HTML
 
   it "should render a single element" do
-	content = span("Hello World")
-	content.to_html.should == "<span>Hello World</span>"
+    content = span("Hello World")
+    content.to_html.should == "<span>Hello World</span>"
   end
 
   it "should render a child element" do
-	content = span do
-	  span "Hello World"
-	end
-	content.to_html.should == "<span><span>Hello World</span></span>"
+    content = span do
+      span "Hello World"
+    end
+    content.to_html.should == "<span><span>Hello World</span></span>"
   end
 
   it "should render an unordered list" do
-	content = ul do
-	  li "First"
-	  li "Second"
-	  li "Third"
-	end
-	content.to_html.should == "<ul><li>First</li><li>Second</li><li>Third</li></ul>"
+    content = ul do
+      li "First"
+      li "Second"
+      li "Third"
+    end
+    content.to_html.should == "<ul><li>First</li><li>Second</li><li>Third</li></ul>"
   end
 
   it "should return the correct object" do
-	list_1 = ul
-	list_2 = ul
-	list_1.should be_instance_of(ActiveAdmin::Renderer::HTML::Tag)
-	list_2.should be_instance_of(ActiveAdmin::Renderer::HTML::Tag)
+    list_1 = ul
+    list_2 = ul
+    list_1.should be_instance_of(ActiveAdmin::Renderer::HTML::Tag)
+    list_2.should be_instance_of(ActiveAdmin::Renderer::HTML::Tag)
   end
 
   it "should allow local variables inside the tags" do
-	first = "First"
-	second = "Second"
-	content = ul do
-	  li first
-	  li second
-	end
-	content.to_html.should == "<ul><li>First</li><li>Second</li></ul>"
+    first = "First"
+    second = "Second"
+    content = ul do
+      li first
+      li second
+    end
+    content.to_html.should == "<ul><li>First</li><li>Second</li></ul>"
   end
 
   it "should have the parent set on it" do
@@ -71,11 +71,11 @@ describe ActiveAdmin::Renderer, "HTML DSL" do
 
   describe "document" do
     it "should contain the contents" do
-	ul do
-	  li "First"
-	  li "Second"
-	end
-	__current_html_document__.to_html.should == "<ul><li>First</li><li>Second</li></ul>"
+      ul do
+        li "First"
+        li "Second"
+      end
+      __.to_html.should == "<ul><li>First</li><li>Second</li></ul>"
     end
   end
 
