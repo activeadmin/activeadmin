1833559762549533360883575a002f4a609d3677
Fabio Kreusch <fabiokr@gmail.com>
2011-05-29 14:28:07 -0700

    Fix to load all available locale files

--- lib/active_admin.rb
+++ lib/active_admin.rb
@@ -36,7 +36,7 @@ module ActiveAdmin
   class Railtie < ::Rails::Railtie
     # Add load paths straight to I18n, so engines and application can overwrite it.
     require 'active_support/i18n'
-    I18n.load_path << File.expand_path('../active_admin/locales/en.yml', __FILE__)
+    I18n.load_path += Dir[File.expand_path('../active_admin/locales/*.yml', __FILE__)]
   end
 
   module Configuration
