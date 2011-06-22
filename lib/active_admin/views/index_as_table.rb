2627885d1ea5db61926a1b02139e6b27156b87b2
Víctor Martínez <knoopx@gmail.com>
2011-06-22 20:21:09 +0200

    default_actions should exclude actions not defined in the controller. closes #200

--- lib/active_admin/views/index_as_table.rb
+++ lib/active_admin/views/index_as_table.rb
@@ -33,14 +33,14 @@ module ActiveAdmin
 
         # Adds links to View, Edit and Delete
         def default_actions(options = {})
-          options = {
-            :name => ""
-          }.merge(options)
-          column options[:name] do |resource|
-            links = link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link"
-            links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
-            links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
-            links
+          name = options.delete(:name) || ""
+          column(name) do |resource|
+            active_admin_resource = active_admin_config.namespace.resource_for(resource.class)
+            links = []
+            links << link_to(I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link") if active_admin_resource.controller.action_methods.include?("show")
+            links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link") if active_admin_resource.controller.action_methods.include?("edit")
+            links << link_to(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link") if active_admin_resource.controller.action_methods.include?("destroy")
+            links.join.html_safe
           end
         end
 
