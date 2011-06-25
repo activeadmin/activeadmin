8ed83c4206c6cadb67075cff965bf439d600e6a9
Greg Bell <gregdbell@gmail.com>
2011-06-25 09:15:17 -0700

    Refactored Resource naming code into its own module

--- lib/active_admin/resource.rb
+++ lib/active_admin/resource.rb
@@ -1,3 +1,5 @@
+require 'active_admin/resource/naming'
+
 module ActiveAdmin
 
   # Resource is the primary data storage for resource configuration in Active Admin
@@ -15,6 +17,7 @@ module ActiveAdmin
 
     autoload :BelongsTo, 'active_admin/resource/belongs_to'
 
+
     # The namespace this resource belongs to
     attr_reader :namespace
 
@@ -30,9 +33,6 @@ module ActiveAdmin
 
     # An array of collection actions defined for this resource
     attr_reader :collection_actions
-
-    # The titleized name to use for this resource
-    attr_accessor :resource_name
 
     # The default sort order to use in the controller
     attr_accessor :sort_order
@@ -60,29 +60,7 @@ module ActiveAdmin
       @scopes = []
     end
 
-    # An underscored safe representation internally for this resource
-    def underscored_resource_name
-      @underscored_resource_name ||= if @options[:as]
-        @options[:as].gsub(' ', '').underscore.singularize
-      else
-        resource.name.gsub('::','').underscore
-      end
-    end
-
-    # A camelized safe representation for this resource
-    def camelized_resource_name
-      underscored_resource_name.camelize
-    end
-
-    # Returns the name to call this resource.
-    # By default will use resource.model_name.human
-    def resource_name
-      @resource_name ||= if @options[:as] || !resource.respond_to?(:model_name)
-        underscored_resource_name.titleize
-      else
-        resource.model_name.human.titleize
-      end
-    end
+    include Naming
 
     # Returns the plural version of this resource
     def plural_resource_name
