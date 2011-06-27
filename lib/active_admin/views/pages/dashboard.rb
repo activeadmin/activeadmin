ed1b00478807377d8ec35585520336039f45c306
Matt Vague <mattvague@gmail.com>
2011-06-27 15:46:46 -0700

    Default dashboard message is now styled as a blank slate

--- lib/active_admin/views/pages/dashboard.rb
+++ lib/active_admin/views/pages/dashboard.rb
@@ -48,8 +48,11 @@ module ActiveAdmin
         end
 
         def default_welcome_section
-          para :id => "dashboard_default_message" do
-            I18n.t('active_admin.dashboard_welcome')
+          div :class => "blank_slate_container", :id => "dashboard_default_message" do
+            span :class => "blank_slate" do
+              span I18n.t('active_admin.dashboard_welcome.welcome')
+              small I18n.t('active_admin.dashboard_welcome.call_to_action')
+            end
           end
         end
 
