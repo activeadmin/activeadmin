7cd0bcc2e953969541c22c2fe5842995f4fbb80d
Greg Bell <gregdbell@gmail.com>
2011-07-26 15:25:00 -0700

    Fixed Arbre rendering for Rails 3.1.0.rc5 compatibility.

--- lib/active_admin/arbre.rb
+++ lib/active_admin/arbre.rb
@@ -19,5 +19,5 @@ end
 require 'action_view'
 
 ActionView::Template.register_template_handler :arb, lambda { |template|
-  "self.class.send :include, Arbre::HTML; @_helpers = self; @__current_dom_element__ = Arbre::Context.new(assigns, helpers); begin; #{template.source}; end; current_dom_context"
+  "self.class.send :include, Arbre::HTML; @_helpers = self; @__current_dom_element__ = Arbre::Context.new(assigns, self); begin; #{template.source}; end; current_dom_context"
 }
