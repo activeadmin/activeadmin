255f7d1a0aa8c9bede42c2194c8ed6081a53e139
Jesper Hvirring Henriksen <jesper@hvirring.dk>
2011-08-03 16:57:59 +0200

    should call resource_name and not use @resource_name directly
    Bugfix for pull request #337

--- lib/active_admin/resource/naming.rb
+++ lib/active_admin/resource/naming.rb
@@ -29,7 +29,7 @@ module ActiveAdmin
       # Returns the plural version of this resource
       def plural_resource_name
         @plural_resource_name ||= if @options[:as] || !resource.respond_to?(:model_name)
-          @resource_name.pluralize
+          resource_name.pluralize
         else
           resource.model_name.human(:count => 3)
         end
