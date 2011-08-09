b60a4a8a59bc7ae067d3e84a27aa9146d8e1909e
Sam Vincent <sam.vincent@mac.com>
2011-08-08 19:06:52 -0700

    Fixing issue where camelcase classes were rendering spaces in ID attribute of IndexAsTable

--- lib/active_admin/views/index_as_table.rb
+++ lib/active_admin/views/index_as_table.rb
@@ -78,7 +78,7 @@ module ActiveAdmin
 
       def build(page_config, collection)
         table_options = {
-          :id => active_admin_config.plural_resource_name.underscore,
+          :id => active_admin_config.resource.model_name.tableize,
           :sortable => true,
           :class => "index_table",
           :i18n => active_admin_config.resource
