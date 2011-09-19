e98a60158c5aa086472d594312a5a67641701630
Sam Vincent <sam.vincent@mac.com>
2011-09-19 15:26:46 -0700

    Issue #185 - Add support for conditionally display Dashboard sections at run-time
    Provide a method name or Proc object to conditionally render a section at run time
      section "Membership Summary", :if => :memberships_enabled?
      section "Membership Summary", :if => Proc.new { current_admin_user.account.memberships.any? }

--- spec/unit/dashboard_controller_spec.rb
+++ spec/unit/dashboard_controller_spec.rb
@@ -23,4 +23,48 @@ describe ActiveAdmin::Dashboards::DashboardController do
     end
   end
 
+  describe "conditionally displaying sections" do
+    before { ActiveAdmin::Dashboards.clear_all_sections! }
+    let(:controller){ Admin::DashboardController.new }
+    
+    context "when :if not specified" do
+      before do
+        @section = ActiveAdmin::Dashboards.add_section('Stats').last
+      end
+      
+      it "should include section" do
+        controller.send(:find_sections).should include(@section)
+      end
+    end
+    
+    context "when :if option specified as a method" do
+      before do
+        @section = ActiveAdmin::Dashboards.add_section('Secret Codes', :if => :i_am_awesome?).last
+      end
+      
+      it "should call the method of the same name" do
+        controller.should_receive(:i_am_awesome?).and_return(true)
+        controller.send(:find_sections).should include(@section)
+        
+        controller.should_receive(:i_am_awesome?).and_return(false)
+        controller.send(:find_sections).should_not include(@section)
+      end
+    end
+    
+    context "when :if option specified as block" do
+      before do
+        @proc    = Proc.new { true }
+        @section = ActiveAdmin::Dashboards.add_section('Secret Codes', :if => proc {}).last
+      end
+      
+      it "should evaluate the block" do
+        controller.should_receive(:instance_exec).with(&@proc).and_return(true)
+        controller.send(:find_sections).should include(@section)
+        
+        controller.should_receive(:instance_exec).with(&@proc).and_return(false)
+        controller.send(:find_sections).should_not include(@section)
+      end
+    end
+  end
+  
 end
