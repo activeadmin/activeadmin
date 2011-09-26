9d6075f28902fdd778a88d32a8ad6e7c47ad66f0
Edgars Beigarts <edgars.beigarts@makit.lv>
2011-09-26 17:45:17 +0300

    Fixed check boxed filter.

--- lib/active_admin/inputs/filter_check_boxes_input.rb
+++ lib/active_admin/inputs/filter_check_boxes_input.rb
@@ -6,6 +6,35 @@ module ActiveAdmin
       def input_name
         "#{object_name}[#{association_primary_key || method}_in][]"
       end
+
+      def selected_values
+        @object.send("#{association_primary_key || method}_in") || []
+      end
+
+      # Add whitespace before label
+      def choice_label(choice)
+        " #{super(choice)}"
+      end
+
+      # Don't wrap in UL tag
+      def choices_group_wrapping(&block)
+        template.capture(&block)
+      end
+
+      # Don't wrap in LI tag
+      def choice_wrapping(html_options, &block)
+        template.capture(&block)
+      end
+
+      # Don't render hidden fields
+      def hidden_field_for_all
+        ""
+      end
+
+      # Don't render hidden fields
+      def hidden_fields?
+        false
+      end
     end
   end
 end
