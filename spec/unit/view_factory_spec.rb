15b5c6b6139c34ea21538fe9aeb7a53b6b0c1b24
Matt Vague <mattvague@gmail.com>
2011-10-02 17:24:19 -0700

    Built action list popover on top of popover

--- spec/unit/view_factory_spec.rb
+++ spec/unit/view_factory_spec.rb
@@ -8,13 +8,14 @@ end
 
 describe ActiveAdmin::ViewFactory do
 
-  it_should_have_view :global_navigation, ActiveAdmin::Views::TabbedNavigation
-  it_should_have_view :action_items,      ActiveAdmin::Views::ActionItems
-  it_should_have_view :header,            ActiveAdmin::Views::HeaderRenderer
-  it_should_have_view :blank_slate,       ActiveAdmin::Views::BlankSlate
-  it_should_have_view :popover,           ActiveAdmin::Views::Popover
+  it_should_have_view :global_navigation,    ActiveAdmin::Views::TabbedNavigation
+  it_should_have_view :action_items,         ActiveAdmin::Views::ActionItems
+  it_should_have_view :header,               ActiveAdmin::Views::HeaderRenderer
+  it_should_have_view :blank_slate,          ActiveAdmin::Views::BlankSlate
+  it_should_have_view :popover,              ActiveAdmin::Views::Popover
+  it_should_have_view :action_list_popover,  ActiveAdmin::Views::ActionListPopover
 
-  it_should_have_view :dashboard_page,    ActiveAdmin::Views::Pages::Dashboard
-  it_should_have_view :layout,            ActiveAdmin::Views::Pages::Layout
+  it_should_have_view :dashboard_page,       ActiveAdmin::Views::Pages::Dashboard
+  it_should_have_view :layout,               ActiveAdmin::Views::Pages::Layout
 
 end
