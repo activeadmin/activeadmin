82c0997e83ed108b8083324d982dc0f1c403462a
Matt Vague <mattvague@gmail.com>
2011-10-06 09:55:14 -0700

    More table tools JS and style tweaks

--- app/assets/javascripts/active_admin/base.js
+++ app/assets/javascripts/active_admin/base.js
@@ -13,7 +13,7 @@ $(function(){
   // Batch actions stuff
   
   $('#batch_actions_button').toggle(function() {
-    if ($(this).hasClass("selected")) {
+    if (!$(this).hasClass("disabled")) {
       $("#batch_actions_popover").fadeIn(50);
     };
   }, function() {
@@ -23,7 +23,7 @@ $(function(){
   $('.index_table thead :checkbox').click(function() {
     if ($(this).attr('checked') == true) {
       $('#batch_actions_button').removeClass("disabled");
-      $('#batch_actions_button').addClass("selected");
+      //$('#batch_actions_button').addClass("selected");
       $(this).parents(".index_table").find('tr :checkbox').attr('checked', 'true');
       $(this).parents(".index_table").find('tr').addClass("selected");
     } else {
@@ -37,7 +37,7 @@ $(function(){
   $('.index_table tbody :checkbox').click(function() {
     if ($(this).attr('checked') == true) {
       $('#batch_actions_button').removeClass("disabled");
-      $('#batch_actions_button').addClass("selected");
+      //$('#batch_actions_button').addClass("selected");
       $(this).parents('tr').addClass("selected");
     } else {
       if ($(this).parents(".index_table").find("tbody").find("input:checked").length == 0) {
