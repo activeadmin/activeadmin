dc50a274aaa38f249f6f5c4c8490a77b713316a6
Matt Vague <mattvague@gmail.com>
2011-10-05 23:43:49 -0700

    Bunch of style and interaction improvements for batch actions: - Table rows highlight when selected - Better selected state for batch action button - More spacing between titlebar and page content and more...

--- app/assets/javascripts/active_admin/base.js
+++ app/assets/javascripts/active_admin/base.js
@@ -10,33 +10,46 @@ $(function(){
     return false;
   });
 
+  // Batch actions stuff
+  
   $('#batch_actions_button').toggle(function() {
-    $("#batch_actions_popover").fadeIn(50);
+    if ($(this).hasClass("selected")) {
+      $("#batch_actions_popover").fadeIn(50);
+    };
   }, function() {
     $("#batch_actions_popover").fadeOut(100);
   });
   
-  $('.index_table thead :checkbox').toggle(function() {
-    $('#batch_actions_button').removeClass("disabled");
-    $('#batch_actions_button').addClass("selected");
-    $(this).parents(".index_table").find('tr :checkbox').attr('checked', 'true');
-    $(this).attr('checked', 'true');
-  }, function() {
-    $('#batch_actions_button').addClass("disabled");
-    $('#batch_actions_button').removeClass("selected");
-    $(this).parents(".index_table").find('tr :checkbox').attr('checked', '');
-    $(this).attr('checked', 'false');
+  $('.index_table thead :checkbox').click(function() {
+    if ($(this).attr('checked') == true) {
+      $('#batch_actions_button').removeClass("disabled");
+      $('#batch_actions_button').addClass("selected");
+      $(this).parents(".index_table").find('tr :checkbox').attr('checked', 'true');
+      $(this).parents(".index_table").find('tr').addClass("selected");
+    } else {
+      $('#batch_actions_button').addClass("disabled");
+      $('#batch_actions_button').removeClass("selected");
+      $(this).parents(".index_table").find('tr :checkbox').attr('checked', '');
+      $(this).parents(".index_table").find('tr').removeClass("selected");
+    }
   });
   
-  $('.index_table tbody :checkbox').toggle(function() {
-    $('#batch_actions_button').removeClass("disabled");
-    $('#batch_actions_button').addClass("selected");
-    return true;
-  }, function() {
-    $('#batch_actions_button').addClass("disabled");
-    $('#batch_actions_button').removeClass("selected");
-    return true;
-    
+  $('.index_table tbody :checkbox').click(function() {
+    if ($(this).attr('checked') == true) {
+      $('#batch_actions_button').removeClass("disabled");
+      $('#batch_actions_button').addClass("selected");
+      $(this).parents('tr').addClass("selected");
+    } else {
+      if ($(this).parents(".index_table").find("tbody").find("input:checked").length == 0) {
+        $('#batch_actions_button').addClass("disabled");
+        $('#batch_actions_button').removeClass("selected");
+      };
+      
+      $(this).parents('tr').removeClass("selected");
+      
+      $(this).parents(".index_table").find('thead th :checkbox').attr('checked', '');
+      
+    }
   });
 
 });
