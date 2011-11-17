d21f2b2d24ed958b2ac12ee2fccf5c3693e2af7a
Philippe Creux <pcreux@gmail.com>
2011-11-17 14:47:12 +0100

    Add abstract methods and spec Config::Naming.

--- lib/active_admin/config/naming.rb
+++ lib/active_admin/config/naming.rb
@@ -1,6 +1,18 @@
 module ActiveAdmin
   class Config
     module Naming
+      def resource_name
+        raise "Subclasses of Config should implement resource_name"
+      end
+
+      def plural_resource_name
+        raise "Subclasses of Config should implement plural_resource_name"
+      end
+
+      def underscored_resource_name
+        raise "Subclasses of Config should implement underscored_resource_name"
+      end
+
       # A camelized safe representation for this resource
       def camelized_resource_name
         underscored_resource_name.camelize
--- spec/unit/config_shared_examples.rb
+++ spec/unit/config_shared_examples.rb
@@ -17,8 +17,34 @@ shared_examples_for "ActiveAdmin::Config" do
   it { respond_to :route_collection_path }
   it { respond_to :comments? }
   it { respond_to :belongs_to? }
-  it { respond_to :comments? }
   it { respond_to :action_items? }
   it { respond_to :sidebar_sections? }
 
+  describe "Naming" do
+    it "implements #resource_name" do
+      expect { config.resource_name }.should_not raise_error
+    end
+
+    it "implements #plural_resource_name" do
+      expect { config.plural_resource_name }.should_not raise_error
+    end
+
+    it "implements #underscored_resource_name" do
+      expect { config.underscored_resource_name }.should_not raise_error
+    end
+
+    describe "#camelized_resource_name" do
+      it "returns the underscored_resource_name camelized" do
+        config.should_receive(:underscored_resource_name).and_return "my_resource"
+        config.camelized_resource_name.should == "MyResource"
+      end
+    end
+
+    describe "#plural_underscored_resource_name" do
+      it "returns the plural_resource_name underscored" do
+        config.should_receive(:plural_resource_name).and_return "My Resources"
+        config.plural_underscored_resource_name.should == "my_resources"
+      end
+    end
+  end
 end
