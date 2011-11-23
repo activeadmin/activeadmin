e940baf1adaaee4621c1fe39830926215679f8fe
Philippe Creux <pcreux@gmail.com>
2011-11-23 14:25:17 +0100

    Resource names are now titleized.

--- lib/active_admin/resource/naming.rb
+++ lib/active_admin/resource/naming.rb
@@ -1,14 +1,14 @@
 module ActiveAdmin
   class Resource < Config
     module Naming
-      # Returns the name to call this resource.
+      # Returns the name to call this resource such as "Bank Account"
       def resource_name
         @resource_name ||= @options[:as]
         @resource_name ||= singular_human_name
         @resource_name ||= resource.name.gsub('::',' ')
       end
 
-      # Returns the plural version of this resource
+      # Returns the plural version of this resource such as "Bank Accounts"
       def plural_resource_name
         @plural_resource_name ||= @options[:as].pluralize if @options[:as]
         @plural_resource_name ||= plural_human_name
@@ -17,18 +17,18 @@ module ActiveAdmin
 
       private
 
-      # @return [String] Human name via ActiveRecord I18n or nil
+      # @return [String] Titleized human name via ActiveRecord I18n or nil
       def singular_human_name
         return nil unless resource.respond_to?(:model_name)
-        resource.model_name.human
+        resource.model_name.human.titleize
       end
 
-      # @return [String] Plural human name via ActiveRecord I18n or nil
+      # @return [String] Titleized plural human name via ActiveRecord I18n or nil
       def plural_human_name
         return nil unless resource.respond_to?(:model_name)
+
         begin
-          I18n.translate!("activerecord.models.#{resource.model_name.i18n_key}")
-          resource.model_name.human(:count => 3)
+          I18n.translate!("activerecord.models.#{resource.model_name.underscore}.other").titleize
         rescue I18n::MissingTranslationData
           nil
         end
