9f32a1a6e0b975ce96ed7e40068a91a86292f664
Juan Schwindt <juan@schwindt.org>
2011-12-07 13:03:29 -0300

    Add assets to precompile list

--- lib/active_admin/engine.rb
+++ lib/active_admin/engine.rb
@@ -1,4 +1,7 @@
 module ActiveAdmin
   class Engine < Rails::Engine
+    initializer "ActiveAdmin precompile hook" do |app|
+      app.config.assets.precompile += ['active_admin.js', 'active_admin.css']
+    end
   end
 end
