4791541ea626d97381cc73de102466b4ed2f26cb
Jarrod Carlson <jarrod.carlson@gmail.com>
2011-12-10 17:52:26 -0500

    Allow options hash for routes. This allows multiple routes to share the same path with different verbs.
    For example, you can now have a 'GET /posts/1/publish' and a 'PUT /posts/1/publish' by using syntax like the following in your resource registration, e.g., 'app/admin/posts.rb':
    
      member_action :publish!, :as => :publish, :method => :put do
        resource.publish!
        redirect_to [:admin, resource], :notice => "Post was published"
      end
    
      member_action :publish, :method => :get do
        @post = resource
      end

--- lib/active_admin/controller_action.rb
+++ lib/active_admin/controller_action.rb
@@ -1,12 +1,20 @@
 module ActiveAdmin
   class ControllerAction
-    attr_reader :name
+    
+    attr_reader :options
+    
     def initialize(name, options = {})
+      options[:action] = name
       @name, @options = name, options
     end
 
     def http_verb
       @options[:method] ||= :get
     end
+    
+    def name
+      @options[:as] || @name
+    end
+    
   end
 end
