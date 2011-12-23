008f592b7fbc12ae5368b6e9aa6f1ac9af50977c
Peter Fry <peter@ourstage.com>
2011-12-23 10:34:34 -0500

    Add :if clause to sidebars and action_items

--- features/step_definitions/action_item_steps.rb
+++ features/step_definitions/action_item_steps.rb
@@ -1,3 +1,7 @@
 Then /^I should see an action item link to "([^"]*)"$/ do |link|
   page.should have_css('.action_item a', :text => link)
 end
+
+Then /^I should not see an action item link to "([^"]*)"$/ do |link|
+  page.should_not have_css('.action_item a', :text => link)
+end
