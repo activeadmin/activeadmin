8ae5a7e4bce724b2c457bb4afd2f2578d5b87e64
Peter Fry <peter@ourstage.com>
2011-12-23 10:34:34 -0500

    Add :if clause to sidebars and action_items

--- lib/active_admin/resource/action_items.rb
+++ lib/active_admin/resource/action_items.rb
@@ -33,8 +33,8 @@ module ActiveAdmin
       # @param [String, Symbol] action the action to retrieve action items for
       #
       # @return [Array] Array of ActionItems for the controller actions
-      def action_items_for(action)
-        action_items.select{|item| item.display_on?(action) }
+      def action_items_for(action, render_context = nil)
+        action_items.select{|item| item.display_on?(action, render_context) }
       end
 
       # Clears all the existing action items for this resource
