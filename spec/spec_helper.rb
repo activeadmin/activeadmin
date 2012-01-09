67558e19f8ac8afe3f02abff5b9629f311c484f3
Greg Bell <gregdbell@gmail.com>
2012-01-09 06:47:06 -0800

    Added a spec_helper_without_rails for isolated specs. Mucho faster.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,4 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH << File.expand_path('../support', __FILE__)
-
-ENV['BUNDLE_GEMFILE'] = File.expand_path('../../Gemfile', __FILE__)
-
-require 'detect_rails_version'
-ENV['RAILS'] = detect_rails_version
-
-require "bundler"
-Bundler.setup
+require File.expand_path("../spec_helper_without_rails", __FILE__)
 
 require 'shoulda/active_record'
 include Shoulda::ActiveRecord::Macros
