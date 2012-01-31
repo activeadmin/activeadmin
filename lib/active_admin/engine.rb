245244c006d290eef221b8933b215b08d59e132c
Juan Schwindt <juan@schwindt.org>
2012-01-31 16:03:47 -0300

    Validates that Rails version supports assets pipeline

--- lib/active_admin/engine.rb
+++ lib/active_admin/engine.rb
@@ -1,7 +1,9 @@
 module ActiveAdmin
   class Engine < Rails::Engine
-    initializer "ActiveAdmin precompile hook" do |app|
-      app.config.assets.precompile += ['active_admin.js', 'active_admin.css']
+    if Rails.version > "3.1"
+      initializer "ActiveAdmin precompile hook" do |app|
+        app.config.assets.precompile += ['active_admin.js', 'active_admin.css']
+      end
     end
   end
 end
