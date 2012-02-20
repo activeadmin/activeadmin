a2986e8454c54da65db056d31a57c2575391412e
Jeff Ancel <jancel@gmail.com>
2012-02-19 18:49:05 -0600

    add compatibility for comments with model ids that are strings issue 34

--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -6,3 +6,8 @@ When /^I add a comment "([^"]*)"$/ do |comment|
   step %{I fill in "active_admin_comment_body" with "#{comment}"}
   step  %{I press "Add Comment"}
 end
+
+
+Given /^a tag with the name "([^"]*)" exists$/ do |tag_name|
+  Tag.create(:name => tag_name)
+end
