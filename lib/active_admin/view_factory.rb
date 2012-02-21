96be7444bd63ad0a952e9e6629df8e908fea79b0
Greg Bell <gregdbell@gmail.com>
2012-02-21 07:56:04 -0800

    Extracted a new component from Base page for TitleBar

--- lib/active_admin/view_factory.rb
+++ lib/active_admin/view_factory.rb
@@ -8,6 +8,7 @@ module ActiveAdmin
               :site_title         => ActiveAdmin::Views::SiteTitle,
               :utility_navigation => ActiveAdmin::Views::UtilityNav,
               :action_items       => ActiveAdmin::Views::ActionItems,
+              :title_bar          => ActiveAdmin::Views::TitleBar,
               :header             => ActiveAdmin::Views::Header,
               :dashboard_section  => ActiveAdmin::Views::DashboardSection,
               :index_scopes       => ActiveAdmin::Views::Scopes,
