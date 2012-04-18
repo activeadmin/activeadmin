9253dfbcf84f1e1818acdc2bdf776ce810fd90e9
George Anderson <george@benevolentcode.com>
2012-04-17 23:22:22 -0400

    #items_in_collection? returns false if collection.empty?
    See: https://github.com/gregbell/active_admin/issues/1245

--- lib/active_admin/views/pages/index.rb
+++ lib/active_admin/views/pages/index.rb
@@ -40,7 +40,7 @@ module ActiveAdmin
           #
           # If we don't reorder, there may be some columns referenced in the order
           # clause that requires the original select.
-          collection.reorder("").limit(1).exists?
+          !collection.empty? && collection.reorder("").limit(1).exists?
         end
 
         def build_collection
