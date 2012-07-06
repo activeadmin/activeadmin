02854fee7b1a06d441f362ec6deb26f867173592
James McKinney <james@slashpoundbang.com>
2012-07-06 11:57:30 -0400

    instead of overriding Formtastic's #input_class, override its #input_class_with_const_defined and #input_class_by_trying

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -125,23 +125,35 @@ module ActiveAdmin
       "ActiveAdmin::Inputs::#{as.to_s.camelize}Input"
     end
 
-    def input_class(as)
-      @input_classes_cache ||= {}
-      @input_classes_cache[as] ||= begin
-        begin
-          begin
-            custom_input_class_name(as).constantize
-          rescue NameError
-            begin
-              active_admin_input_class_name(as).constantize
-            rescue NameError
-              standard_input_class_name(as).constantize
-            end
-          end
-        rescue NameError
-          raise Formtastic::UnknownInputError
-        end
+    # prevent exceptions in production environment for better performance
+    def input_class_with_const_defined(as)
+      input_class_name = custom_input_class_name(as)
+
+      if ::Object.const_defined?(input_class_name)
+        input_class_name.constantize
+      elsif ActiveAdmin::Inputs.const_defined?(input_class_name)
+        active_admin_input_class_name(as).constantize 
+      elsif Formtastic::Inputs.const_defined?(input_class_name)
+        standard_input_class_name(as).constantize 
+      else
+        raise Formtastic::UnknownInputError
       end
+    end
+
+    # use auto-loading in development environment
+    def input_class_by_trying(as)
+      begin
+        begin
+          custom_input_class_name(as).constantize
+        rescue NameError
+          begin
+            active_admin_input_class_name(as).constantize
+          rescue NameError
+            standard_input_class_name(as).constantize
+          end
+        end
+    rescue NameError
+      raise Formtastic::UnknownInputError
     end
 
     private
