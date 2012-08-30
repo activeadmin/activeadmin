5447ee13217c86af58cc414c4d3770cb9ba16ad4
rheaton <rachelmheaton@gmail.com>
2012-08-30 16:07:52 -0400

    Active Admin developer can pass options to the CSV generation\ For example, in active admin config: `config.csv_options = { :force_quotes => true }` or in the active admin resource definition: `csv :options => { :force_quotes => true} do . . .`

--- lib/active_admin/application.rb
+++ lib/active_admin/application.rb
@@ -69,6 +69,9 @@ module ActiveAdmin
     # Default CSV separator
     inheritable_setting :csv_column_separator, ','
 
+    # Default CSV options
+    inheritable_setting :csv_options, {}
+
     # Active Admin makes educated guesses when displaying objects, this is
     # the list of methods it tries calling in order
     setting :display_name_methods, [ :display_name,
