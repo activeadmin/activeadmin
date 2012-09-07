9ac88afa45a999895d2572d2abed766be03a1dfe
RainChen <hirainchen@gmail.com>
2012-09-07 15:31:08 +0800

    enable directly accessing assigns in view

--- lib/active_admin/view_helpers/assigns_with_indifferent_access_helper.rb
+++ lib/active_admin/view_helpers/assigns_with_indifferent_access_helper.rb
@@ -4,4 +4,13 @@ module AssignsWithIndifferentAccessHelper
     @assigns_with_indifferent_access_helper ||= HashWithIndifferentAccess.new(super)
   end
 
+  # directly accessing assigns in view
+  def method_missing(name, *args, &block)
+    if assigns && assigns.has_key?(name)
+      assigns[name]
+    else
+      super
+    end
+  end
+
 end
