6637cb00034572577debe1388dbfb893c1984b2b
rheaton <rachelmheaton@gmail.com>
2012-09-26 12:44:29 -0400

    deprecate csv_column_separator in favor of using csv_options

--- spec/unit/application_spec.rb
+++ spec/unit/application_spec.rb
@@ -30,7 +30,7 @@ describe ActiveAdmin::Application do
     application.site_title = "New Title"
     application.site_title.should == "New Title"
   end
-  
+
   it "should store the site's title link" do
     application.site_title_link.should == ""
   end
@@ -39,11 +39,11 @@ describe ActiveAdmin::Application do
     application.site_title_link = "http://www.mygreatsite.com"
     application.site_title_link.should == "http://www.mygreatsite.com"
   end
-  
+
   it "should store the site's title image" do
     application.site_title_image.should == ""
   end
-  
+
   it "should set the site's title image" do
     application.site_title_image = "http://railscasts.com/assets/episodes/stills/284-active-admin.png?1316476106"
     application.site_title_image.should == "http://railscasts.com/assets/episodes/stills/284-active-admin.png?1316476106"
@@ -53,7 +53,7 @@ describe ActiveAdmin::Application do
     application.view_factory.should be_an_instance_of(ActiveAdmin::ViewFactory)
   end
 
-  it "should have deprecated admin notes by default" do 
+  it "should have deprecated admin notes by default" do
     application.admin_notes.should be_nil
   end
 
@@ -77,6 +77,19 @@ describe ActiveAdmin::Application do
 
     it "should have a logout link method (Devise's default)" do
       application.logout_link_method.should == :get
+    end
+  end
+
+  describe "inheritable settings" do
+    it "should set csv_options" do
+      application.csv_options.should == {}
+    end
+
+    context "when deprecated" do
+      it "should set and warn csv_column_separator" do
+        ActiveAdmin::Deprecation.should_receive(:warn)
+        application.csv_column_separator.should == ','
+      end
     end
   end
 
