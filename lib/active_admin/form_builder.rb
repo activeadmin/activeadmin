62fa53db59ba94e52d47dac0eac8b20321619657
Pablo Torres <tn.pablo@gmail.com>
2012-11-16 23:19:21 -0500

    Pull out method from has_many for readability

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -89,6 +89,7 @@ module ActiveAdmin
           js = template.escape_javascript(js)
           js = template.link_to I18n.t('active_admin.has_many_new', :model => object.class.reflect_on_association(association).klass.model_name.human), "#", :onclick => "$(this).siblings('li.input').append('#{js}'.replace(/#{placeholder}/g, new Date().getTime())); return false;", :class => "button"
 
+          js = js_for_has_many(association, form_block, template)
           form_buffers.last << js.html_safe
         end
       end
@@ -179,6 +180,29 @@ module ActiveAdmin
       return_value = yield
       form_buffers.pop
       return_value
+    end
+
+    # Capture the ADD JS
+    def js_for_has_many(association, form_block, template)
+      association_reflection = object.class.reflect_on_association(association)
+      association_human_name = association_reflection.klass.model_name.human
+      placeholder = "NEW_#{association_human_name.upcase}_RECORD"
+
+      js = with_new_form_buffer do
+        inputs_for_nested_attributes :for => [association, association_reflection.klass.new],
+                                     :class => "inputs has_many_fields",
+                                     :for_options => { :child_index => placeholder },
+                                     &form_block
+      end
+
+      js = template.escape_javascript(js)
+
+      text = I18n.t 'active_admin.has_many_new', :model => association_human_name
+      onclick = "$(this).before('#{js}'.replace(/#{placeholder}/g, new Date().getTime())); return false;"
+
+      template.link_to text, "#",
+                       :onclick => onclick,
+                       :class => "button"
     end
 
   end
