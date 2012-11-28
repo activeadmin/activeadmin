e1a1519d66fdc612d1a4ac13042f432100ec2f8f
Pablo Torres <tn.pablo@gmail.com>
2012-11-28 10:37:16 -0500

    Handle human names with spaces for JS replacement
    When generating the html that form builder's #has_many replaces via JS,
    the placeholder that it looks for cannot contain spaces, otherwise, JS's
    replace method won't replace all that it was supposed to.
    
    This was originally implemented by @danielwestendorf in PR #1789

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -173,7 +173,7 @@ module ActiveAdmin
     def js_for_has_many(association, form_block, template)
       association_reflection = object.class.reflect_on_association(association)
       association_human_name = association_reflection.klass.model_name.human
-      placeholder = "NEW_#{association_human_name.upcase}_RECORD"
+      placeholder = "NEW_#{association_human_name.upcase.split(' ').join('_')}_RECORD"
 
       js = with_new_form_buffer do
         inputs_for_nested_attributes :for => [association, association_reflection.klass.new],
