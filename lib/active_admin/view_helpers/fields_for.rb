39ed505a86f925bb20336f072116ad909dcc38f6
Krishna Srihari <krishna.srihari@gmail.com>
2012-11-29 10:06:51 +0530

    separate case for nil

--- lib/active_admin/view_helpers/fields_for.rb
+++ lib/active_admin/view_helpers/fields_for.rb
@@ -26,8 +26,8 @@ module ActiveAdmin
           end
 
           case v
-          when String, nil
-            { k => v.nil? ? '' : v }
+          when String
+            { k => v }
           when Symbol
             { k => v.to_s }
           when Hash
@@ -36,6 +36,8 @@ module ActiveAdmin
             v.map do |v|
               { "#{k}[]" => v }
             end
+          when nil
+          	{ k => '' }
           else
             raise "I don't know what to do with #{v.class} params: #{v.inspect}"
           end
