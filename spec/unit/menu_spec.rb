activeadmin/activeadmin

commit 452956eccf77ea368b05902a3d0d8020152a7aaa

UtlityNavigation removed, uses menu system
* MenuItem#label now accepts a proc evaluated in view context
* MenuItem#dom_id of '#' is now dispalyed instead of hidden
* DSL#display_menu now DSL#navigation_menu
* Docs updated on how to override utility nav

lib/active_admin/menu_item.rb
@@ -3,7 +3,7 @@ module ActiveAdmin
   class MenuItem
     include Menu::MenuNode
 
-    attr_accessor :id, :label, :url, :priority, :display_if_block
+    attr_accessor :id, :label, :url, :priority, :display_if_block, :html_options
     attr_reader :parent
 
     # Build a new menu item
@@ -31,27 +31,22 @@ module ActiveAdmin
     #         A block for the view to call to decide if this menu item should be displayed.
     #         The block should return true of false
     #
+    # @option options [Hash] :html_options
+    #         A hash of options to pass to `link_to` when rendering the item
+    #
     # @param [ActiveAdmin::MenuItem] parent The parent menu item of this item
     #
     def initialize(options = {}, parent = nil)
-      @label    = options[:label]
-      @id       = normalize_id(options[:id] || label)
-      @url      = options[:url] || "#"
-      @priority = options[:priority] || 10
-      @parent   = parent
+      @label        = options[:label]
+      @id           = normalize_id(options[:id] || label)
+      @url          = options[:url] || "#"
+      @priority     = options[:priority] || 10
+      @html_options = options[:html_options] || {}
+      @parent       = parent
 
       @display_if_block = options[:if]
 
       yield(self) if block_given? # Builder style syntax
-    end
-
-    def label
-      case @label
-      when Proc
-        @label.call
-      else
-        @label.to_s
-      end
     end
 
     def parent?
@@ -59,7 +54,12 @@ module ActiveAdmin
     end
 
     def dom_id
-      id.gsub( " ", '_' ).gsub( /[^a-z0-9_]/, '' )
+      case id
+      when Proc
+        id
+      else
+        id.to_s.gsub( " ", '_' ).gsub( /[^a-z0-9_]/, '' )
+      end
     end
 
     # Returns an array of the ancestory of this menu item
@@ -71,7 +71,7 @@ module ActiveAdmin
 
     def <=>(other)
       result = priority <=> other.priority
-      result = label <=> other.label if result == 0
+      result = label.to_s <=> other.label.to_s if result == 0
       result
     end
 

app/assets/stylesheets/active_admin/_header.css.scss
@@ -42,7 +42,7 @@
     height: 20px
   }
 
-  ul#tabs {
+  ul.tabs {
     display: inline-block; 
     height: 100%;
     margin: 0; 
@@ -128,10 +128,13 @@
     display: inline-block;
     margin: 0;
     padding: 0;
-    span, a { margin-left: 10px; }
 
     a { text-decoration: none; }
     a:hover { color: #fff; }
+
+    li {
+      display:inline;
+    }
   }
 
 }

app/assets/stylesheets/active_admin/print.css.scss
@@ -40,7 +40,7 @@ h3 {
 #header {
   float: left;
 
-  #tabs, #utility_nav {
+  #tabs, .tabs, #utility_nav {
     display: none;
   }
 

features/belongs_to.feature
@@ -55,7 +55,7 @@ Feature: Belongs To
       ActiveAdmin.register User
       ActiveAdmin.register Post do
         belongs_to :user
-        display_menu :user
+        navigation_menu :user
       end
     """
     When I go to the last author's posts

lib/active_admin/base_controller/menu.rb
@@ -11,7 +11,7 @@ module ActiveAdmin
       protected
 
       def current_menu
-        active_admin_config.display_menu
+        active_admin_config.navigation_menu
       end
 
       # Set's @current_tab to be name of the tab to mark as current

lib/active_admin/dsl.rb
@@ -121,7 +121,7 @@ module ActiveAdmin
       config.menu_item_options = options
     end
 
-    # Set the name of the menu to display. This is mainly used in conjuction with the
+    # Set the name of the navigation menu to display. This is mainly used in conjuction with the
     # `#belongs_to` functionality.
     #
     # @param [Symbol] menu_name The name of the menu to display as the global navigation
@@ -130,8 +130,8 @@ module ActiveAdmin
     # Pass a block returning the name of a menu you want rendered for the request, being
     # executed in the context of the controller
     #
-    def display_menu(menu_name=nil, &block)
-      config.display_menu_name = menu_name || block
+    def navigation_menu(menu_name=nil, &block)
+      config.navigation_menu_name = menu_name || block
     end
 
     # Rewrite breadcrumb links.

lib/active_admin/menu.rb
@@ -76,7 +76,12 @@ module ActiveAdmin
       end
 
       def normalize_id(string)
-        string.to_s.downcase.gsub(" ", "_")
+        case string
+        when Proc
+          string
+        else
+          string.to_s.downcase.gsub(" ", "_")
+        end
       end
 
     end

lib/active_admin/menu_collection.rb
@@ -26,6 +26,10 @@ module ActiveAdmin
       @built = false
     end
 
+    def exists?(menu_name)
+      @menus.keys.include? menu_name
+    end
+
     def fetch(menu_name)
       build_menus!
 

commit 77054662c4aa02735af61a5c014562a89ae800cb

Fix sorting of menu items whose labels are procs
Commit 452956eccf77ea368b05902a3d0d8020152a7aaa introduced
a bug in `MenuItem#<=>`: if the label was a proc, it would
not compare `label.call <=> other.label.call`, but instead
`label.to_s <=> other.label.to_s`, and `Proc#to_s` returns
some irrelevant metadata about the Proc object.

lib/active_admin/menu_item.rb
@@ -69,9 +69,10 @@ module ActiveAdmin
       [parent, parent.ancestors].flatten
     end
 
+    # Sort menu items first by their priority attribute, then by their labels.
     def <=>(other)
       result = priority <=> other.priority
-      result = label.to_s <=> other.label.to_s if result == 0
+      result = label_value <=> other.label_value if result == 0
       result
     end
 
@@ -79,6 +80,13 @@ module ActiveAdmin
     # a default block always returning true will be returned.
     def display_if_block
       @display_if_block || lambda { |_| true }
+    end
+
+    def label_value
+      case label
+      when String then label
+      when Proc then label.call.to_s
+      end
     end
 
   end

spec/unit/menu_spec.rb
@@ -52,5 +52,19 @@ describe ActiveAdmin::Menu do
     end
   end
 
+  describe "sorting items" do
+    def build_label_proc(label)
+      proc {|label| label }
+    end
+
+    it "should sort children by the result of their label proc" do
+      menu = Menu.new
+      blog_post_menu = menu.add :label => build_label_proc("Blog posts")
+      user_menu = menu.add :label => build_label_proc("Users")
+      projects_menu = menu.add :label => build_label_proc("Projects")
+
+      menu.items.should == [blog_post_menu, user_menu, projects_menu]
+    end
+  end
 end
 