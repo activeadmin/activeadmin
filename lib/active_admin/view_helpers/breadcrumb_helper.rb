9339da6cfe9895f7a9cc2a922f4056f1296856f9
Daxter <xovatdev@gmail.com>
2013-04-22 14:07:58 -0500

    fix breadcrumb bug so non-belongs_to resources get `display_name` called
    before this fix, non-nested (non-belongs_to) resources in the URL wouldn't
    get massaged by `display_name` so only an ID would be shown.

--- lib/active_admin/view_helpers/breadcrumb_helper.rb
+++ lib/active_admin/view_helpers/breadcrumb_helper.rb
@@ -11,9 +11,9 @@ module ActiveAdmin
           # If an object (users/23), look it up via ActiveRecord and capture its name.
           # If name is nil, look up the model translation, using `titlecase` as the backup.
           if part =~ /^\d|^[a-f0-9]{24}$/ && parent = parts[index-1]
-            config = active_admin_config.belongs_to_config
-            obj    = config.target.resource_class.find_by_id(part) if config
-            name   = display_name(obj)                             if obj
+            config = active_admin_config.belongs_to_config.try(:target) || active_admin_config
+            obj    = config.resource_class.find_by_id(part) if config
+            name   = display_name(obj)                      if obj
           end
           name ||= I18n.t "activerecord.models.#{part.singularize}", :count => 1.1, :default => part.titlecase
 
