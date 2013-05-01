851fbce4447f9340db8b6986cdf6a05d537c17b0
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2013-05-01 10:03:03 +0200

    ADD: new method translate_per_model

---
+++ lib/active_admin/base_controller/translation.rb
@@ -0,0 +1,23 @@
+module ActiveAdmin
+  class BaseController < ::InheritedResources::Base
+    module Translation
+      extend ActiveSupport::Concern
+
+      included do
+        helper_method :translation_per_model
+      end
+
+      protected
+
+      def translation_per_model(resource_class, action, model_label)
+        puts "key: active_admin.#{resource_class.to_s.downcase}.#{action}"
+        t_per_model = I18n.t("active_admin.#{resource_class.to_s.downcase}.#{action}", :model => model_label, :default => "")
+        unless t_per_model.blank?
+          return t_per_model
+        else
+          return I18n.t("active_admin.#{action}", :model => model_label)
+        end            
+      end
+    end
+  end
+end
