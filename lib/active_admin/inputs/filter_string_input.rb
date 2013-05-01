b3c8a1713fa0cfada67a315c61d8b6821e88a31b
JoseLuis Torres <joseluis@powhow.com>
2013-05-01 16:38:25 -0500

    Feature dropdown for string filter to use contains, starts_with and ends_with #2002

--- lib/active_admin/inputs/filter_string_input.rb
+++ lib/active_admin/inputs/filter_string_input.rb
@@ -2,25 +2,37 @@ module ActiveAdmin
   module Inputs
     class FilterStringInput < ::Formtastic::Inputs::StringInput
       include FilterBase
+      include FilterBase::SearchMethodSelect
 
       def to_html
-        input_wrapping do
-          label_html <<
-          builder.text_field(input_name, input_html_options)
+        if search_conditions =~ method.to_s
+          input_wrapping do
+            label_html <<
+            builder.text_field(input_name_simple, input_html_options)
+          end
+        else
+          super
         end
+      end
+
+      def input_name_simple
+        method.to_s.match(search_conditions) ? method : "#{method}_contains"
+      end
+
+      def search_conditions
+        /_(contains|starts_with|ends_with)\z/
       end
 
       def label_text
         I18n.t('active_admin.search_field', :field => super)
       end
 
-      def input_name
-        method.to_s.match(metasearch_conditions) ? method : "#{method}_contains"
+      def default_filters
+        [ [I18n.t('active_admin.contains'),    'contains'],
+          [I18n.t('active_admin.starts_with'), 'starts_with'],
+          [I18n.t('active_admin.ends_with'),   'ends_with'] ]
       end
 
-      def metasearch_conditions
-        /starts_with|ends_with/
-      end
     end
   end
 end
