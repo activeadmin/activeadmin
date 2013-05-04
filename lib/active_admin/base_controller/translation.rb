dbe87763b2544fe77fa56f767447d7c6a35ed7ce
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2013-05-05 01:27:41 +0200

    Squashed:
    FIX: Fix for #218
    
    REFACTOR: proc instead of if statement as suggested by @Daxter
    
    FIX: syntax error
    
    CHG: unified getting a name of resource class
    
    CHG: There's no need to invoke ActiveSupport::Concern here. This version works just as well, with considerably less magic - help from Daxter
    
    REFACTOR: Instead of passing "new_model" to translation_per_model, you should simply pass :new as an action, and then internally do this:
    
    "#{action}_model"
    
    I18n.t "active_admin.#{resource_class.to_s.downcase}.#{action}",
      :default => proc{ I18n.t "active_admin.#{action}_model", :model => model_label }
    Note that this changes the behaviour quite a bit. That's because you shouldn't need to pass :model to the first translation, only to the default.
    
    (suggestion from @Daxter)
    
    ADD: form builder spec test

---
+++ lib/active_admin/base_controller/translation.rb
@@ -0,0 +1,18 @@
+module ActiveAdmin
+  class BaseController < ::InheritedResources::Base
+    module Translation
+
+      def self.included(c)
+        c.helper_method :translation_per_model
+      end
+
+      protected
+
+      def translation_per_model(resource_class, action, model_label)
+        I18n.t "active_admin.#{resource_class.to_s.downcase}.#{action.to_s}_model", 
+          model: model_label, 
+          default: proc { I18n.t "active_admin.#{action.to_s}_model", :model => model_label }
+      end
+    end
+  end
+end
