71f7ca99229508eb651e0068355777f464f12df9
David Francisco <dmfrancisc@gmail.com>
2013-05-16 13:14:20 +0000

    Revert "remove needless Formtastic overrides"
    This reverts commit 47fe4c5f483399c0efda0d3cf275d5d12b0283d3.

--- lib/active_admin/inputs/filter_boolean_input.rb
+++ lib/active_admin/inputs/filter_boolean_input.rb
@@ -11,16 +11,28 @@ module ActiveAdmin
         end
       end
 
-      def label_text
-        super.sub(/_eq\z/, '') + '?'
+      def check_box_html
+        template.check_box_tag("#{object_name}[#{method}]", checked_value, checked?, input_html_options)
       end
 
-      def method
-        super.to_s =~ search_conditions ? super : "#{super}_eq"
+      def search_method
+        method.to_s.match(metasearch_conditions) ? method : "#{method}_eq"
       end
 
-      def search_conditions
-        /(is_true|is_false|is_present|is_blank|is_null|is_not_null)\z/
+      def checked?
+        if defined? ActionView::Helpers::InstanceTag
+          object && ActionView::Helpers::InstanceTag.check_box_checked?(object.send(search_method), checked_value)
+        else
+          object && boolean_checked?(object.send(search_method), checked_value)
+        end
+      end
+
+      def input_html_options
+        { :name => "q[#{search_method}]" }
+      end
+
+      def metasearch_conditions
+        /(is_true|is_false|is_present|is_blank|is_null|is_not_null)$/
       end
 
     end
