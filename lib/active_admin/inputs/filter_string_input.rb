5829b543fdd4aaa8b1530d4e336ff352f4dee9d3
JoseLuis Torres <joseluis@powhow.com>
2013-05-24 12:11:24 -0500

    updating the string filter to use the new module #2002

--- lib/active_admin/inputs/filter_string_input.rb
+++ lib/active_admin/inputs/filter_string_input.rb
@@ -2,6 +2,7 @@ module ActiveAdmin
   module Inputs
     class FilterStringInput < ::Formtastic::Inputs::StringInput
       include FilterBase
+      include FilterBase::SearchMethodSelect
 
       def to_html
         if method.to_s.match(metasearch_conditions)
@@ -10,13 +11,7 @@ module ActiveAdmin
             builder.text_field(input_name_simple, input_html_options)
           end
         else
-          input_wrapping do
-            [ label_html,
-              select_html,
-              " ",
-              input_html
-            ].join("\n").html_safe
-          end
+          super
         end
       end
 
@@ -30,37 +25,6 @@ module ActiveAdmin
 
       def label_text
         I18n.t('active_admin.search_field', :field => super)
-      end
-
-      def input_html
-        builder.text_field current_filter, input_html_options_multiple
-      end
-
-      def input_html_options_multiple
-        { :size => 10, :id => "q_#{method}" }
-      end
-
-      def select_html
-        template.select_tag '', select_options, select_html_options
-      end
-
-      def select_options
-        template.options_for_select filters, current_filter
-      end
-
-      def select_html_options
-        { :onchange => "document.getElementById('q_#{method}').name = 'q[' + this.value + ']';" }
-      end
-
-      # Returns the filter currently in use, or the first one available
-      def current_filter
-        ( filters.detect{ |(_,query)| @object.send query } || filters.first )[1]
-      end
-
-      def filters
-        (options[:filters] || default_filters).collect do |(translation,scope)|
-          [translation, "#{method}_#{scope}"]
-        end
       end
 
       def default_filters
