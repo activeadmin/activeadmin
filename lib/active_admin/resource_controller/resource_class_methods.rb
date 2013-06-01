65456ded6148fa46574a9c183d9bb0578b5b02b2
Daxter <xovatdev@gmail.com>
2013-06-01 17:47:45 -0500

    revert part of 93d1d866
    it appears that instance methods can't be defined in this manner
    without using `class_exec`

--- lib/active_admin/resource_controller/resource_class_methods.rb
+++ lib/active_admin/resource_controller/resource_class_methods.rb
@@ -6,14 +6,16 @@ module ActiveAdmin
       # methods to only return the class defined in the instance
       # of ActiveAdmin::Resource
       def override_resource_class_methods!
-        def self.resource_class=(klass); end
+        class_exec do
+          def self.resource_class=(klass); end
 
-        def self.resource_class
-          @active_admin_config ? @active_admin_config.resource_class : nil
-        end
+          def self.resource_class
+            @active_admin_config ? @active_admin_config.resource_class : nil
+          end
 
-        def resource_class
-          self.class.resource_class
+          def resource_class
+            self.class.resource_class
+          end
         end
       end
 
