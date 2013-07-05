4b30acfdd30ad04f0608373966cbf597bfd3b33d
Sean Linsley <xovatdev@gmail.com>
2013-07-05 00:52:20 -0500

    prevent Ransack method naming conflicts with other gems

--- lib/active_admin/resource_controller/data_access.rb
+++ lib/active_admin/resource_controller/data_access.rb
@@ -230,8 +230,10 @@ module ActiveAdmin
         end
       end
 
+      # Applies any Ransack search methods to the currently scoped collection.
+      # Both `search` and `ransack` are provided, but we use `ransack` to prevent conflicts.
       def apply_filtering(chain)
-        @search = chain.search clean_search_params params[:q]
+        @search = chain.ransack clean_search_params params[:q]
         @search.result
       end
 
