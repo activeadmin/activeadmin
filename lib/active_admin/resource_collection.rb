3523ea13e80f01c1678d27c00c2c2c883102797c
Charles Maresh <charles.maresh@orm-tech.com>
2013-09-03 14:20:01 -0500

    Clarify find_matching_resource intentions
    - Add documentation and use smaller methods to reveal
      ResourceCollection.find_matching_resource intentions and the order of
      precedence.
    - Add more specs to verify functionality.

--- lib/active_admin/resource_collection.rb
+++ lib/active_admin/resource_collection.rb
@@ -19,22 +19,43 @@ module ActiveAdmin
       values.each &block
     end
 
-    # Find a resource by Resource::Name, resource's class, or resource's base class.
+    # Find a resource by resource_name, resource_class, or class's base_class.
+    #
+    # @param [Class, String] name_or_class Class or name to find a resource
+    #                        (e.g. Category or "Publisher")
+    #
+    # @returns [ActiveAdmin::Resource, nil]
     def find_matching_resource(name_or_class)
-      klass, name = name_or_class, name_or_class.to_s
-
-      if match = self[klass]
-        match
-      elsif match = real_resources.detect{ |r| r.resource_name.to_s == name }
-        match
-      elsif match = real_resources.detect{ |r| r.resource_class == klass }
-        match
-      elsif klass.respond_to? :base_class
-        real_resources.detect{ |r| r.resource_class == klass.base_class }
-      end
+      find_by_resource_name(name_or_class) || find_by_resource_class(name_or_class) || find_by_resource_base_class(name_or_class)
     end
 
     private
+
+    def find_by_resource_name(klass)
+      find do |resource|
+        resource.resource_name.to_s == klass.to_s
+      end
+    end
+
+    def find_by_resource_class(klass)
+      find do |resource|
+        resource.resource_class.to_s == klass.to_s
+      end
+    end
+
+    def find_by_resource_base_class(klass)
+      if klass.respond_to? :base_class
+        base_class = klass.base_class
+
+        find do |resource|
+          resource.resource_class.to_s == base_class.to_s
+        end
+      end
+    end
+
+    def find(&block)
+      real_resources.detect &block
+    end
 
     # REFACTOR: ResourceCollection currently stores Resource and Page objects. That doesn't
     # make sense, because by definition a ResourceCollection is a collection of resources.
