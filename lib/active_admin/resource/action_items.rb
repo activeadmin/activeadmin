505c8dcca6e75ca2c331be391c07296169a9a53f
Sean Linsley <xovatdev@gmail.com>
2013-10-09 20:20:38 -0500

    code style update

--- lib/active_admin/resource/action_items.rb
+++ lib/active_admin/resource/action_items.rb
@@ -5,8 +5,7 @@ module ActiveAdmin
   class Resource
     module ActionItems
 
-      # Add the default action items to a resource when it's
-      # initialized
+      # Adds the default action items to a resource when it's initialized
       def initialize(*args)
         super
         add_default_action_items
@@ -34,7 +33,7 @@ module ActiveAdmin
       #
       # @return [Array] Array of ActionItems for the controller actions
       def action_items_for(action, render_context = nil)
-        action_items.select{|item| item.display_on?(action, render_context) }
+        action_items.select{ |item| item.display_on? action, render_context }
       end
 
       # Clears all the existing action items for this resource
@@ -51,26 +50,25 @@ module ActiveAdmin
 
       # Adds the default action items to each resource
       def add_default_action_items
-        # New Link on index
+        # New link on index
         add_action_item only: :index do
           if controller.action_methods.include?('new') && authorized?(ActiveAdmin::Auth::CREATE, active_admin_config.resource_class)
-            link_to(I18n.t('active_admin.new_model', :model => active_admin_config.resource_label), new_resource_path)
+            link_to I18n.t('active_admin.new_model', model: active_admin_config.resource_label), new_resource_path
           end
         end
 
         # Edit link on show
-        add_action_item :only => :show do
+        add_action_item only: :show do
           if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-            link_to(I18n.t('active_admin.edit_model', :model => active_admin_config.resource_label), edit_resource_path(resource))
+            link_to I18n.t('active_admin.edit_model', model: active_admin_config.resource_label), edit_resource_path(resource)
           end
         end
 
         # Destroy link on show
-        add_action_item :only => :show do
-          if controller.action_methods.include?("destroy") && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-            link_to(I18n.t('active_admin.delete_model', :model => active_admin_config.resource_label),
-              resource_path(resource),
-              :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')})
+        add_action_item only: :show do
+          if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
+            link_to I18n.t('active_admin.delete_model', model: active_admin_config.resource_label), resource_path(resource),
+              method: :delete, data: {confirm: I18n.t('active_admin.delete_confirmation')}
           end
         end
       end
