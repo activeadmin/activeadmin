fb6472a97f35295d60713469daccf461ec46f610
Michael Durrant <m2@snap2web.com>
2013-10-22 21:23:46 -0400

    Split the stylesheet and javascript tests into two contexts.  The tests are the same, just different grouping.

--- spec/unit/asset_registration_spec.rb
+++ spec/unit/asset_registration_spec.rb
@@ -8,45 +8,52 @@ describe ActiveAdmin::AssetRegistration do
     clear_javascripts!
   end
 
-  it "should register a stylesheet file" do
-    register_stylesheet "active_admin.css"
-    stylesheets.length.should == 1
-    stylesheets.keys.first.should == "active_admin.css"
+  context "Stylesheet files" do
+
+    it "should register a stylesheet file" do
+      register_stylesheet "active_admin.css"
+      stylesheets.length.should == 1
+      stylesheets.keys.first.should == "active_admin.css"
+    end
+
+    it "should clear all existing stylesheets" do
+      register_stylesheet "active_admin.css"
+      stylesheets.length.should == 1
+      clear_stylesheets!
+      stylesheets.should be_empty
+    end
+
+    it "should allow media option when registering stylesheet" do
+      register_stylesheet "active_admin.css", media: :print
+      stylesheets.values.first[:media].should == :print
+    end
+
+    it "shouldn't register a stylesheet twice" do
+      register_stylesheet "active_admin.css"
+      register_stylesheet "active_admin.css"
+      stylesheets.length.should == 1
+    end
+
   end
 
-  it "should clear all existing stylesheets" do
-    register_stylesheet "active_admin.css"
-    stylesheets.length.should == 1
-    clear_stylesheets!
-    stylesheets.should be_empty
-  end
+  context "Javascript files" do
 
-  it "should allow media option when registering stylesheet" do
-    register_stylesheet "active_admin.css", media: :print
-    stylesheets.values.first[:media].should == :print
-  end
+    it "should register a javascript file" do
+      register_javascript "active_admin.js"
+      javascripts.should == ["active_admin.js"].to_set
+    end
 
-  it "shouldn't register a stylesheet twice" do
-    register_stylesheet "active_admin.css"
-    register_stylesheet "active_admin.css"
-    stylesheets.length.should == 1
-  end
+    it "should clear all existing javascripts" do
+      register_javascript "active_admin.js"
+      javascripts.should == ["active_admin.js"].to_set
+      clear_javascripts!
+      javascripts.should be_empty
+    end
 
-  it "should register a javascript file" do
-    register_javascript "active_admin.js"
-    javascripts.should == ["active_admin.js"].to_set
-  end
-
-  it "should clear all existing javascripts" do
-    register_javascript "active_admin.js"
-    javascripts.should == ["active_admin.js"].to_set
-    clear_javascripts!
-    javascripts.should be_empty
-  end
-
-  it "shouldn't register a javascript twice" do
-    register_javascript "active_admin.js"
-    register_javascript "active_admin.js"
-    javascripts.length.should == 1
+    it "shouldn't register a javascript twice" do
+      register_javascript "active_admin.js"
+      register_javascript "active_admin.js"
+      javascripts.length.should == 1
+    end
   end
 end
