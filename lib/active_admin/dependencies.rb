526de735f710cc214eb78e918b2608f241391b0f
Sean Linsley <xovatdev@gmail.com>
2013-12-06 10:42:41 -0800

    Dynamic dependency checker initially for Draper
    Noramally bundler would be used to ensure a specific version of a
    dependency such as Draper, but we don't want to depend on Draper
    at all.
    
    Orginally written by @seanlinsley (see https://github.com/gregbell/active_admin/pull/2662)
    
    Committed by @amiel

---
+++ lib/active_admin/dependencies.rb
@@ -0,0 +1,38 @@
+module ActiveAdmin
+  module Dependencies
+
+    # Provides a simple query interface to check for gem dependencies
+    #
+    # ActiveAdmin::Dependencies.draper
+    # => #<Gem::Specification:0x3ffb89c49ae0 draper-1.2.1>
+    #
+    # ActiveAdmin::Dependencies.draper?
+    # => true
+    #
+    # ActiveAdmin::Dependencies.draper? :<=, '1.1.0'
+    # => false
+    #
+    # ActiveAdmin::Dependencies.draper? :==, '1.2.1'
+    # => true
+    #
+    def self.check_for(gem_name)
+      gem_name = gem_name.to_s
+
+      singleton_class.send :define_method, gem_name do
+        Gem.loaded_specs[gem_name]
+      end
+
+      singleton_class.send :define_method, gem_name+'?' do |verb = nil, version = nil|
+        spec = send gem_name
+        if verb && version
+          !!spec && spec.version.send(verb, Gem::Version.create(version))
+        else
+          !!spec
+        end
+      end
+    end
+
+    check_for :draper
+
+  end
+end
