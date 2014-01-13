a779f3784ad5c05828dabe798d4a3ec2fcfb01c0
Artin Boghosian <artinboghosian@gmail.com>
2014-01-13 15:29:31 -0800

    Only reorder collection if it is grouped
    Only need to use work around of GROUP BY + COUNT queries if we are actually using grouping.

--- lib/active_admin/helpers/collection.rb
+++ lib/active_admin/helpers/collection.rb
@@ -7,12 +7,55 @@ module ActiveAdmin
       # the ORDER statement mentions a column defined in the SELECT statement.
       #
       # We remove the ORDER statement to work around this issue.
+      #
+      # If you use includes method on collection you will potentially call two queries for each
+      # association. For example, if you ORDER BY created_at DESC, calls
+      # to this method will remove the order clause and execute the query bringing
+      # back results sorted by id ASC (which is probably equivalent to created_at ASC)
+      # and all the associations for that result set will be eager loaded as well as
+      # the ones for your intended result set sorted by created_at DESC.
+      #
+      # Example:
+      #   class Company < ActiveRecord::Base
+      #     has_many :users
+      #   end
+      #
+      #   ActiveAdmin.register Company do
+      #     config.per_page = 5
+      #     config.sort_order = 'created_at_desc'
+      #     
+      #     actions :index
+      #
+      #     controller do
+      #       def index
+      #         index! do |format|
+      #           @companies = collection.includes(:users); format.html
+      #         end
+      #       end
+      #     end
+      #
+      #     # rest of admin code
+      #
+      #   SELECT * FROM companies ORDER BY created_at DESC LIMIT 5 OFFSET 0; # ids: 10, 9, 8, 7, 6
+      #   SELECT * FROM users WHERE company_id IN (10,9,8,7,6)
+      #
+      #   # now collection_size is called (e.g. from #items_in_collection?
+      #
+      #   SELECT * FROM companies LIMIT 5 OFFSET 0; # ids: 1,2,3,4,5
+      #   SELECT * FROM users WHERE company_id IN (1,2,3,4,5)
+      #
+      #   So we should only use this work around if the #group method was called on the
+      #   ActiveRecord::Relation. Or else proceed as normal.
       def collection_size(collection=collection)
-        size = collection.reorder("").count
-        # when GROUP BY is used, AR returns Hash instead of Fixnum for .size
-        size = size.size if size.kind_of?(Hash)
+        if collection.grouped_values.present?
+          size = collection.reorder("").count
+          # when GROUP BY is used, AR returns Hash instead of Fixnum for .size
+          size = size.size if size.kind_of?(Hash)
 
-        size
+          size
+        else
+          collection.count
+        end
       end
 
       def collection_is_empty?(collection=collection)
