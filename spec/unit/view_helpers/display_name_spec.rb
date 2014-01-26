7fc9be64650f65349a95365ecb76ac6ec198cfa7
Sean Linsley <xovatdev@gmail.com>
2014-01-26 12:35:54 -0600

    display_name spec cleanup
    note that our use of `subject{ }` here was incorrect -- the subject
    returned was actually the `"display_name"` string from the parent
    `describe` block :<

--- spec/unit/view_helpers/display_name_spec.rb
+++ spec/unit/view_helpers/display_name_spec.rb
@@ -4,37 +4,35 @@ describe "display_name" do
 
   include ActiveAdmin::ViewHelpers
 
-  [:display_name, :full_name, :name, :username, :login, :title, :email, :to_s].each do |m|
+  ActiveAdmin::Application.new.display_name_methods.map(&:to_s).each do |m|
     it "should return #{m} when defined" do
-      r = Class.new do
-        define_method m do
-          m.to_s
-        end
-      end.new
-      expect(display_name(r)).to eq m.to_s
+      klass = Class.new do
+        define_method(m) { m }
+      end
+      expect(display_name klass.new).to eq m
     end
   end
 
   it "should memeoize the result for the class" do
-    subject { Class.new.new }
+    subject = Class.new.new
     expect(subject).to receive(:name).twice.and_return "My Name"
-    expect(display_name(subject)).to eq "My Name"
+    expect(display_name subject).to eq "My Name"
     expect(ActiveAdmin.application).to_not receive(:display_name_methods)
-    expect(display_name(subject)).to eq "My Name"
+    expect(display_name subject).to eq "My Name"
   end
 
   it "should not call a method if it's an association" do
-    subject { Class.new.new }
+    subject = Class.new.new
     subject.stub_chain(:class, :reflect_on_all_associations).and_return [ double(name: :login) ]
     subject.stub :login
     expect(subject).to_not receive :login
     subject.stub(:email).and_return 'foo@bar.baz'
-    expect(display_name(subject)).to eq 'foo@bar.baz'
+    expect(display_name subject).to eq 'foo@bar.baz'
   end
 
   [nil, false].each do |type|
     it "should return nil when the passed object is #{type.inspect}" do
-      expect(display_name(type)).to eq nil
+      expect(display_name type).to eq nil
     end
   end
 
