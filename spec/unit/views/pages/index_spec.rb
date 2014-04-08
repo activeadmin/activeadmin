0fdc739fbee0878da5ddcfc2d0191c67acf0a6f7
Timo Schilling <timo@schilling.io>
2014-04-08 22:52:18 +0200

    tests for ActiveAdmin::Views::Pages::Index#title

---
+++ spec/unit/views/pages/index_spec.rb
@@ -0,0 +1,61 @@
+require "spec_helper"
+
+describe ActiveAdmin::Views::Pages::Index do
+  describe "#title" do
+    let!(:application){ ActiveAdmin::Application.new }
+    let(:namespace){ ActiveAdmin::Namespace.new(application, "Admin") }
+    let!(:params){ { controller: "UsersController", action: "edit" } }
+    let(:helpers) do
+      helpers = mock_action_view
+      helpers.stub active_admin_config: namespace.register(Post),
+                   params: params
+
+      helpers
+    end
+
+    let(:arbre_context) do
+      OpenStruct.new(params: params, helpers: helpers, assigns: {})
+    end
+
+    context "when config[:title] is assigned" do
+      context "with a Proc" do
+        it "should return the value of the assigned Proc" do
+          page = ActiveAdmin::Views::Pages::Index.new(arbre_context)
+          page.stub config: { title: ->{ "My Page Title" } }
+          expect(page.title).to eq "My Page Title"
+        end
+      end
+
+      context "with a String" do
+        it "should return the assigned String" do
+          page = ActiveAdmin::Views::Pages::Index.new(arbre_context)
+          page.stub config: { title: "My Page Title" }
+          expect(page.title).to eq "My Page Title"
+        end
+      end
+
+      context "with a Integer" do
+        it "should return the Integer" do
+          page = ActiveAdmin::Views::Pages::Index.new(arbre_context)
+          page.stub config: { title: 1 }
+          expect(page.title).to eq 1
+        end
+      end
+    end
+
+    context "when page_title is assigned" do
+      it "should return the set page title" do
+        arbre_context.assigns[:page_title] = "My Page Title"
+        page = ActiveAdmin::Views::Pages::Index.new(arbre_context)
+        expect(page.title).to eq "My Page Title"
+      end
+    end
+
+    context "when page_title is not assigned" do
+      it "should return the correct I18n text" do
+        page = ActiveAdmin::Views::Pages::Index.new(arbre_context)
+        expect(page.title).to eq "Posts"
+      end
+    end
+  end
+end
