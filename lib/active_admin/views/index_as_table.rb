76ef5f72072d6bd3fdb5342730951b99dd9ef65a
Sean Linsley <xovatdev@gmail.com>
2014-05-14 19:46:46 -0500

    style update

--- lib/active_admin/views/index_as_table.rb
+++ lib/active_admin/views/index_as_table.rb
@@ -283,13 +283,14 @@ module ActiveAdmin
 
         def items_default_actions(resource)
           if controller.action_methods.include?('show') && authorized?(ActiveAdmin::Auth::READ, resource)
-            item(I18n.t('active_admin.view'), resource_path(resource), :class => "view_link")
+            item I18n.t('active_admin.view'), resource_path(resource), class: 'view_link'
           end
           if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-            item(I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "edit_link")
+            item I18n.t('active_admin.edit'), edit_resource_path(resource), class: 'edit_link'
           end
           if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-            item(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "delete_link")
+            item I18n.t('active_admin.delete'), resource_path(resource), class: 'delete_link',
+              method: :delete, data: {confirm: I18n.t('active_admin.delete_confirmation')}
           end
         end
 
@@ -297,13 +298,14 @@ module ActiveAdmin
           links = proc do |resource|
             links = ''.html_safe
             if controller.action_methods.include?('show') && authorized?(ActiveAdmin::Auth::READ, resource)
-              links << link_to(I18n.t('active_admin.view'), resource_path(resource), class: "member_link view_link")
+              links << link_to(I18n.t('active_admin.view'), resource_path(resource), class: 'member_link view_link')
             end
             if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-              links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), class: "member_link edit_link")
+              links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), class: 'member_link edit_link')
             end
             if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-              links << link_to(I18n.t('active_admin.delete'), resource_path(resource), method: :delete, data: {confirm: I18n.t('active_admin.delete_confirmation')}, class: "member_link delete_link")
+              links << link_to(I18n.t('active_admin.delete'), resource_path(resource), class: 'member_link delete_link',
+                method: :delete, data: {confirm: I18n.t('active_admin.delete_confirmation')})
             end
             links
           end
