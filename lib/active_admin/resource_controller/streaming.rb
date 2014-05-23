8b4c1b5ef65ebdf77413bcaa955c0f7badb26ffb
Sean Linsley <xovatdev@gmail.com>
2014-05-22 23:19:18 -0500

    don't use AS::Concern

--- lib/active_admin/resource_controller/streaming.rb
+++ lib/active_admin/resource_controller/streaming.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 module ActiveAdmin
   class ResourceController < BaseController
 
@@ -5,49 +7,41 @@ module ActiveAdmin
     # Could be expanded to JSON and XML in the future.
     #
     module Streaming
-      extend ActiveSupport::Concern
 
-      included do
-        require 'csv' unless defined?(CSV)
-        include CSVStream
+      def index
+        super { |format| format.csv { stream_csv collection } }
       end
 
-      module CSVStream
+      protected
 
-        def index
-          super { |format| format.csv { stream_csv collection } }
-        end
+      def csv_line(resource, columns, options)
+        columns.map do |column|
+          s = call_method_or_proc_on resource, column.data
 
-        protected
-
-        def csv_line(resource, columns, options)
-          columns.map do |column|
-            s = call_method_or_proc_on resource, column.data
-
-            if options[:encoding] && s.respond_to?(:encode!)
-              s.encode! options[:encoding], options[:encoding_options]
-            else
-              s
-            end
-          end
-        end
-
-        def stream_csv(collection)
-          default = ActiveAdmin.application.csv_options
-          options = default.merge active_admin_config.csv_builder.options
-          columns = active_admin_config.csv_builder.render_columns(self)
-
-          headers['X-Accel-Buffering'] = 'no'
-          headers['Cache-Control'] = 'no-cache'
-
-          self.response_body = Enumerator.new do |csv|
-            csv << CSV.generate_line(columns.map(&:name), options)
-            collection.find_each do |resource|
-              csv << CSV.generate_line(csv_line(resource, columns, options), options)
-            end
+          if options[:encoding] && s.respond_to?(:encode!)
+            s.encode! options[:encoding], options[:encoding_options]
+          else
+            s
           end
         end
       end
+
+      def stream_csv(collection)
+        default = ActiveAdmin.application.csv_options
+        options = default.merge active_admin_config.csv_builder.options
+        columns = active_admin_config.csv_builder.render_columns(self)
+
+        headers['X-Accel-Buffering'] = 'no'
+        headers['Cache-Control'] = 'no-cache'
+
+        self.response_body = Enumerator.new do |csv|
+          csv << CSV.generate_line(columns.map(&:name), options)
+          collection.find_each do |resource|
+            csv << CSV.generate_line(csv_line(resource, columns, options), options)
+          end
+        end
+      end
+
     end
   end
 end
