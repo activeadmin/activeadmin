b241495d035c4c635a54f03e9e59abbf68061124
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2014-06-03 08:27:54 -0700

    Update Gemfile and specs so that everything runs under JRuby

--- spec/integration/memory_spec.rb
+++ spec/integration/memory_spec.rb
@@ -2,21 +2,23 @@ require 'spec_helper'
 
 describe "Memory Leak" do
 
-  def count_instances_of(klass)
-    ObjectSpace.each_object(klass) { }
-  end
+  if (RUBY_PLATFORM != "java")
+    def count_instances_of(klass)
+      ObjectSpace.each_object(klass) { }
+    end
 
-  [ActiveAdmin::Namespace, ActiveAdmin::Resource].each do |klass|
-    it "should not leak #{klass}" do
-      previously_disabled = GC.enable
-      GC.start
-      count = count_instances_of(klass)
+    [ActiveAdmin::Namespace, ActiveAdmin::Resource].each do |klass|
+      it "should not leak #{klass}" do
+        previously_disabled = GC.enable
+        GC.start
+        count = count_instances_of(klass)
 
-      load_defaults!
+        load_defaults!
 
-      GC.start
-      GC.disable if previously_disabled
-      expect(count_instances_of klass).to be <= count
+        GC.start
+        GC.disable if previously_disabled
+        expect(count_instances_of klass).to be <= count
+      end
     end
   end
 
