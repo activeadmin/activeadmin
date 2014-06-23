c63b7b2e912802709e17b10859b1766adf39cb8a
Timo Schilling <timo@schilling.io>
2014-06-23 19:27:55 +0200

    replace stubs with allow receive in from_builder_spec

--- spec/unit/form_builder_spec.rb
+++ spec/unit/form_builder_spec.rb
@@ -383,7 +383,7 @@ describe ActiveAdmin::FormBuilder do
       context "with an existing post" do
         let :body do
           build_form({url: '/categories'}, Category.new) do |f|
-            f.object.posts.build.stub(:new_record? => false)
+            allow(f.object.posts.build).to receive(:new_record?).and_return(false)
             f.has_many :posts, allow_destroy: true do |p|
               p.input :title
             end
