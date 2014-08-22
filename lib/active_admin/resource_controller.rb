0870e0e579bcdd66a46e4f9526d7344c22e92f83
Timo Schilling <timo@schilling.io>
2014-08-22 14:31:09 +0200

    remove require inherited_resources

--- lib/active_admin/resource_controller.rb
+++ lib/active_admin/resource_controller.rb
@@ -1,4 +1,3 @@
-require 'inherited_resources'
 require 'active_admin/resource_controller/action_builder'
 require 'active_admin/resource_controller/data_access'
 require 'active_admin/resource_controller/decorators'
