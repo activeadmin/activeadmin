0ef0e9aeaf4e5ff35b28f48e4ea5a31e1385c904
Timo Schilling <timo@schilling.io>
2014-09-04 00:27:35 +0200

    simple page to view all ActiveAdmin::Iconic::ICONS

---
+++ app/admin/icons.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register_page "Icons", namespace: :active_admin do
+  content do
+    ul do
+      ActiveAdmin::Iconic::ICONS.keys.each do |key|
+        li do
+          span do
+            ActiveAdmin::Iconic.icon key, width: 50, height: 50
+          end
+          span do
+            key
+          end
+        end
+      end
+    end
+  end
+end
