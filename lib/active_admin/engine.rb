9ee93c89a57e9b7e8dd1a74989a2cb4534b45e07
Timo Schilling <timo@schilling.io>
2014-09-09 18:00:45 +0200

    setup for infopage namespace

--- lib/active_admin/engine.rb
+++ lib/active_admin/engine.rb
@@ -3,5 +3,15 @@ module ActiveAdmin
     initializer "ActiveAdmin precompile hook", group: :all do |app|
       app.config.assets.precompile += %w(active_admin.js active_admin.css active_admin/print.css)
     end
+
+    initializer "ActiveAdmin info page setup" do
+      ActiveAdmin.setup do |config|
+        config.namespace :active_admin do |active_admin|
+          active_admin.authorization_adapter = ActiveAdmin::AuthorizationAdapter
+          active_admin.authentication_method = false
+          active_admin.current_user_method = false
+        end
+      end
+    end
   end
 end
