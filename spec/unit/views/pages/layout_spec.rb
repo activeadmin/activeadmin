fbc4191f5ada08691bc234e69bb29acccd46f763
Timo Schilling <timo@schilling.io>
2014-09-10 23:12:05 +0200

    fix tests

--- spec/unit/views/pages/layout_spec.rb
+++ spec/unit/views/pages/layout_spec.rb
@@ -13,7 +13,8 @@ describe ActiveAdmin::Views::Pages::Layout do
       current_active_admin_user:  nil,
       current_active_admin_user?: false,
       current_menu:               double('Menu', items: []),
-      params:                     {controller: 'UsersController', action: 'edit'}
+      params:                     {controller: 'UsersController', action: 'edit'},
+      env:                        {}
     }.each do |method, returns|
       allow(helpers).to receive(method).and_return returns
     end
