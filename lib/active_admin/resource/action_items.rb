d6bb02810c00d6dd34ad883c0f0fdb4799074089
Isaac Betesh <iybetesh@gmail.com>
2014-09-22 16:59:25 -0400

    made it possible to override action item labels in I18n at the model level

--- lib/active_admin/resource/action_items.rb
+++ lib/active_admin/resource/action_items.rb
@@ -58,26 +58,29 @@ module ActiveAdmin
         # New link on index
         add_action_item :new, only: :index do
           if controller.action_methods.include?('new') && authorized?(ActiveAdmin::Auth::CREATE, active_admin_config.resource_class)
-            link_to I18n.t('active_admin.new_model', model: active_admin_config.resource_label), new_resource_path
+            link_to i18n(:new), new_resource_path
           end
         end
 
         # Edit link on show
         add_action_item :show, only: :show do
           if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-            link_to I18n.t('active_admin.edit_model', model: active_admin_config.resource_label), edit_resource_path(resource)
+            link_to i18n(:edit), edit_resource_path(resource)
           end
         end
 
         # Destroy link on show
         add_action_item :destroy, only: :show do
           if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-            link_to I18n.t('active_admin.delete_model', model: active_admin_config.resource_label), resource_path(resource),
+            link_to i18n(:destroy), resource_path(resource),
               method: :delete, data: {confirm: I18n.t('active_admin.delete_confirmation')}
           end
         end
       end
 
+      def i18n(action)
+        I18n.t("active_admin.#{active_admin_config.resource_name.singular}.#{action}_model", model: active_admin_config.resource_label, defaults: ["active_admin.#{action}_model"])
+      end
     end
   end
 
