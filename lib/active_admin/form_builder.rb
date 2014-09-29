activeadmin/activeadmin

commit 2133d7cdf6ec9d85406a95af9a6496306ef6492e

cleanup

lib/active_admin/form_builder.rb
@@ -166,7 +166,7 @@ module ActiveAdmin
     def js_for_has_many(assoc, form_block, template, new_record, class_string)
       assoc_reflection = object.class.reflect_on_association assoc
       assoc_name       = assoc_reflection.klass.model_name
-      placeholder      = "NEW_#{assoc_name.to_s.upcase.split(' ').join('_')}_RECORD"
+      placeholder      = "NEW_#{assoc_name.to_s.underscore.upcase}_RECORD"
       opts = {
         for: [assoc, assoc_reflection.klass.new],
         class: class_string,

commit c74e650d56150e22594e7a648647e50f91d491d9

Fix faulty placeholder in has many forms.
2133d7cdf6ec9d85406a95af9a6496306ef6492e introduced an error in all has_many forms with namespaced models.
Because ids cannot contain a forward-slash it will be rewritten by rails helpers to an underscore. So the javascript could not replace the placeholders anymore.

lib/active_admin/form_builder.rb
@@ -166,7 +166,7 @@ module ActiveAdmin
     def js_for_has_many(assoc, form_block, template, new_record, class_string)
       assoc_reflection = object.class.reflect_on_association assoc
       assoc_name       = assoc_reflection.klass.model_name
-      placeholder      = "NEW_#{assoc_name.to_s.underscore.upcase}_RECORD"
+      placeholder      = "NEW_#{assoc_name.to_s.underscore.upcase.gsub(/\//, '_')}_RECORD"
       opts = {
         for: [assoc, assoc_reflection.klass.new],
         class: class_string,