f072e2f5723325a08b97580523014fa3429bc004
Артём Большаков <abolshakov@spbtv.com>
2014-10-15 13:46:29 +0400

    Do not ask if its array, convert to array in needed or return obj

--- lib/active_admin/views/components/attributes_table.rb
+++ lib/active_admin/views/components/attributes_table.rb
@@ -5,7 +5,7 @@ module ActiveAdmin
       builder_method :attributes_table_for
 
       def build(obj, *attrs)
-        @collection     = is_array?(obj) ? obj : [obj]
+        @collection     = as_array(obj)
         @resource_class = @collection.first.class
         options = { }
         options[:for] = @collection.first if single_record?
@@ -103,9 +103,13 @@ module ActiveAdmin
       def single_record?
         @single_record ||= @collection.size == 1
       end
-      
-      def is_array?(obj)
-        obj.respond_to?(:each) && obj.respond_to?(:first) && !obj.is_a?(Hash)
+
+      def as_array(obj)
+        if obj.respond_to?(:each) && obj.respond_to?(:first) && !obj.is_a?(Hash)
+          obj
+        else
+          [obj]
+        end
       end
     end
 
