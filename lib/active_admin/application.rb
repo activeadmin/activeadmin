7971bc6b5b06e44b08a223900eea3318c9d6fe90
Timo Schilling <timo@schilling.io>
2014-10-26 09:55:28 +0100

    add byte order mark option to csv

--- lib/active_admin/application.rb
+++ lib/active_admin/application.rb
@@ -71,7 +71,7 @@ module ActiveAdmin
     inheritable_setting :breadcrumb, true
 
     # Default CSV options
-    inheritable_setting :csv_options, {col_sep: ','}
+    inheritable_setting :csv_options, { col_sep: ',', byte_order_mark: "\xEF\xBB\xBF" }
 
     # Default Download Links options
     inheritable_setting :download_links, true
