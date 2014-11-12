7fc08608bfe6900f7c319d00d67b539fdbef7076
Alexes <alexes.dev@gmail.com>
2014-11-12 11:20:29 +0300

    fix build_comment_form method

--- lib/active_admin/orm/active_record/comments/views/active_admin_comments.rb
+++ lib/active_admin/orm/active_record/comments/views/active_admin_comments.rb
@@ -53,7 +53,7 @@ module ActiveAdmin
         end
 
         def build_comment_form
-          self << active_admin_form_for(ActiveAdmin::Comment.new, url: comment_form_url) do |f|
+          active_admin_form_for(ActiveAdmin::Comment.new, url: comment_form_url) do |f|
             f.inputs do
               f.input :resource_type, as: :hidden,  input_html: { value: ActiveAdmin::Comment.resource_type(@resource) }
               f.input :resource_id,   as: :hidden,  input_html: { value: @resource.id }
