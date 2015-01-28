47b702594503d79188110a2169b571b019b387d1
Craig McNamara <craig@caring.com>
2015-01-27 21:46:31 -0800

    Fix default delete prompt.
    Comes through in HTML with a data=“{:ruby=>’hash’}”

--- lib/active_admin/views/index_as_table.rb
+++ lib/active_admin/views/index_as_table.rb
@@ -194,7 +194,7 @@ module ActiveAdmin
               links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link")
             end
             if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-              links << link_to(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "member_link delete_link")
+              links << link_to(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, 'data-confirm' => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link")
             end
             links
           end
