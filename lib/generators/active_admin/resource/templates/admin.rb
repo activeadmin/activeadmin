8a99a44aca3a4b8f18ae11fd4e79ee4bcd727f4e
Chuck Callebs <cacallebs@gmail.com>
2015-02-28 14:55:53 -0500

    Add proof of concept for generation boilerplate.

--- lib/generators/active_admin/resource/templates/admin.rb
+++ lib/generators/active_admin/resource/templates/admin.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register <%= class_name %> do
 
+# Available Filters
+#<% class_name.constantize.new.attributes.keys.each do |attr| %>
+# filter :<%= attr.gsub(/_id$/, '') %><% end %>
+
+# Sample Index
+#
+# index do
+#   selectable_column
+#   id_column<% class_name.constantize.new.attributes.keys.each do |attr| %>
+#   column :<%= attr.gsub(/_id$/, '') %><% end %>
+#   actions
+# end
+
 <% if Rails::VERSION::MAJOR == 4 || defined?(ActionController::StrongParameters) %>
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -14,5 +27,6 @@ ActiveAdmin.register <%= class_name %> do
   #   permitted
   # end
 <% end %>
+
 
 end
