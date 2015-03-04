e27ccba8a7ea1f7f3085748decec1f6911f6d5d2
Timo Schilling <timo@schilling.io>
2015-03-04 19:04:02 +0100

    refactor errors

--- lib/active_admin/error.rb
+++ lib/active_admin/error.rb
@@ -43,7 +43,7 @@ module ActiveAdmin
       raise new exception
     end
 
-  private
+    private
 
     def self.database_error_classes
       @classes ||= []
@@ -54,6 +54,9 @@ module ActiveAdmin
   end
 
   class NoMenuError < KeyError
+  end
+
+  class GeneratorError < Error
   end
 
 end
