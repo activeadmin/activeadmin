747164b640dbf407eac7c086c2415d3106f7d137
Javier Julio <jjfutbol@gmail.com>
2015-04-01 22:39:17 -0400

    Fix failing form_builder_spec
    I added “action” to the class for a form cancel link button so the
    selector here needs to be changed to a contains lookup. Spec now
    passing.

--- spec/unit/form_builder_spec.rb
+++ spec/unit/form_builder_spec.rb
@@ -157,7 +157,7 @@ describe ActiveAdmin::FormBuilder do
         f.actions
       end
       expect(body).to have_selector("[type=submit]", count: 1)
-      expect(body).to have_selector("[class=cancel]", count: 1)
+      expect(body).to have_selector("[class~=cancel]", count: 1)
     end
     it "should generate multiple actions" do
       body = build_form do |f|
@@ -167,7 +167,7 @@ describe ActiveAdmin::FormBuilder do
         end
       end
       expect(body).to have_selector("[type=submit]", count: 2)
-      expect(body).to have_selector("[class=cancel]", count: 0)
+      expect(body).to have_selector("[class~=cancel]", count: 0)
     end
 
   end
