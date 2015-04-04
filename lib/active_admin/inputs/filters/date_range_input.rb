dca85ce4505927e71293b0a6190da8f3ecc65dd6
Javier Julio <jjfutbol@gmail.com>
2015-04-03 22:35:12 -0400

    Display date placeholder in date picker field, remove icon
    We should show placeholders where possible. Clearer to show format than
    to have the icon. Ideally we’d like to have SVG icons that can work in
    both CSS and in HTML (current Iconic solution only works in HTML). The
    icons look like they could be updated too (Iconic is a paid solution so
    we should look into an open source SVG one if necessary). Remove nbsp
    as its unnecessary.

--- lib/active_admin/inputs/filters/date_range_input.rb
+++ lib/active_admin/inputs/filters/date_range_input.rb
@@ -7,8 +7,8 @@ module ActiveAdmin
         def to_html
           input_wrapping do
             [ label_html,
-              builder.text_field(gt_input_name, input_html_options(gt_input_name, gt_input_placeholder)),
-              builder.text_field(lt_input_name, input_html_options(lt_input_name, lt_input_placeholder)),
+              builder.text_field(gt_input_name, input_html_options(gt_input_name)),
+              builder.text_field(lt_input_name, input_html_options(lt_input_name)),
             ].join("\n").html_safe
           end
         end
@@ -22,20 +22,16 @@ module ActiveAdmin
           "#{method}_lteq"
         end
 
-        def gt_input_placeholder
-          I18n.t("active_admin.filters.predicates.starts_with")
+        def input_placeholder
+          I18n.t("active_admin.filters.placeholders.date")
         end
 
-        def lt_input_placeholder
-          I18n.t("active_admin.filters.predicates.ends_with")
-        end
-
-        def input_html_options(input_name = gt_input_name, placeholder = gt_input_placeholder)
+        def input_html_options(input_name = gt_input_name)
           current_value = @object.public_send input_name
           { size: 12,
             class: "datepicker",
             max: 10,
-            placeholder: placeholder,
+            placeholder: input_placeholder,
             value: current_value.respond_to?(:strftime) ? current_value.strftime("%Y-%m-%d") : "" }
         end
       end
