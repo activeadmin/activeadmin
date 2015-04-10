932dacf5b515f404a56a083ab1da9d97f198adee
Javier Julio <jjfutbol@gmail.com>
2015-04-10 00:02:26 -0400

    Change .header to .main-navbar and add .utility-nav
    Also remove unused .header-item for now.

--- lib/active_admin/views/header.rb
+++ lib/active_admin/views/header.rb
@@ -4,15 +4,15 @@ module ActiveAdmin
     class Header < Component
 
       def build(namespace, menu)
-        super(id: "header")
+        super(id: "header", class: "main-navbar")
 
         @namespace = namespace
         @menu = menu
         @utility_menu = @namespace.fetch_menu(:utility_navigation)
 
         site_title @namespace
-        global_navigation @menu, class: "header-item tabs"
-        utility_navigation @utility_menu, id: "utility_nav", class: "header-item tabs"
+        global_navigation @menu, class: "main-navigation tabs"
+        utility_navigation @utility_menu, id: "utility_nav", class: "main-utility-navigation tabs"
       end
 
     end
