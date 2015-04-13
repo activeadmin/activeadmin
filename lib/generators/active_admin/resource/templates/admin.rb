c96fadb5a5e00c7fef3aa18e2a76ee9b6e4214cc
Timo Schilling <timo@schilling.io>
2015-04-13 17:44:38 +0200

    change resource generator output formation

--- lib/generators/active_admin/resource/templates/admin.rb
+++ lib/generators/active_admin/resource/templates/admin.rb
@@ -1,17 +1,17 @@
 ActiveAdmin.register <%= class_name %> do
 <% if Rails::VERSION::MAJOR == 4 || defined?(ActionController::StrongParameters) %>
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
 <% end %>
 <% if options.include_boilerplate? %>
 # Limit actions available to your users by adding them to the 'except' array
