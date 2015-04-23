e61e299709f566a99ae00ea3bf425e00b7f8cd9e
Javier Julio <jjfutbol@gmail.com>
2015-04-22 23:23:13 -0400

    Fix failing form_builder spec
    This was a long lived branch that had a conflict here and I ended up
    choosing the wrong one when correcting it. All tests green.

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -60,7 +60,7 @@ module ActiveAdmin
           template.concat has_many_actions(has_many_form, builder_options, "".html_safe)
         end
 
-        template.assign('has_many_block'=> true)
+        template.assign(has_many_block: true)
         contents = without_wrapper { inputs(options, &form_block) }
 
         if builder_options[:new_record]
