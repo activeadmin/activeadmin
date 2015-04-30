14b64cdf946449091a467ea22adbafd88782b614
Javier Julio <jjfutbol@gmail.com>
2015-04-30 14:54:12 -0400

    Include print styles in main stylesheet
    No need to do this separately which makes an extra http request, plus
    this has the benefit of delaying print styles until needed. I’ve been
    doing this for a long time now and as its been well supported to
    include print styles using a media block. The HTML5 Boilerplate has
    stressed this as a good default approach for many years now.
    https://github.com/h5bp/html5-boilerplate/blob/c9f88cc79d380150e4e2e39e3c2deccfe8aa3d8c/dist/css/style.css#L189-L193

--- lib/active_admin/application.rb
+++ lib/active_admin/application.rb
@@ -164,8 +164,7 @@ module ActiveAdmin
     private
 
     def register_default_assets
-      register_stylesheet "active_admin.css", media: "screen"
-      register_stylesheet "active_admin/print.css", media: "print"
+      register_stylesheet "active_admin.css", media: "all"
       register_javascript "active_admin.js"
     end
 
