40eaf8f6c74f804742e3d464d6779836b03afcce
Andrey Koleshko <ka8725@gmail.com>
2015-09-05 20:04:22 +0300

    Fix warning from Ruby 2.2.0

--- lib/active_admin/helpers/collection.rb
+++ lib/active_admin/helpers/collection.rb
@@ -3,13 +3,13 @@ module ActiveAdmin
     module Collection
       # 1. removes `select` and `order` to prevent invalid SQL
       # 2. correctly handles the Hash returned when `group by` is used
-      def collection_size(c = collection)
+      def collection_size(c = collection())
         c = c.except :select, :order
 
         c.group_values.present? ? c.count.count : c.count
       end
 
-      def collection_is_empty?(c = collection)
+      def collection_is_empty?(c = collection())
         collection_size(c) == 0
       end
     end
