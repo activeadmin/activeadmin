69f213476129baf55a615d40456fc752bfdfe814
Sean Linsley <xovatdev@gmail.com>
2015-11-19 19:03:13 -0600

    change code style in CSV builder

--- lib/active_admin/csv_builder.rb
+++ lib/active_admin/csv_builder.rb
@@ -40,26 +40,23 @@ module ActiveAdmin
     end
 
     def build(controller, csv)
-      @collection = controller.send(:find_collection, except: :pagination)
-      options = ActiveAdmin.application.csv_options.merge self.options
-      columns = exec_columns controller.view_context
+      @collection  = controller.send :find_collection, except: :pagination
+      columns      = exec_columns controller.view_context
+      options      = ActiveAdmin.application.csv_options.merge self.options
+      bom          = options.delete :byte_order_mark
+      column_names = options.delete(:column_names) { true }
+      csv_options  = options.except :encoding_options
 
-      if byte_order_mark = options.delete(:byte_order_mark)
-        csv << byte_order_mark
+      csv << bom if bom
+
+      if column_names
+        csv << CSV.generate_line(columns.map{ |c| encode c.name, options }, csv_options)
       end
 
-      if options.delete(:column_names) { true }
-        csv << CSV.generate_line(
-          columns.map { |c| encode c.name, options },
-          options.except(:encoding_options))
-      end
-
-      (1..paginated_collection.total_pages).each do |page_no|
-        paginated_collection(page_no).each do |resource|
+      (1..paginated_collection.total_pages).each do |page|
+        paginated_collection(page).each do |resource|
           resource = controller.send :apply_decorator, resource
-          csv << CSV.generate_line(
-            build_row(resource, columns, options),
-            options.except(:encoding_options))
+          csv << CSV.generate_line(build_row(resource, columns, options), csv_options)
         end
       end
 
