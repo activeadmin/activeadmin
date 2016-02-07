ee8287f0f8e35fab7f4a318e2a75c21b58378d36
Sean Linsley <xovatdev@gmail.com>
2016-02-07 16:21:31 -0600

    turn on colored output for RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -133,6 +133,7 @@ RSpec.configure do |config|
   config.filter_run focus: true
   config.filter_run_excluding skip: true
   config.run_all_when_everything_filtered = true
+  config.color = true
 end
 
 # All RSpec configuration needs to happen before any examples
