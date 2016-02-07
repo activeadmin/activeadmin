4292aafa33910b705c3076f36e0cb8b2322dbca9
Sean Linsley <xovatdev@gmail.com>
2016-02-07 15:45:56 -0600

    fix batch actions URL generation

--- lib/active_admin/batch_actions/resource_extension.rb
+++ lib/active_admin/batch_actions/resource_extension.rb
@@ -54,8 +54,9 @@ module ActiveAdmin
       # Path to the batch action itself
       def batch_action_path(params = {})
         path = [route_collection_path(params), "batch_action"].join("/")
-        query = params.slice(:q, :scope).to_param
-        [path, query].reject(&:blank?).join("?")
+        query = params.slice(:q, :scope)
+        query = query.permit!.to_h if query.respond_to? :permit!
+        [path, query.to_param].reject(&:blank?).join("?")
       end
 
       private
