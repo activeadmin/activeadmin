dedf092c20381e34f8044deb21b8ac04dfa95c8b
jens <jens.kippels@gmail.com>
2016-06-07 18:14:34 +0200

    fixed locale en de

--- lib/active_admin/batch_actions/resource_extension.rb
+++ lib/active_admin/batch_actions/resource_extension.rb
@@ -53,6 +53,7 @@ module ActiveAdmin
 
       # Path to the batch action itself
       def batch_action_path(params = {})
+        # Hotfix here
         path = [route_collection_path(params), "batch_action"].join("/").gsub("?locale=de","").gsub("?locale=en","")
         query = params.slice(:q, :scope)
         query = query.permit!.to_h if query.respond_to? :permit!
