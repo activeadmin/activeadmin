b47291c09b125789ee26918c5b7f258761f2de62
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-10 15:42:40 -0300

    Tweak DB resetting in cucumber
    Use `trucation` strategy only for JS.

--- features/support/env.rb
+++ features/support/env.rb
@@ -72,35 +72,9 @@ Capybara.ignore_hidden_elements = false
 # of your scenarios, as this makes it hard to discover errors in your application.
 ActionController::Base.allow_rescue = false
 
-# If you set this to true, each scenario will run in a database transaction.
-# You can still turn off transactions on a per-scenario basis, simply tagging
-# a feature or scenario with the @no-txn tag. If you are using Capybara,
-# tagging with @culerity or @javascript will also turn transactions off.
-#
-# If you set this to false, transactions will be off for all scenarios,
-# regardless of whether you use @no-txn or not.
-#
-# Beware that turning transactions off will leave data in your database
-# after each scenario, which can lead to hard-to-debug failures in
-# subsequent scenarios. If you do this, we recommend you create a Before
-# block that will explicitly put your database in a known state.
-if ActiveAdmin::Dependency.rails5?
-  Cucumber::Rails::World.use_transactional_tests = true
-else
-  Cucumber::Rails::World.use_transactional_fixtures = true
-end
-
-# How to clean your database when transactions are turned off. See
-# http://github.com/bmabey/database_cleaner for more info.
-if defined?(ActiveRecord::Base)
-  begin
-    require 'database_cleaner'
-    require 'database_cleaner/cucumber'
-    DatabaseCleaner.strategy = :truncation
-  rescue LoadError
-    # ignore if database_cleaner isn't present
-  end
-end
+# Database resetting strategy
+DatabaseCleaner.strategy = :truncation
+Cucumber::Rails::Database.javascript_strategy = :truncation
 
 # Warden helpers to speed up login
 # See https://github.com/plataformatec/devise/wiki/How-To:-Test-with-Capybara
