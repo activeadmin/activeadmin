ed0e50c46253226f4ed022c9701e0f1f02f2e42f
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-10 12:40:32 -0300

    Remove Rspec 2 stuff from jasmine config

--- spec/javascripts/support/jasmine_config.rb
+++ spec/javascripts/support/jasmine_config.rb
@@ -5,19 +5,3 @@ module Jasmine
 
   end
 end
-
-
-# Note - this is necessary for rspec2, which has removed the backtrace
-module Jasmine
-  class SpecBuilder
-    def declare_spec(parent, spec)
-      me = self
-      example_name = spec["name"]
-      @spec_ids << spec["id"]
-      backtrace = @example_locations[parent.description + " " + example_name]
-      parent.it example_name, {} do
-        me.report_spec(spec["id"])
-      end
-    end
-  end
-end
