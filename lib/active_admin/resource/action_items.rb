11f6cf4d1bea3e52c477a9c7e87db70d97f2a3f8
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2017-01-17 14:08:13 +0100

    ADD: Action item labels per model

--- lib/active_admin/resource/action_items.rb
+++ lib/active_admin/resource/action_items.rb
@@ -64,7 +64,9 @@ module ActiveAdmin
       def add_default_new_action_item
         add_action_item :new, only: :index do
           if controller.action_methods.include?('new') && authorized?(ActiveAdmin::Auth::CREATE, active_admin_config.resource_class)
-            link_to I18n.t('active_admin.new_model', model: active_admin_config.resource_label), new_resource_path
+            per_model_yml_path = "active_admin.#{active_admin_config.resource_name.i18n_key.to_s}.new_model"
+            label = (!I18n.t(per_model_yml_path).include?("translation missing: ")) ? I18n.t(per_model_yml_path) : I18n.t('active_admin.new_model',     model: active_admin_config.resource_label)
+            link_to label, new_resource_path
           end
         end
       end
@@ -73,7 +75,9 @@ module ActiveAdmin
       def add_default_edit_action_item
         add_action_item :edit, only: :show do
           if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-            link_to I18n.t('active_admin.edit_model', model: active_admin_config.resource_label), edit_resource_path(resource)
+            per_model_yml_path = "active_admin.#{active_admin_config.resource_name.i18n_key.to_s}.edit_model"
+            label = (!I18n.t(per_model_yml_path).include?("translation missing: ")) ? I18n.t(per_model_yml_path) : I18n.t('active_admin.new_model',     model: active_admin_config.resource_label)
+            link_to label, edit_resource_path(resource)
           end
         end
       end
@@ -82,7 +86,9 @@ module ActiveAdmin
       def add_default_show_action_item
         add_action_item :destroy, only: :show do
           if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-            link_to I18n.t('active_admin.delete_model', model: active_admin_config.resource_label), resource_path(resource),
+            per_model_yml_path = "active_admin.#{active_admin_config.resource_name.i18n_key.to_s}.delete_model"
+            label = (!I18n.t(per_model_yml_path).include?("translation missing: ")) ? I18n.t(per_model_yml_path) : I18n.t('active_admin.delete_model',  model: active_admin_config.resource_label)
+            link_to label, resource_path(resource),
               method: :delete, data: {confirm: I18n.t('active_admin.delete_confirmation')}
           end
         end
