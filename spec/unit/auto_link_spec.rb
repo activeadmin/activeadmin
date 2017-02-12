d518690217fbbfee95ecac4ba6a87c720f1f82a2
David Rodríguez <deivid.rodriguez@gmail.com>
2017-02-12 14:36:51 -0200

    Rework `auto_link` tests
    Previous tests used too much mocking and were order dependent.
    
    Fixes
    
    ```
    rspec spec/unit/auto_link_spec.rb --seed 1234
    ```

--- spec/unit/auto_link_spec.rb
+++ spec/unit/auto_link_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "auto linking resources" do
+RSpec.describe "auto linking resources", type: :view do
   include ActiveAdmin::ViewHelpers::ActiveAdminApplicationHelper
   include ActiveAdmin::ViewHelpers::AutoLinkHelper
   include ActiveAdmin::ViewHelpers::DisplayHelper
   include MethodOrProcHelper
 
   let(:active_admin_config)   { double namespace: namespace }
-  let(:active_admin_namespace){ ActiveAdmin::Namespace.new(ActiveAdmin::Application.new, :admin) }
+  let(:active_admin_namespace){ ActiveAdmin.application.namespace(:admin) }
   let(:post){ Post.create! title: "Hello World" }
 
-  def authorized?(*)
-    true
+  before do
+    allow(self).to receive(:authorized?).and_return(true)
   end
 
   context "when the resource is not registered" do
+    before do
+      load_resources {}
+    end
+
     it "should return the display name of the object" do
       expect(auto_link(post)).to eq "Hello World"
     end
@@ -22,39 +26,43 @@ RSpec.describe "auto linking resources" do
 
   context "when the resource is registered" do
     before do
-      active_admin_namespace.register Post
+      load_resources do
+        active_admin_namespace.register Post
+      end
     end
+
     it "should return a link with the display name of the object" do
-      url_path = "/admin/posts/#{post.id}?locale=en"
-      expect(self).to receive(:url_options).and_return(locale: 'en')
-      expect(self).to receive(:url_for) { |url| url }
-      expect(self).to receive(:link_to).with "Hello World", url_path
-      auto_link(post)
+      expect(auto_link(post)).to \
+          match(%r{<a href="/admin/posts/\d+">Hello World</a>})
     end
 
     context "but the user doesn't have access" do
+      before do
+        allow(self).to receive(:authorized?).and_return(false)
+      end
+
       it "should return the display name of the object" do
-        expect(self).to receive(:authorized?).twice.and_return(false)
         expect(auto_link(post)).to eq "Hello World"
       end
     end
+  end
 
-    context "but the show action is disabled" do
-      before do
+  context "when the resource is registered with the show action disabled" do
+    before do
+      load_resources do
         active_admin_namespace.register(Post) { actions :all, except: :show }
-      end
-
-      it "should fallback to edit" do
-        url_path = "/admin/posts/#{post.id}/edit?locale=en"
-        expect(self).to receive(:url_options).and_return(locale: 'en')
-        expect(self).to receive(:url_for) { |url| url }
-        expect(self).to receive(:link_to).with "Hello World", url_path
-        auto_link(post)
       end
     end
 
-    context "but the show and edit actions are disabled" do
-      before do
+    it "should fallback to edit" do
+      expect(auto_link(post)).to \
+        match(%r{<a href="/admin/posts/\d+/edit">Hello World</a>})
+    end
+  end
+
+  context "when the resource is registered with the show & edit actions disabled" do
+    before do
+      load_resources do
         active_admin_namespace.register(Post) do
           actions :all, except: [:show, :edit]
         end
