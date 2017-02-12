7d88eb45a95048c66417f0f0a99b07a447fa1b91
David Rodríguez <deivid.rodriguez@gmail.com>
2017-02-12 14:40:54 -0200

    Make resource_localizer_spec directly runnable

--- spec/unit/localizers/resource_localizer_spec.rb
+++ spec/unit/localizers/resource_localizer_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 require File.expand_path('../config_shared_examples', File.dirname(__FILE__))
 
 RSpec.describe ActiveAdmin::Localizers::ResourceLocalizer do
