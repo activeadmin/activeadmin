ced8a16a2f4bda2271a007c1ff4f4ae2e151c167
David Rodríguez <deivid.rodriguez@gmail.com>
2017-02-12 14:52:59 -0200

    Move shared examples to the only file using them

--- spec/unit/localizers/resource_localizer_spec.rb
+++ spec/unit/localizers/resource_localizer_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
-require File.expand_path('../config_shared_examples', File.dirname(__FILE__))
+
+RSpec.shared_examples_for "ActiveAdmin::Localizers::ResourceLocalizer" do
+  it "should use proper translation" do
+    string = ActiveAdmin::Localizers::ResourceLocalizer.t(action, model: model, model_name: model_name)
+    expect(string).to eq translation
+  end
+
+  it "should accessible via ActiveAdmin::Localizers" do
+    resource = double(resource_label: model, resource_name: double(i18n_key: model_name))
+    localizer = ActiveAdmin::Localizers.resource(resource)
+    expect(localizer.t(action)).to eq translation
+  end
+end
 
 RSpec.describe ActiveAdmin::Localizers::ResourceLocalizer do
   let(:action) { 'new_model' }
