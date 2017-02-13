9639cfe766b8e836ce944894eb40b87d333fc1e5
David Rodríguez <deivid.rodriguez@gmail.com>
2017-02-13 11:39:28 -0200

    Make sure all tests have the necessary setup
    Fixes
    
    ```
    rspec ./spec/unit/resource_registration_spec.rb[1:1:2]
    ```

--- spec/unit/resource_registration_spec.rb
+++ spec/unit/resource_registration_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe "Registering an object to administer" do
   context "with no configuration" do
     let(:namespace) { ActiveAdmin::Namespace.new(application, :admin) }
 
-    it "should call register on the namespace" do
+    before do
       application.namespaces[namespace.name] = namespace
+    end
+
+    it "should call register on the namespace" do
       expect(namespace).to receive(:register)
 
       application.register Category
@@ -15,6 +18,7 @@ RSpec.describe "Registering an object to administer" do
 
     it "should dispatch a Resource::RegisterEvent" do
       expect(ActiveSupport::Notifications).to receive(:publish).with(ActiveAdmin::Resource::RegisterEvent, an_instance_of(ActiveAdmin::Resource))
+
       application.register Category
     end
   end
