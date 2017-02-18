aca21fb8fe228adcdff3a6a45b19754118c0b33c
David Rodríguez <deivid.rodriguez@gmail.com>
2017-02-18 07:43:39 -0200

    Remove unnecessary global scope constant

--- spec/unit/authorization/controller_authorization_spec.rb
+++ spec/unit/authorization/controller_authorization_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-Auth = ActiveAdmin::Authorization
 
 RSpec.describe Admin::PostsController, "Controller Authorization", type: :controller do
 
@@ -12,28 +11,33 @@ RSpec.describe Admin::PostsController, "Controller Authorization", type: :contro
   end
 
   it "should authorize the index action" do
-    expect(authorization).to receive(:authorized?).with(Auth::READ, Post).and_return true
+    expect(authorization).to receive(:authorized?).with(auth::READ, Post).and_return true
     get :index
     expect(response).to be_success
   end
 
   it "should authorize the new action" do
-    expect(authorization).to receive(:authorized?).with(Auth::CREATE, an_instance_of(Post)).and_return true
+    expect(authorization).to receive(:authorized?).with(auth::CREATE, an_instance_of(Post)).and_return true
     get :new
     expect(response).to be_success
   end
 
   it "should authorize the create action with the new resource" do
-    expect(authorization).to receive(:authorized?).with(Auth::CREATE, an_instance_of(Post)).and_return true
+    expect(authorization).to receive(:authorized?).with(auth::CREATE, an_instance_of(Post)).and_return true
     post :create
     expect(response).to redirect_to action: 'show', id: Post.last.id
   end
 
   it "should redirect when the user isn't authorized" do
-    expect(authorization).to receive(:authorized?).with(Auth::READ, Post).and_return false
+    expect(authorization).to receive(:authorized?).with(auth::READ, Post).and_return false
     get :index
     expect(response.body).to eq '<html><body>You are being <a href="http://test.host/admin">redirected</a>.</body></html>'
     expect(response).to redirect_to '/admin'
   end
 
+  private
+
+  def auth
+    ActiveAdmin::Authorization
+  end
 end
