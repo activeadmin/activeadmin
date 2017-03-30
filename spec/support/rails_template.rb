1808f29a79741d2a6fe080baee1e40dbc5430aa0
Piers Chambers <piers@varyonic.com>
2017-03-30 17:01:45 -0400

    As of Rails 5.1 digesting is done by Webpack and digest = false appears ignored.
    Enable asset digests.
    As of 5.1 digesting is done by Webpack and digest = false appears ignored.

--- spec/support/rails_template.rb
+++ spec/support/rails_template.rb
@@ -2,6 +2,10 @@
 
 copy_file File.expand_path('../templates/manifest.js', __FILE__), 'app/assets/config/manifest.js', force: true
 
+create_file 'app/assets/stylesheets/some-random-css.css'
+create_file 'app/assets/javascripts/some-random-js.js'
+create_file 'app/assets/images/a/favicon.ico'
+
 generate :model, 'post title:string body:text published_date:date author_id:integer ' +
   'position:integer custom_category_id:integer starred:boolean foo_id:integer'
 create_file 'app/models/post.rb', <<-RUBY.strip_heredoc, force: true
@@ -131,7 +135,7 @@ gsub_file 'config/environments/test.rb', /  config.cache_classes = true/, <<-RUB
 
   config.cache_classes = !ENV['CLASS_RELOADING']
   config.action_mailer.default_url_options = {host: 'example.com'}
-  config.assets.digest = false
+  config.assets.precompile += %w( some-random-css.css some-random-js.js a/favicon.ico )
 
   config.active_record.maintain_test_schema = false
 
