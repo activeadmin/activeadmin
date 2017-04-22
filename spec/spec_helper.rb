01cd0ebf15c55d427ca5f08e2d63d6a643a9a72a
David Rodríguez <deivid.rodriguez@gmail.com>
2017-04-22 13:40:27 -0300

    Only run simplecov when env var specified
    Since i18n-tasks specs are extremely slow when it's enabled.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 require 'simplecov'
 
-SimpleCov.start do
-  add_filter 'spec/'
-  add_filter 'features/'
+# @todo: Always run simplecov again once
+# https://github.com/colszowka/simplecov/issues/404,
+# https://github.com/glebm/i18n-tasks/issues/221 are fixed
+if ENV['COVERAGE'] == 'true'
+  SimpleCov.start do
+    add_filter 'spec/'
+    add_filter 'features/'
+  end
 end
 
 if ENV['CI'] == 'true'
