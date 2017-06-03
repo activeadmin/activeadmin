41b708852c2b8c99c4f5d37f91ea916c63eb36c4
Javier Julio <jjfutbol@gmail.com>
2017-06-03 18:18:25 -0400

    Remove inline separator

--- lib/active_admin/inputs/filters/date_range_input.rb
+++ lib/active_admin/inputs/filters/date_range_input.rb
@@ -8,7 +8,6 @@ module ActiveAdmin
           input_wrapping do
             [ label_html,
               builder.text_field(gt_input_name, input_html_options(gt_input_name)),
-              template.content_tag(:span, "-", class: "seperator"),
               builder.text_field(lt_input_name, input_html_options(lt_input_name)),
             ].join("\n").html_safe
           end
