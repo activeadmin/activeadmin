1e96b956b14e1e045b98c7a285f9e059b303acc3
Javier Julio <jjfutbol@gmail.com>
2017-06-03 18:22:35 -0400

    Use "starts/ends with" placeholders for date range fields
    This is all around a better choice as its descriptive what each field
    does in place not having a label like other filter fields do. We need
    to apply box-sizing: border-box on the input so they stay evenly side
    by side because the width we give it is the width we want it to have.
    Without that rule, the padding is in addition to the width (meaning 5px
    horizontal padding and 100px width, then the rendered width is 110px).

--- lib/active_admin/inputs/filters/date_range_input.rb
+++ lib/active_admin/inputs/filters/date_range_input.rb
@@ -7,8 +7,8 @@ module ActiveAdmin
         def to_html
           input_wrapping do
             [ label_html,
-              builder.text_field(gt_input_name, input_html_options(gt_input_name)),
-              builder.text_field(lt_input_name, input_html_options(lt_input_name)),
+              builder.text_field(gt_input_name, input_html_options(gt_input_name, gt_input_placeholder)),
+              builder.text_field(lt_input_name, input_html_options(lt_input_name, lt_input_placeholder)),
             ].join("\n").html_safe
           end
         end
@@ -22,7 +22,7 @@ module ActiveAdmin
           column && column.type == :date ? "#{method}_lteq" : "#{method}_lteq_datetime"
         end
 
-        def input_html_options(input_name = gt_input_name)
+        def input_html_options(input_name = gt_input_name, placeholder = gt_input_placeholder)
           current_value = begin
             #cast value to date object before rendering input
             @object.public_send(input_name).to_s.to_date
@@ -32,7 +32,16 @@ module ActiveAdmin
           { size: 12,
             class: "datepicker",
             maxlength: 10,
+            placeholder: placeholder,
             value: current_value ? current_value.strftime("%Y-%m-%d") : "" }
+        end
+
+        def gt_input_placeholder
+          I18n.t("active_admin.filters.predicates.starts_with")
+        end
+
+        def lt_input_placeholder
+          I18n.t("active_admin.filters.predicates.ends_with")
         end
       end
     end
