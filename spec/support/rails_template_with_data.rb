373b5bfe09694526ac7b0ff05f7b33a1c62a157f
Javier Julio <jjfutbol@gmail.com>
2017-06-05 22:26:45 -0400

    Add tag page and enable has many taggings
    Noticed this model existed but without an admin page. With taggings
    this makes it a great candidate for testing the has_many form without
    breaking any tests either.

--- spec/support/rails_template_with_data.rb
+++ spec/support/rails_template_with_data.rb
@@ -1,6 +1,6 @@
 apply File.expand_path("../rails_template.rb", __FILE__)
 
-%w{Post User Category}.each do |type|
+%w{Post User Category Tag}.each do |type|
   generate :'active_admin:resource', type
 end
 
@@ -20,7 +20,7 @@ RUBY
 
 inject_into_file 'app/admin/post.rb', <<-RUBY, after: "ActiveAdmin.register Post do\n"
 
-  permit_params [:custom_category_id, :author_id, :title, :body, :published_date, :position, :starred]
+  permit_params :custom_category_id, :author_id, :title, :body, :published_date, :position, :starred, taggings_attributes: [ :id, :tag_id, :name, :position, :_destroy ]
 
   scope :all, default: true
 
@@ -39,6 +39,78 @@ inject_into_file 'app/admin/post.rb', <<-RUBY, after: "ActiveAdmin.register Post
   scope :my_posts do |posts|
     posts.where(author_id: current_admin_user.id)
   end
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :published_date
+    column :author
+    column :category
+    column :starred
+    column :position
+    column :created_at
+    column :updated_at
+  end
+
+  show do |post|
+    attributes_table do
+      row :id
+      row :title
+      row :body
+    end
+
+    panel 'Tags' do
+      table_for(post.taggings.order(:position)) do
+        column :tag_id do |tagging|
+          link_to tagging.tag_id, admin_tag_path(tagging.tag)
+        end
+        column :tag_name
+        column :position
+        column :created_at
+        column :updated_at
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs 'Details' do
+      f.input :title
+      f.input :body
+      f.input :starred
+      f.input :author
+      f.input :position
+      f.input :published_date
+      f.input :author_id
+      f.input :custom_category_id
+      f.input :category
+    end
+    f.inputs "Tags" do
+      f.has_many :taggings, sortable: :position do |t|
+        t.input :tag
+        t.input :_destroy, as: :boolean
+      end
+    end
+    f.actions
+  end
+RUBY
+
+inject_into_file 'app/admin/tag.rb', <<-RUBY, after: "ActiveAdmin.register Tag do\n"
+
+  config.create_another = true
+
+  permit_params [:name]
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :created_at
+    actions dropdown: true do |tag|
+      item "Preview", admin_tag_path(tag)
+    end
+  end
+
 RUBY
 
 append_file "db/seeds.rb", "\n\n" + <<-RUBY.strip_heredoc
