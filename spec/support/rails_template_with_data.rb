d74f32a1fcbe7d758196f530c514f3f02f75d9ee
Javier Julio <jjfutbol@gmail.com>
2017-06-06 21:36:49 -0400

    Create a custom page and call it Kitchen Sink
    This is useful since it shows how to create a custom page that isn’t
    tied to a model but also as its name suggests, a dumping ground for
    various misc things we want to test out that don’t really fit easily
    elsewhere. Things like tabs and various basic HTML that we want to see
    it rendered so we can see how AA styles them.

--- spec/support/rails_template_with_data.rb
+++ spec/support/rails_template_with_data.rb
@@ -10,6 +10,72 @@ inject_into_file 'app/admin/admin_user.rb', <<-RUBY, after: "ActiveAdmin.registe
   menu parent: "Administrative", priority: 1
 RUBY
 
+generate :'active_admin:page', 'KitchenSink'
+
+gsub_file 'app/admin/kitchen_sink.rb', /^ *content.*?end/m, <<-RUBY
+
+  sidebar "Sample Sidebar" do
+    para "Sidebars can also be used on custom pages."
+    para do
+      a "Active Admin", href: "https://github.com/activeadmin/activeadmin"
+      text_node "is a Ruby on Rails framework for"
+      em "creating elegant backends"
+      text_node "for"
+      strong "website administration."
+    end
+    para do
+      abbr "HTML", title: "HyperText Markup Language"
+      text_node "is the most basic building block of the Web."
+    end
+  end
+
+  content do
+    columns do
+      column do
+        panel "Panel title" do
+          h1 "This is an h1"
+          h2 "This is an h2"
+          h3 "This is an h3"
+        end
+      end
+      column do
+        table_for User.all do
+          column :id
+          column :display_name
+          column :username
+          column :age
+          column :updated_at
+        end
+      end
+    end
+
+    tabs do
+      tab :first do
+        ul do
+          li "List item"
+          li "Another list item"
+          li "Last item"
+        end
+        ol do
+          li "First list item"
+          li "Second list item"
+          li "Third list item"
+        end
+      end
+      tab :second do
+        para "A popular quote."
+        blockquote do
+          text_node "&ldqou;Be yourself; everyone else is already taken.&rdqou;".html_safe
+          cite "― Oscar Wilde"
+        end
+      end
+      tab :third do
+        para "Third tab content."
+      end
+    end
+  end
+RUBY
+
 %w{Post User Category Tag}.each do |type|
   generate :'active_admin:resource', type
 end
