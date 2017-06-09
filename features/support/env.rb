e0286b95e1a65296c69b66a1a2a4f38c42638500
David Rodríguez <deivid.rodriguez@riseup.net>
2017-06-09 18:01:14 -0300

    Configure simplecov in the standard place

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,7 @@
 
 ENV['RAILS_ENV'] = 'test'
 
-require File.expand_path('../../../spec/spec_helper', __FILE__)
+require 'simplecov'
 
 Dir["#{File.expand_path('../../step_definitions', __FILE__)}/*.rb"].each do |f|
   require f
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,1 @@
 require 'simplecov'
-
-# @todo: Always run simplecov again once
-# https://github.com/colszowka/simplecov/issues/404,
-# https://github.com/glebm/i18n-tasks/issues/221 are fixed
-if ENV['COVERAGE'] == 'true'
-  SimpleCov.start do
-    add_filter 'spec/'
-    add_filter 'features/'
-  end
-end
-
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
