49fdf19cee18a85fc16d2eede65d1a3f31470fe9
Benjamin Cavileer <bcavileer@gmail.com>
2017-06-26 20:09:51 -0400

    Render meta tags before javascripts so they are available when scripts load.

--- lib/active_admin/views/pages/base.rb
+++ lib/active_admin/views/pages/base.rb
@@ -29,16 +29,16 @@ module ActiveAdmin
               text_node stylesheet_link_tag(style, options).html_safe
             end
 
+            active_admin_namespace.meta_tags.each do |name, content|
+              text_node(tag(:meta, name: name, content: content))
+            end
+
             active_admin_application.javascripts.each do |path|
               text_node(javascript_include_tag(path))
             end
 
             if active_admin_namespace.favicon
               text_node(favicon_link_tag(active_admin_namespace.favicon))
-            end
-
-            active_admin_namespace.meta_tags.each do |name, content|
-              text_node(tag(:meta, name: name, content: content))
             end
 
             text_node csrf_meta_tag
