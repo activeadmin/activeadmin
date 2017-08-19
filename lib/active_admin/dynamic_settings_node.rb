020d477a00613e5f8b2cb06514a2a7a148c73f4a
Piers Chambers <piers@varyonic.com>
2017-08-19 14:09:47 -0400

    Add a StringSymbolOrProcSetting inspired by MethodProcHelper.render_or_call_method_or_proc_on

--- lib/active_admin/dynamic_settings_node.rb
+++ lib/active_admin/dynamic_settings_node.rb
@@ -5,22 +5,22 @@ module ActiveAdmin
 
   class DynamicSettingsNode < SettingsNode
     class << self
-      def register(name, value)
+      def register(name, value, type = nil)
         class_attribute "#{name}_setting"
         add_reader(name)
-        add_writer(name)
+        add_writer(name, type)
         send "#{name}=", value
       end
 
       def add_reader(name)
-        define_singleton_method(name) do
-          send("#{name}_setting").value
+        define_singleton_method(name) do |*args|
+          send("#{name}_setting").value(*args)
         end
       end
 
-      def add_writer(name)
+      def add_writer(name, type)
         define_singleton_method("#{name}=") do |value|
-          send("#{name}_setting=", DynamicSetting.build(value))
+          send("#{name}_setting=", DynamicSetting.build(value, type))
         end
       end
     end
