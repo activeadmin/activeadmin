ca8c97f1dca9d8d913bd262499d8e958276242c8
Piers Chambers <piers@varyonic.com>
2017-10-15 17:23:22 -0400

    Simplify header view.

--- lib/active_admin/views/header.rb
+++ lib/active_admin/views/header.rb
@@ -9,20 +9,8 @@ module ActiveAdmin
         @menu = menu
         @utility_menu = @namespace.fetch_menu(:utility_navigation)
 
-        build_site_title
-        build_global_navigation
-        build_utility_navigation
-      end
-
-      def build_site_title
         site_title @namespace
-      end
-
-      def build_global_navigation
         global_navigation @menu, class: 'header-item tabs'
-      end
-
-      def build_utility_navigation
         utility_navigation @utility_menu, id: "utility_nav", class: 'header-item tabs'
       end
 
