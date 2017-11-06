4ef4ed7c96bed789e9bf1525615f0319ecb826cd
Piers Chambers <piers@varyonic.com>
2017-11-06 18:41:14 -0500

    Merge pull request #5196 from varyonic/refactor-has-many
    Refactor has many, extracting HasManyBuilder

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -28,72 +28,93 @@ module ActiveAdmin
 
     attr_accessor :already_in_an_inputs_block
 
-    def assoc_heading(assoc)
-      object.class.reflect_on_association(assoc).klass.model_name.
-        human(count: ::ActiveAdmin::Helpers::I18n::PLURAL_MANY_COUNT)
-    end
-
     def has_many(assoc, options = {}, &block)
-      # remove options that should not render as attributes
-      custom_settings = :new_record, :allow_destroy, :heading, :sortable, :sortable_start
-      builder_options = {new_record: true}.merge! options.slice  *custom_settings
-      options         = {for: assoc      }.merge! options.except *custom_settings
-      options[:class] = [options[:class], "inputs has_many_fields"].compact.join(' ')
-      sortable_column = builder_options[:sortable]
-      sortable_start  = builder_options.fetch(:sortable_start, 0)
+      HasManyBuilder.new(self, assoc, options).render(&block)
+    end
+  end
+
+  # Decorates a FormBuilder with the additional attributes and methods
+  # to build a has_many block.  Nested has_many blocks are handled by
+  # nested decorators.
+  class HasManyBuilder < SimpleDelegator
+    attr_reader :assoc
+    attr_reader :options
+    attr_reader :heading, :sortable_column, :sortable_start
+    attr_reader :new_record, :destroy_option
+
+    def initialize(has_many_form, assoc, options)
+      super has_many_form
+      @assoc = assoc
+      @options = extract_custom_settings!(options.dup)
+      @options.reverse_merge!(for: assoc)
+      @options[:class] = [options[:class], "inputs has_many_fields"].compact.join(' ')
 
       if sortable_column
-        options[:for] = [assoc, sorted_children(assoc, sortable_column)]
+        @options[:for] = [assoc, sorted_children(sortable_column)]
       end
+    end
 
+    def render(&block)
       html = "".html_safe
-      unless builder_options.key?(:heading) && !builder_options[:heading]
-        html << template.content_tag(:h3) do
-          builder_options[:heading] || assoc_heading(assoc)
-        end
-      end
-
-      html << template.capture do
-        form_block = proc do |has_many_form|
-          index    = parent_child_index options[:parent] if options[:parent]
-          block_contents = template.capture do
-            block.call(has_many_form, index)
-          end
-          template.concat(block_contents)
-          template.concat has_many_actions(has_many_form, builder_options, "".html_safe)
-        end
-
-        template.assigns[:has_many_block] = true
-        contents = without_wrapper { inputs(options, &form_block) } || "".html_safe
-
-        if builder_options[:new_record]
-          contents << js_for_has_many(assoc, form_block, template, builder_options[:new_record], options[:class])
-        else
-          contents
-        end
-      end
-
-      tag = @already_in_an_inputs_block ? :li : :div
-      html = template.content_tag(tag, html, class: "has_many_container #{assoc}", 'data-sortable' => sortable_column, 'data-sortable-start' => sortable_start)
-      template.concat(html) if template.output_buffer
-      html
+      html << template.content_tag(:h3) { heading } if heading.present?
+      html << template.capture { content_has_many(&block) }
+      wrap_div_or_li(html)
     end
 
     protected
 
-    def has_many_actions(has_many_form, builder_options, contents)
-      if has_many_form.object.new_record?
+    # remove options that should not render as attributes
+    def extract_custom_settings!(options)
+      @heading = options.key?(:heading) ? options.delete(:heading) : default_heading
+      @sortable_column = options.delete(:sortable)
+      @sortable_start  = options.delete(:sortable_start) || 0
+      @new_record = options.key?(:new_record) ? options.delete(:new_record) : true
+      @destroy_option = options.delete(:allow_destroy)
+      options
+    end
+
+    def default_heading
+      assoc_klass.model_name.
+        human(count: ::ActiveAdmin::Helpers::I18n::PLURAL_MANY_COUNT)
+    end
+
+    def assoc_klass
+      @assoc_klass ||= __getobj__.object.class.reflect_on_association(assoc).klass
+    end
+
+    def content_has_many(&block)
+      form_block = proc do |form_builder|
+        render_has_many_form(form_builder, options[:parent], &block)
+      end
+
+      template.assigns[:has_many_block] = true
+      contents = without_wrapper { inputs(options, &form_block) }
+      contents ||= "".html_safe
+
+      js = new_record ? js_for_has_many(options[:class], &form_block) : ''
+      contents << js
+    end
+
+    # Renders the Formtastic inputs then appends ActiveAdmin delete and sort actions.
+    def render_has_many_form(form_builder, parent, &block)
+      index = parent && form_builder.send(:parent_child_index, parent)
+      template.concat template.capture { yield(form_builder, index) }
+      template.concat has_many_actions(form_builder, "".html_safe)
+    end
+
+    def has_many_actions(form_builder, contents)
+      if form_builder.object.new_record?
         contents << template.content_tag(:li) do
           template.link_to I18n.t('active_admin.has_many_remove'), "#", class: 'button has_many_remove'
         end
-      elsif allow_destroy?(has_many_form.object, builder_options[:allow_destroy])
-        has_many_form.input(:_destroy, as: :boolean,
+      elsif allow_destroy?(form_builder.object)
+        form_builder.input(:_destroy, as: :boolean,
                             wrapper_html: {class: 'has_many_delete'},
                             label: I18n.t('active_admin.has_many_delete'))
       end
 
-      if builder_options[:sortable]
-        has_many_form.input builder_options[:sortable], as: :hidden
+      if sortable_column
+        form_builder.input sortable_column, as: :hidden
 
         contents << template.content_tag(:li, class: 'handle') do
           I18n.t('active_admin.move')
@@ -103,7 +124,7 @@ module ActiveAdmin
       contents
     end
 
-    def allow_destroy?(form_object, destroy_option)
+    def allow_destroy?(form_object)
       !! case destroy_option
          when Symbol, String
            form_object.public_send destroy_option
@@ -114,41 +135,46 @@ module ActiveAdmin
          end
     end
 
-    def sorted_children(assoc, column)
-      object.public_send(assoc).sort_by do |o|
+    def sorted_children(column)
+      __getobj__.object.public_send(assoc).sort_by do |o|
         attribute = o.public_send column
         [attribute.nil? ? Float::INFINITY : attribute, o.id || Float::INFINITY]
       end
     end
 
-    private
-
     def without_wrapper
-      is_being_wrapped = @already_in_an_inputs_block
-      @already_in_an_inputs_block = false
+      is_being_wrapped = already_in_an_inputs_block
+      self.already_in_an_inputs_block = false
 
       html = yield
 
-      @already_in_an_inputs_block = is_being_wrapped
+      self.already_in_an_inputs_block = is_being_wrapped
       html
     end
 
     # Capture the ADD JS
-    def js_for_has_many(assoc, form_block, template, new_record, class_string)
-      assoc_reflection = object.class.reflect_on_association assoc
-      assoc_name       = assoc_reflection.klass.model_name
+    def js_for_has_many(class_string, &form_block)
+      assoc_name       = assoc_klass.model_name
       placeholder      = "NEW_#{assoc_name.to_s.underscore.upcase.gsub(/\//, '_')}_RECORD"
       opts = {
-        for: [assoc, assoc_reflection.klass.new],
+        for: [assoc, assoc_klass.new],
         class: class_string,
         for_options: { child_index: placeholder }
       }
-      html = template.capture{ inputs_for_nested_attributes opts, &form_block }
+      html = template.capture{ __getobj__.send(:inputs_for_nested_attributes, opts, &form_block) }
       text = new_record.is_a?(String) ? new_record : I18n.t('active_admin.has_many_new', model: assoc_name.human)
 
       template.link_to text, '#', class: "button has_many_add", data: {
         html: CGI.escapeHTML(html).html_safe, placeholder: placeholder
       }
+    end
+
+    def wrap_div_or_li(html)
+      template.content_tag(already_in_an_inputs_block ? :li : :div,
+                           html,
+                           class: "has_many_container #{assoc}",
+                           'data-sortable' => sortable_column,
+                           'data-sortable-start' => sortable_start)
     end
   end
 end
