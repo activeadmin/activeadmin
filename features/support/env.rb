1177d9ca2dd3ec3858c40b944e720edee198f643
David Rodríguez <deivid.rodriguez@riseup.net>
2017-12-07 13:06:50 -0300

    Enable logging

--- features/support/env.rb
+++ features/support/env.rb
@@ -105,10 +105,6 @@ end
 # Force deprecations to raise an exception.
 ActiveSupport::Deprecation.behavior = :raise
 
-# improve the performance of the specs suite by not logging anything
-# see http://blog.plataformatec.com.br/2011/12/three-tips-to-improve-the-performance-of-your-test-suite/
-Rails.logger.level = 4
-
 Around '@silent_unpermitted_params_failure' do |scenario, block|
   original = ActionController::Parameters.action_on_unpermitted_parameters
   ActionController::Parameters.action_on_unpermitted_parameters = false
