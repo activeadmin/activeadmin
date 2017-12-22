66950fed7436f5882f9437b70b1af9de4d054fae
Nikita Sokolov <faucct@gmail.com>
2017-12-22 13:39:06 +0300

    Backport pull request #5299 from SPBTV/bugfix/resource_filters_xss
    Adapt unit tests for copied helper
    DisplayHelper should not sanitize its input
    Filters sidebar should not be vulnerable to XSS

--- lib/active_admin/views/components/sidebar_section.rb
+++ lib/active_admin/views/components/sidebar_section.rb
@@ -1,8 +1,11 @@
+require 'active_admin/helpers/output_safety_helper'
+
 module ActiveAdmin
   module Views
 
     class SidebarSection < Panel
       builder_method :sidebar_section
+      include OutputSafetyHelper
 
       # Takes a ActiveAdmin::SidebarSection instance
       def build(section)
