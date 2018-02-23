f4bbc4c4c008169aaf54a03e1ad7f01109f13ab2
Leonardo Diez <leiodd@gmail.com>
2018-02-23 16:27:48 +0100

    Added scopes groups to sample application.

--- spec/support/rails_template_with_data.rb
+++ spec/support/rails_template_with_data.rb
@@ -143,19 +143,19 @@ inject_into_file 'app/admin/posts.rb', <<-'RUBY', after: "ActiveAdmin.register P
 
   scope :all, default: true
 
-  scope :drafts do |posts|
+  scope :drafts, group: :status do |posts|
     posts.where(["published_date IS NULL"])
   end
 
-  scope :scheduled do |posts|
+  scope :scheduled, group: :status do |posts|
     posts.where(["posts.published_date IS NOT NULL AND posts.published_date > ?", Time.now.utc])
   end
 
-  scope :published do |posts|
+  scope :published, group: :status do |posts|
     posts.where(["posts.published_date IS NOT NULL AND posts.published_date < ?", Time.now.utc])
   end
 
-  scope :my_posts do |posts|
+  scope :my_posts, group: :author do |posts|
     posts.where(author_id: current_admin_user.id)
   end
 
