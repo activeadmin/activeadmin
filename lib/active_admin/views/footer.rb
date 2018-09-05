5fc21bbaab53e6452fd99beb19fd28d9ac8f939f
YumaInaura <yuma.inaura@gmail.com>
2018-09-05 13:49:37 +0900

    Update footer powered by link http to https

--- lib/active_admin/views/footer.rb
+++ lib/active_admin/views/footer.rb
@@ -21,7 +21,7 @@ module ActiveAdmin
 
       def powered_by_message
         I18n.t('active_admin.powered_by',
-          active_admin: link_to("Active Admin", "http://www.activeadmin.info"),
+          active_admin: link_to("Active Admin", "https://activeadmin.info"),
           version: ActiveAdmin::VERSION).html_safe
       end
 
