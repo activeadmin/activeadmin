cb52eed65eebb128d3bc589d8f65fb6637253d94
David Rodríguez <deivid.rodriguez@riseup.net>
2018-09-28 20:20:41 -0300

    Bump capybara to latest version

--- features/support/env.rb
+++ features/support/env.rb
@@ -62,6 +62,8 @@ end
 
 Capybara.javascript_driver = :chrome
 
+Capybara.server = :puma, { Silent: true }
+
 # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
