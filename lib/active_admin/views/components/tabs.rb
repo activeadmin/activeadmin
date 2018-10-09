0c0d89a337157c8167da92bddaa866f4abe5b98e
omitter <17044127+omitter@users.noreply.github.com>
2018-10-09 11:21:17 +0900

    Tab ids will use non-ascii characters if title is non-ascii only
    Also an error is raised if the id is blank

--- lib/active_admin/views/components/tabs.rb
+++ lib/active_admin/views/components/tabs.rb
@@ -16,6 +16,8 @@ module ActiveAdmin
 
       def build_menu_item(title, options, &block)
         fragment = options.fetch(:id, title.parameterize)
+        fragment = title.gsub(/[[:space:]]/, "") if fragment.blank?
+        raise "The tab title is not transliterable into an ascii only id that can be used as a url fragment for this tab. Either manually specify a tab id, or add the proper transliteration rules to your application." if fragment.blank?
         html_options = options.fetch(:html_options, {})
         li html_options do
           link_to title, "##{fragment}"
