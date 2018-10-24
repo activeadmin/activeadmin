64b30319e72573a20a283a80a43ee30bbaaef817
David Rodríguez <deivid.rodriguez@riseup.net>
2018-10-24 12:57:45 -0300

    Improve changelog specs
    Using the Kramdown parser warnings includes warnings when implicit links
    have no definition, and it prints a better error, actually. Also, it
    probably warns about other stuff that can be interesting to know (and
    fix).

--- spec/changelog_spec.lint.rb
+++ spec/changelog_spec.lint.rb
@@ -1,16 +1,11 @@
 SimpleCov.command_name "lint" if ENV["COVERAGE"] == "true"
 
+require "kramdown"
+
 RSpec.describe "Changelog" do
   subject(:changelog) do
     path = File.join(File.dirname(__dir__), "CHANGELOG.md")
     File.read(path)
-  end
-
-  it 'has definitions for all implicit links' do
-    implicit_link_names = changelog.scan(/\[([^\\\[:]+)\][^(]/).flatten.uniq
-    implicit_link_names.each do |name|
-      expect(changelog).to include("[#{name}]: https")
-    end
   end
 
   it 'uses the simplest style for implicit links' do
@@ -41,5 +36,11 @@ RSpec.describe "Changelog" do
         expect(entry).not_to match(/\.$/)
       end
     end
+  end
+
+  describe 'warnings' do
+    subject(:document) { Kramdown::Document.new(changelog) }
+
+    specify { expect(document.warnings).to be_empty }
   end
 end
