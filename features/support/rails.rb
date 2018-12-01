aff143759ab3237a8e3cb3a3a07b6b20bc3bc999
David Rodríguez <deivid.rodriguez@riseup.net>
2018-12-01 09:20:49 +0100

    Move cucumber rails setup to a separate file

---
+++ features/support/rails.rb
@@ -0,0 +1,8 @@
+require 'cucumber/rails/application'
+require 'cucumber/rails/action_controller'
+require 'cucumber/rails/world'
+require 'cucumber/rails/hooks'
+require 'cucumber/rails/capybara'
+require 'cucumber/rails/database'
+
+MultiTest.disable_autorun
