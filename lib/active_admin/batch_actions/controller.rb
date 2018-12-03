40927ae1384edaacc339cb8b6c58bd84d3bd58f4
David Rodríguez <deivid.rodriguez@riseup.net>
2018-12-03 01:26:46 +0100

    Use `render_in_context` for batch action `form`

--- lib/active_admin/batch_actions/controller.rb
+++ lib/active_admin/batch_actions/controller.rb
@@ -7,7 +7,7 @@ module ActiveAdmin
         if action_present?
           selection  =            params[:collection_selection] || []
           inputs     = JSON.parse params[:batch_action_inputs]  || '{}'
-          valid_keys = MethodOrProcHelper.call_method_or_exec_proc(current_batch_action.inputs).try(:keys)
+          valid_keys = MethodOrProcHelper.render_in_context(self, current_batch_action.inputs).try(:keys)
           inputs     = inputs.with_indifferent_access.slice *valid_keys
           instance_exec selection, inputs, &current_batch_action.block
         else
