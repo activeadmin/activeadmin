f933c3366c6f64b27e97ee45f8491d937162161e
David Rodríguez <deivid.rodriguez@riseup.net>
2019-03-12 10:43:03 +0100

    Restore removed cucumber steps
    This had been causing a bunch of features to be undefined.

--- features/step_definitions/tab_steps.rb
+++ features/step_definitions/tab_steps.rb
@@ -1,3 +1,7 @@
+Then(/^the "([^"]*)" tab should be selected$/) do |name|
+  step %{I should see "#{name}" within "ul#tabs li.current"}
+end
+
 Then("I should see tabs:") do |table|
   table.rows.each do |title, _|
     step %{I should see "#{title}" within "#main_content .tabs .nav"}
