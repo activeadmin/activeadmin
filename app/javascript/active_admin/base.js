1b95e5517ac1483b29b9483c542a6b1110aba5b4
sgara <stephane@garagnani.fr>
2019-09-27 11:21:36 +0200

    Add rollup config

--- app/javascript/active_admin/base.js
+++ app/javascript/active_admin/base.js
@@ -1,23 +1,24 @@
-//= require jquery3
-//= require jquery-ui/widgets/datepicker
-//= require jquery-ui/widgets/dialog
-//= require jquery-ui/widgets/sortable
-//= require jquery-ui/widgets/tabs
-//= require jquery-ui/widget
-//= require jquery_ujs
-//= require_self
-//= require ./ext/jquery
-//= require ./ext/jquery-ui
-//= require ./lib/active_admin
-//= require ./lib/batch_actions
-//= require ./lib/dropdown-menu
-//= require ./lib/has_many
-//= require ./lib/modal_dialog
-//= require ./lib/per_page
-//= require ./lib/checkbox-toggler
-//= require ./lib/table-checkbox-toggler
-//= require ./initializers/datepicker
-//= require ./initializers/filters
-//= require ./initializers/tabs
+import "jquery"
+import "jquery-ui/ui/widgets/datepicker"
+import "jquery-ui/ui/widgets/dialog"
+import "jquery-ui/ui/widgets/sortable"
+import "jquery-ui/ui/widgets/tabs"
+import "jquery-ui/ui/widget"
+import "jquery-ujs"
 
-window.ActiveAdmin = {}
+import "./ext/jquery"
+import "./ext/jquery-ui"
+import "./lib/active_admin"
+import "./lib/batch_actions"
+import "./lib/checkbox-toggler"
+import "./lib/dropdown-menu"
+import "./lib/has_many"
+import "./lib/modal_dialog"
+import "./lib/per_page"
+import "./lib/table-checkbox-toggler"
+import "./initializers/datepicker"
+import "./initializers/filters"
+import "./initializers/tabs"
+
+
+window.ActiveAdmin = {};
