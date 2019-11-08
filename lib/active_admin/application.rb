663990d9de39a2b92e90d1a76ac143b4d6969af1
Weston Ganger <weston@westonganger.com>
2019-11-07 17:28:56 -0800

    Convert namespace to sym to prevent duplicate namespaces

--- lib/active_admin/application.rb
+++ lib/active_admin/application.rb
@@ -73,7 +73,7 @@ module ActiveAdmin
     def namespace(name)
       name ||= :root
 
-      namespace = namespaces[name] ||= begin
+      namespace = namespaces[name.to_sym] ||= begin
         namespace = Namespace.new(self, name)
         ActiveSupport::Notifications.publish ActiveAdmin::Namespace::RegisterEvent, namespace
         namespace
