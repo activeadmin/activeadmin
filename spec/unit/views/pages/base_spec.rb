86e5aa3d781da70d644f25cf267194b16e95254d
Michal Kovac <miso@github.ksp.sk>
2019-11-29 14:47:34 +0100

    Display multiple flash messages in separate elements (#5929)
    closes #5279

--- spec/unit/views/pages/base_spec.rb
+++ spec/unit/views/pages/base_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe ActiveAdmin::Views::Pages::Base do
   it "defines default main content" do
     expect(NewPage.new.main_content).to eq 'Please implement NewPage#main_content to display content.'
   end
+
+  describe "build_flash_messages" do
+    class PageWithFlashMessages < ActiveAdmin::Views::Pages::Base
+      def flash_messages
+        { alert: 'Alert message', notice: ['First notice message', 'Second notice message'] }
+      end
+    end
+
+    it "shows all flash messages" do
+      div = PageWithFlashMessages.new.send :build_flash_messages
+      expect(div.get_elements_by_tag_name('div').map(&:content)).to contain_exactly('Alert message', 'First notice message', 'Second notice message')
+    end
+  end
 end
