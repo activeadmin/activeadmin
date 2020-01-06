fdedfa3e9caded166165008b5ee8940477a19b23
Kevin Jones <kevin@vcsjones.com>
2020-01-06 10:12:53 -0500

    Use double-splat where appropriate as suggested

--- lib/active_admin/localizers/resource_localizer.rb
+++ lib/active_admin/localizers/resource_localizer.rb
@@ -21,7 +21,7 @@ module ActiveAdmin
         scope = options.delete(:scope)
         specific_key = array_to_key('resources', @model_name, scope, key)
         defaults = [array_to_key(scope, key), key.to_s.titleize]
-        ::I18n.t specific_key, options.reverse_merge(model: @model, default: defaults, scope: 'active_admin')
+        ::I18n.t specific_key, **options.reverse_merge(model: @model, default: defaults, scope: 'active_admin')
       end
       alias_method :t, :translate
 
