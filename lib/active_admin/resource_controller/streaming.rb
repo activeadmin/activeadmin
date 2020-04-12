7a741f89573a2a97c1eecbdedcb28619b13da8db
bhargav <bhargavdasari13@gmail.com>
2020-04-12 10:10:14 -0700

    Update streaming.rb
    Following error I am getting
    ArgumentError - wrong number of arguments (given 1, expected 0):
       () Users/bhargav/.rvm/gems/ruby-2.4.4/bundler/gems/activeadmin-85ab3f5961e1/lib/active_admin/resource_controller/streaming.rb:11:in `index'

--- lib/active_admin/resource_controller/streaming.rb
+++ lib/active_admin/resource_controller/streaming.rb
@@ -8,7 +8,7 @@ module ActiveAdmin
     #
     module Streaming
 
-      def index
+      def index(arg=nil)
         super do |format|
           format.csv { stream_csv }
           yield(format) if block_given?
