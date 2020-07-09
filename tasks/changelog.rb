7c8a4c275d7908a115ddfc950ab7f7d993ed00df
David Rodríguez <deivid.rodriguez@riseup.net>
2020-07-09 13:03:39 +0200

    Extract a couple of helper methods for later reuse

--- tasks/changelog.rb
+++ tasks/changelog.rb
@@ -1,12 +1,17 @@
 class Changelog
   def cut_version(header)
-    old_content = File.read(changelog_file).split("\n")
-    new_content = [old_content[0..3], header, "", old_content[4..-1]].join("\n") + "\n"
-
-    File.write(changelog_file, new_content)
+    replace_with_lines([content[0..3], header, "", content[4..-1]])
   end
 
   private
+
+  def replace_with_lines(new_lines)
+    File.write(changelog_file, new_lines.join("\n") + "\n")
+  end
+
+  def content
+    File.read(changelog_file).split("\n")
+  end
 
   def changelog_file
     "CHANGELOG.md"
