9da0e5db8452d80e0a7947b6da2a0d57cf491068
Daniel Rice <daniel.rice@ldstudios.co>
2020-08-02 19:31:20 -0600

    Remove whitespace #linter

--- spec/unit/generators/install_spec.rb
+++ spec/unit/generators/install_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe "AA installation" do
   context "should create" do
     let(:asset_pipeline_stylesheet_dir) { Rails.root + 'app/assets/stylesheets' }
     let(:asset_pipeline_javascript_dir) { Rails.root + 'app/assets/javascripts' }
-    
+
     let(:webpacker_stylesheet_dir) { Rails.root + 'app/javascript/stylesheets' }
     let(:webpacker_javascript_dir) { Rails.root + 'app/javascript/packs' }
 
     let(:stylesheet_dir) { ActiveAdmin.application.use_webpacker ? webpacker_stylesheet_dir : asset_pipeline_stylesheet_dir }
     let(:javascript_dir) { ActiveAdmin.application.use_webpacker ? webpacker_javascript_dir : asset_pipeline_javascript_dir }
-    
+
     it 'active_admin.scss' do
       path = stylesheet_dir + 'active_admin.scss'
 
