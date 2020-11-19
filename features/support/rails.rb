a09a1a4d05d703019b3e0c104e20a2184218c40e
David Rodríguez <deivid.rodriguez@riseup.net>
2020-11-19 17:10:26 +0100

    Bump cucumber-rails to 2.2.0 (#6547)
    Co-authored-by: dependabot-preview[bot] <27856297+dependabot-preview[bot]@users.noreply.github.com>

--- features/support/rails.rb
+++ features/support/rails.rb
@@ -3,6 +3,10 @@ require "cucumber/rails/action_dispatch"
 require "cucumber/rails/world"
 require "cucumber/rails/hooks"
 require "cucumber/rails/capybara"
+require "cucumber/rails/database/strategy"
+require "cucumber/rails/database/deletion_strategy"
+require "cucumber/rails/database/shared_connection_strategy"
+require "cucumber/rails/database/truncation_strategy"
 require "cucumber/rails/database"
 
 MultiTest.disable_autorun
