74a5b73c0128796fe502e51d1d0e062f3798d04d
Christian Schmidt <github@chsc.dk>
2021-06-26 00:35:37 +0200

    Use type=number for filters

--- lib/active_admin/inputs/filters/numeric_input.rb
+++ lib/active_admin/inputs/filters/numeric_input.rb
@@ -7,6 +7,10 @@ module ActiveAdmin
         include Base::SearchMethodSelect
 
         filter :equals, :greater_than, :less_than
+
+        def input_html
+          builder.number_field current_filter, input_html_options
+        end
       end
     end
   end
