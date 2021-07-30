24174b48581504068a71f32a916d71b71d8285e4
Stéphane Hanser <shanser@captive.fr>
2021-07-30 11:12:43 +0200

    Displays a action_item only if the user is authorized
    Co-authored-by: Marine Dominé <marine@captive.fr>

--- lib/active_admin/resource/action_items.rb
+++ lib/active_admin/resource/action_items.rb
@@ -64,32 +64,29 @@ module ActiveAdmin
 
       # Adds the default New link on index
       def add_default_new_action_item
-        add_action_item :new, only: :index do
-          if controller.action_methods.include?("new") && authorized?(ActiveAdmin::Auth::CREATE, active_admin_config.resource_class)
-            localizer = ActiveAdmin::Localizers.resource(active_admin_config)
-            link_to localizer.t(:new_model), new_resource_path
-          end
+        displayable = -> { controller.action_methods.include?("new") && authorized?(ActiveAdmin::Auth::CREATE, active_admin_config.resource_class) }
+        add_action_item :new, only: :index, if: displayable do
+          localizer = ActiveAdmin::Localizers.resource(active_admin_config)
+          link_to localizer.t(:new_model), new_resource_path
         end
       end
 
       # Adds the default Edit link on show
       def add_default_edit_action_item
-        add_action_item :edit, only: :show do
-          if controller.action_methods.include?("edit") && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-            localizer = ActiveAdmin::Localizers.resource(active_admin_config)
-            link_to localizer.t(:edit_model), edit_resource_path(resource)
-          end
+        displayable = -> { controller.action_methods.include?("edit") && authorized?(ActiveAdmin::Auth::UPDATE, resource) }
+        add_action_item :edit, only: :show, if: displayable do
+          localizer = ActiveAdmin::Localizers.resource(active_admin_config)
+          link_to localizer.t(:edit_model), edit_resource_path(resource)
         end
       end
 
       # Adds the default Destroy link on show
       def add_default_show_action_item
-        add_action_item :destroy, only: :show do
-          if controller.action_methods.include?("destroy") && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-            localizer = ActiveAdmin::Localizers.resource(active_admin_config)
-            link_to localizer.t(:delete_model), resource_path(resource), method: :delete,
-                                                                         data: { confirm: localizer.t(:delete_confirmation) }
-          end
+        displayable = -> { controller.action_methods.include?("destroy") && authorized?(ActiveAdmin::Auth::DESTROY, resource) }
+        add_action_item :destroy, only: :show, if: displayable do
+          localizer = ActiveAdmin::Localizers.resource(active_admin_config)
+          link_to localizer.t(:delete_model), resource_path(resource), method: :delete,
+                                                                       data: { confirm: localizer.t(:delete_confirmation) }
         end
       end
 
