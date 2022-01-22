3913277637c2abe49d0c75a92166c357a447312b
Alejandro Perea <alejandro.perea.fdez@gmail.com>
2022-01-22 10:19:50 +0100

    Remove unused spec templates pundit policies

--- spec/support/templates/policies/post_policy.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class PostPolicy < ApplicationPolicy
-  def update?
-    record.author == user
-  end
-end
--- spec/support/templates/policies/store_policy.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-class StorePolicy < ApplicationPolicy
-end
