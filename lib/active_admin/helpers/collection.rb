116e621985fa38f75041463bac8a11758f099294
Allison Phillips <allison@atebitbyte.com>
2022-05-13 19:43:19 -0400

    Collection Helper should not make redundant COUNT queries.

--- lib/active_admin/helpers/collection.rb
+++ lib/active_admin/helpers/collection.rb
@@ -4,13 +4,13 @@ module ActiveAdmin
     module Collection
       # 1. removes `select` and `order` to prevent invalid SQL
       # 2. correctly handles the Hash returned when `group by` is used
+      # 3. Avoids duplicative COUNT/subquery_for_count queries
       def collection_size(c = collection)
-        return c.count if c.is_a?(Array)
-        return c.length if c.limit_value
+        # 'length' avoids count query on unloaded collection with a limit_value
+        return c.length if c.is_a?(Array) || c.limit_value.present?
 
         c = c.except :select, :order
-
-        c.group_values.present? ? c.count.count : c.count
+        c.group_values.present? ? c.size.size : c.size
       end
 
       def collection_is_empty?(c = collection)
