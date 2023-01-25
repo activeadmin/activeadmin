91535586f195288dba76e63b960f4f69fb3579d2
harunkumars <harun@betacraft.io>
2023-01-25 18:10:45 +0530

    use old enum syntax to get Rails 6 test suite to pass

--- spec/support/templates/models/store.rb
+++ spec/support/templates/models/store.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class Store < ApplicationRecord
-  enum :review_status, [:pending, :approved, :rejected]
+  enum review_status: [:pending, :approved, :rejected]
   enum reject_reason: {
     not_a_good_fit: 0,
     other: 10
