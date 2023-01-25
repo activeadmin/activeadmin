65f79b599d71cf029fff3d3ceecdee970053d4b2
harunkumars <harun@betacraft.io>
2023-01-25 12:33:02 +0530

    add specs to ensure enum labels are returned in active filter

--- spec/support/templates/admin/stores.rb
+++ spec/support/templates/admin/stores.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 ActiveAdmin.register Store do
-  permit_params :name
+  permit_params :name, :review_status, :reject_reason
 
   index pagination_total: false
 end
--- spec/support/templates/models/store.rb
+++ spec/support/templates/models/store.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 class Store < ApplicationRecord
+  enum :review_status, [:pending, :approved, :rejected]
+  enum reject_reason: {
+    not_a_good_fit: 0,
+    other: 10
+  }
 end
