c7b003e58e7b6bf3c10d300551cbb286ba28ac04
codisart <louis.celier@gmail.com>
2023-02-28 13:46:34 +0100

    Add whitespace to rerun lint ci

--- lib/active_admin/inputs/rich_text_area_input.rb
+++ lib/active_admin/inputs/rich_text_area_input.rb
@@ -5,6 +5,7 @@ module ActiveAdmin
       def to_html
         input_wrapping do
           editor_tag = builder.rich_text_area(method, input_html_options)
+          
           editor = template.content_tag('div', editor_tag, class: 'trix-editor-wrapper')
           label_html + editor
         end
