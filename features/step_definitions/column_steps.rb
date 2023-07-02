56581d6438bac96501e4e9ee99e02d322d01911a
Javier Julio <jjfutbol@gmail.com>
2023-07-02 18:00:03 -0400

    Columns component update, properties removed
    This is now just a simple 2 div component where we'll use a new columns class with CSS Grid to maintain previous behavior using plain CSS without custom properties. This component is now deprecated since it has no real value over the approach of using 2 div's directly with classes.

--- features/step_definitions/column_steps.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-Then /^I should see a columns container$/ do
-  expect(page).to have_css ".columns"
-end
-
-Then /^I should see (a|\d+) columns?$/ do |count|
-  count = count == "a" ? 1 : count.to_i
-  expect(page).to have_css ".column", count: count
-end
