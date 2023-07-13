001ff4c9fe9dfc848cd04b0a72173f9f28249483
Javier Julio <jjfutbol@gmail.com>
2023-07-13 09:25:43 -0400

    Fix bug report template with Ransack allowlist
    I missed this while working on #8009 since we only run the bug report template script in CI if the script itself is changing to optimize CI workflow which we want to maintain for now.

--- tasks/bug_report_template.rb
+++ tasks/bug_report_template.rb
@@ -65,7 +65,19 @@ class ApplicationController < ActionController::Base
   include Rails.application.routes.url_helpers
 end
 
-class User < ActiveRecord::Base
+class ApplicationRecord < ActiveRecord::Base
+  primary_abstract_class
+
+  def self.ransackable_attributes(auth_object = nil)
+    authorizable_ransackable_attributes
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    authorizable_ransackable_associations
+  end
+end
+
+class User < ApplicationRecord
 end
 
 ActiveAdmin.setup do |config|
