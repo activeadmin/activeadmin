89760526a16ba3ed4d7e74fa17f79be2012b39e9
Javier Julio <jjfutbol@gmail.com>
2023-07-20 21:17:59 -0400

    DropdownMenu updates
    This now works with Flowbite and has simpler implementation. The item method now takes a block so anything can be added e.g. horizontal divider or other control.
    
    We must have coverage through another test, but wouldn't hurt to have a simple unit test for DropdownMenu. This covers the new feature where item now takes an optional block to render any content besides the default link. We can expand this spec further.

--- lib/active_admin/views/components/dropdown_menu.rb
+++ lib/active_admin/views/components/dropdown_menu.rb
@@ -36,31 +36,32 @@ module ActiveAdmin
         super(options)
       end
 
-      def item(*args, **kwargs)
+      def item(*args, **kwargs, &block)
         within @menu do
-          li link_to(*args, **kwargs)
+          if block_given?
+            li &block
+          else
+            li link_to(*args, **kwargs)
+          end
         end
       end
 
       private
 
-      def build_button(name, button_options)
-        button_options[:class] = "#{button_options[:class]} dropdown_menu_button"
-        button_options[:href] = "#"
+      def build_button(name, options)
+        options[:class] = "#{options[:class]} dropdown_menu_button"
 
-        a name, button_options
+        button name, type: :button, "data-dropdown-toggle": dom_id, "data-dropdown-offset-distance": 3, **options
       end
 
       def build_menu(options)
         options[:class] = "#{options[:class]} dropdown_menu_list"
+        options[:id] = dom_id
+        ul(options)
+      end
 
-        menu_list = nil
-
-        div class: "dropdown_menu_list_wrapper" do
-          menu_list = ul(options)
-        end
-
-        menu_list
+      def dom_id
+        "dropdown-#{object_id}"
       end
 
     end
