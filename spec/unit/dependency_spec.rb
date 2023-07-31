fa8b00045e6149bb7b5ffa430185908ebdcb98fc
Javier Julio <jjfutbol@gmail.com>
2023-07-30 20:34:24 -0400

    Use described_class method
    Also resolves Rubocop warning with layout argument alignment.

--- spec/unit/dependency_spec.rb
+++ spec/unit/dependency_spec.rb
@@ -2,8 +2,6 @@
 require "rails_helper"
 
 RSpec.describe ActiveAdmin::Dependency do
-  k = ActiveAdmin::Dependency
-
   describe "method_missing" do
     before do
       allow(Gem).to receive(:loaded_specs)
@@ -11,65 +9,65 @@ RSpec.describe ActiveAdmin::Dependency do
     end
 
     it "returns a Matcher" do
-      expect(k.foo).to be_a ActiveAdmin::Dependency::Matcher
-      expect(k.foo.inspect).to eq "<ActiveAdmin::Dependency::Matcher for foo 1.2.3>"
-      expect(k.bar.inspect).to eq "<ActiveAdmin::Dependency::Matcher for (missing)>"
+      expect(described_class.foo).to be_a ActiveAdmin::Dependency::Matcher
+      expect(described_class.foo.inspect).to eq "<ActiveAdmin::Dependency::Matcher for foo 1.2.3>"
+      expect(described_class.bar.inspect).to eq "<ActiveAdmin::Dependency::Matcher for (missing)>"
     end
 
     describe "`?`" do
       it "base" do
-        expect(k.foo?).to eq true
-        expect(k.bar?).to eq false
+        expect(described_class.foo?).to eq true
+        expect(described_class.bar?).to eq false
       end
 
       it "=" do
-        expect(k.foo? "= 1.2.3").to eq true
-        expect(k.foo? "= 1").to eq false
+        expect(described_class.foo? "= 1.2.3").to eq true
+        expect(described_class.foo? "= 1").to eq false
       end
 
       it ">" do
-        expect(k.foo? "> 1").to eq true
-        expect(k.foo? "> 2").to eq false
+        expect(described_class.foo? "> 1").to eq true
+        expect(described_class.foo? "> 2").to eq false
       end
 
       it "<" do
-        expect(k.foo? "< 2").to eq true
-        expect(k.foo? "< 1").to eq false
+        expect(described_class.foo? "< 2").to eq true
+        expect(described_class.foo? "< 1").to eq false
       end
 
       it ">=" do
-        expect(k.foo? ">= 1.2.3").to eq true
-        expect(k.foo? ">= 1.2.2").to eq true
-        expect(k.foo? ">= 1.2.4").to eq false
+        expect(described_class.foo? ">= 1.2.3").to eq true
+        expect(described_class.foo? ">= 1.2.2").to eq true
+        expect(described_class.foo? ">= 1.2.4").to eq false
       end
 
       it "<=" do
-        expect(k.foo? "<= 1.2.3").to eq true
-        expect(k.foo? "<= 1.2.4").to eq true
-        expect(k.foo? "<= 1.2.2").to eq false
+        expect(described_class.foo? "<= 1.2.3").to eq true
+        expect(described_class.foo? "<= 1.2.4").to eq true
+        expect(described_class.foo? "<= 1.2.2").to eq false
       end
 
       it "~>" do
-        expect(k.foo? "~> 1.2.0").to eq true
-        expect(k.foo? "~> 1.1").to eq true
-        expect(k.foo? "~> 1.2.4").to eq false
+        expect(described_class.foo? "~> 1.2.0").to eq true
+        expect(described_class.foo? "~> 1.1").to eq true
+        expect(described_class.foo? "~> 1.2.4").to eq false
       end
     end
 
     describe "`!`" do
       it "raises an error if requirement not met" do
-        expect { k.foo! "5" }.to raise_error ActiveAdmin::DependencyError,
-                                             "You provided foo 1.2.3 but we need: 5."
+        expect { described_class.foo! "5" }
+          .to raise_error(ActiveAdmin::DependencyError, "You provided foo 1.2.3 but we need: 5.")
       end
 
       it "accepts multiple arguments" do
-        expect { k.foo! "> 1", "< 1.2" }.to raise_error ActiveAdmin::DependencyError,
-                                                        "You provided foo 1.2.3 but we need: > 1, < 1.2."
+        expect { described_class.foo! "> 1", "< 1.2" }
+          .to raise_error(ActiveAdmin::DependencyError, "You provided foo 1.2.3 but we need: > 1, < 1.2.")
       end
 
       it "raises an error if not provided" do
-        expect { k.bar! }.to raise_error ActiveAdmin::DependencyError,
-                                         "To use bar you need to specify it in your Gemfile."
+        expect { described_class.bar! }
+          .to raise_error(ActiveAdmin::DependencyError, "To use bar you need to specify it in your Gemfile.")
       end
     end
   end
@@ -81,64 +79,64 @@ RSpec.describe ActiveAdmin::Dependency do
     end
 
     it "allows access to gems with an arbitrary name" do
-      expect(k["a-b"]).to be_a ActiveAdmin::Dependency::Matcher
-      expect(k["a-b"].inspect).to eq "<ActiveAdmin::Dependency::Matcher for a-b 1.2.3>"
-      expect(k["c-d"].inspect).to eq "<ActiveAdmin::Dependency::Matcher for (missing)>"
+      expect(described_class["a-b"]).to be_a ActiveAdmin::Dependency::Matcher
+      expect(described_class["a-b"].inspect).to eq "<ActiveAdmin::Dependency::Matcher for a-b 1.2.3>"
+      expect(described_class["c-d"].inspect).to eq "<ActiveAdmin::Dependency::Matcher for (missing)>"
     end
 
     # Note: more extensive tests for match? and match! are above.
 
     it "match?" do
-      expect(k["a-b"].match?).to eq true
-      expect(k["a-b"].match? "1.2.3").to eq true
-      expect(k["b-c"].match?).to eq false
+      expect(described_class["a-b"].match?).to eq true
+      expect(described_class["a-b"].match? "1.2.3").to eq true
+      expect(described_class["b-c"].match?).to eq false
     end
 
     it "match!" do
-      expect(k["a-b"].match!).to eq nil
-      expect(k["a-b"].match! "1.2.3").to eq nil
+      expect(described_class["a-b"].match!).to eq nil
+      expect(described_class["a-b"].match! "1.2.3").to eq nil
 
-      expect { k["a-b"].match! "2.5" }.to raise_error ActiveAdmin::DependencyError,
-                                                      "You provided a-b 1.2.3 but we need: 2.5."
+      expect { described_class["a-b"].match! "2.5" }
+        .to raise_error(ActiveAdmin::DependencyError, "You provided a-b 1.2.3 but we need: 2.5.")
 
-      expect { k["b-c"].match! }.to raise_error ActiveAdmin::DependencyError,
-                                                "To use b-c you need to specify it in your Gemfile."
+      expect { described_class["b-c"].match! }
+        .to raise_error(ActiveAdmin::DependencyError, "To use b-c you need to specify it in your Gemfile.")
     end
 
     # Note: Ruby comparison operators are separate from the `foo? '> 1'` syntax
 
     describe "Ruby comparison syntax" do
       it "==" do
-        expect(k["a-b"] == "1.2.3").to eq true
-        expect(k["a-b"] == "1.2").to eq false
-        expect(k["a-b"] == 1).to eq false
+        expect(described_class["a-b"] == "1.2.3").to eq true
+        expect(described_class["a-b"] == "1.2").to eq false
+        expect(described_class["a-b"] == 1).to eq false
       end
 
       it ">" do
-        expect(k["a-b"] > 1).to eq true
-        expect(k["a-b"] > 2).to eq false
+        expect(described_class["a-b"] > 1).to eq true
+        expect(described_class["a-b"] > 2).to eq false
       end
 
       it "<" do
-        expect(k["a-b"] < 2).to eq true
-        expect(k["a-b"] < 1).to eq false
+        expect(described_class["a-b"] < 2).to eq true
+        expect(described_class["a-b"] < 1).to eq false
       end
 
       it ">=" do
-        expect(k["a-b"] >= "1.2.3").to eq true
-        expect(k["a-b"] >= "1.2.2").to eq true
-        expect(k["a-b"] >= "1.2.4").to eq false
+        expect(described_class["a-b"] >= "1.2.3").to eq true
+        expect(described_class["a-b"] >= "1.2.2").to eq true
+        expect(described_class["a-b"] >= "1.2.4").to eq false
       end
 
       it "<=" do
-        expect(k["a-b"] <= "1.2.3").to eq true
-        expect(k["a-b"] <= "1.2.4").to eq true
-        expect(k["a-b"] <= "1.2.2").to eq false
+        expect(described_class["a-b"] <= "1.2.3").to eq true
+        expect(described_class["a-b"] <= "1.2.4").to eq true
+        expect(described_class["a-b"] <= "1.2.2").to eq false
       end
 
       it "throws a custom error if the gem is missing" do
-        expect { k["b-c"] < 23 }.to raise_error ActiveAdmin::DependencyError,
-                                                "To use b-c you need to specify it in your Gemfile."
+        expect { described_class["b-c"] < 23 }
+          .to raise_error(ActiveAdmin::DependencyError, "To use b-c you need to specify it in your Gemfile.")
       end
     end
   end
