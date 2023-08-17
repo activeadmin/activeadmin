e8ed4d7de855c818ef1056b3d690e1ef780d2f8b
Javier Julio <jjfutbol@gmail.com>
2023-08-17 18:00:09 -0400

    Enable dark mode detection script
    We need this for easier testing

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -122,4 +122,10 @@
     window.location.search = params;
   };
   Rails.delegate(document, ".pagination_per_page > select", "change", setPerPage);
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (localStorage.getItem("color-theme") === "dark" || !("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
 }));
