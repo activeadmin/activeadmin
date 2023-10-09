760db09f603f329ff36105d80170b7c8ba5590f0
Javier Julio <JJfutbol@gmail.com>
2023-10-09 17:23:42 -0400

    Remove upper bound dependency limits from gemspec (#8098)
    Remove upper bound limits in gemspec
    
    Other than the gems we own, remove the upper bound limits to ease maintenance. In some cases these can update (e.g. support new Rails version) without affecting ActiveAdmin, Arbre or InheritedResources so we want to allow that to reduce maintenance effort and to also allow users to try upgrading and submit fixes if needed.

--- spec/tasks/gemspec_spec.rb
+++ spec/tasks/gemspec_spec.rb
@@ -11,10 +11,6 @@ RSpec.describe "gemspec sanity" do
     Open3.capture3("gem build activeadmin.gemspec")
   end
 
-  it "has no warnings" do
-    expect(build[1]).not_to include("WARNING")
-  end
-
   it "succeeds" do
     expect(build[2]).to be_success
   end
