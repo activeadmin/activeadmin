6076f47b8aa30aaf3f38d36a8f09d5e085591d90
Javier Julio <jjfutbol@gmail.com>
2023-10-29 16:45:46 -0400

    Remove unused toggle-bg class styles
    This is something Flowbite declared but even before v2 wasn't using in their docs.

--- plugin.js
+++ plugin.js
@@ -234,33 +234,6 @@ module.exports = plugin(
           background: theme('colors.gray.500', colors.gray[500]),
         },
       },
-      // remove from v2.x+
-      ['.toggle-bg:after']: {
-        content: '""',
-        position: 'absolute',
-        top: spacing[0.5],
-        left: spacing[0.5],
-        background: 'white',
-        'border-color': theme('colors.gray.300', colors.gray[300]),
-        'border-width': borderWidth['DEFAULT'],
-        'border-radius': borderRadius.full,
-        height: theme('height.5'),
-        width: theme('width.5'),
-        'transition-property':
-          'background-color,border-color,color,fill,stroke,opacity,box-shadow,transform,filter,backdrop-filter,-webkit-backdrop-filter',
-        'transition-duration': '.15s',
-        'box-shadow':
-          'var(--tw-ring-inset) 0 0 0 calc(0px + var(--tw-ring-offset-width)) var(--tw-ring-color)',
-      },
-      ['input:checked + .toggle-bg:after']: {
-        transform: 'translateX(100%);',
-        'border-color': 'white',
-      },
-      ['input:checked + .toggle-bg']: {
-        background: theme('colors.blue.600', colors.gray[600]),
-        'border-color': theme('colors.blue.600', colors.gray[600]),
-      },
-      // remove from v2.x+ END
       [['.tooltip-arrow', '.tooltip-arrow:before']]: {
         position: 'absolute',
         width: '8px',
