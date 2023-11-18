bd0be7ca3103fa02478f3bc4d4a74a42d63df323
Javier Julio <jjfutbol@gmail.com>
2023-11-18 18:20:49 -0500

    Update filter form label style
    Use the label class here so when using check boxes for a filter, this only applies to the filter label (group), not the label for each checkbox.

--- plugin.js
+++ plugin.js
@@ -470,7 +470,7 @@ module.exports = plugin(
       // '.delete_link': {
       //   '@apply focus:outline-none text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-md text-sm px-3 py-1.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 no-underline': {}
       // },
-      '.filter_form :where(label)': {
+      '.filter_form :where(.label)': {
         '@apply block mb-1.5 text-sm': {}
       },
       '.filter-input-group': {
