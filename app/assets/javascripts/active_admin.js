5b584fdac626a871b5a58eb78d20e160773f2858
Javier Julio <jjfutbol@gmail.com>
2023-12-13 19:27:58 -0500

    Add dark mode toggle
    Defaults to system preference

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -82,6 +82,41 @@
     }
   };
   Rails.delegate(document, ".paginated-collection tbody td", "click", tableRowClick);
+  const THEME_KEY = "color-scheme";
+  const darkModeMedia = window.matchMedia("(prefers-color-scheme: dark)");
+  const setTheme = () => {
+    const darkIcon = document.getElementById("theme-toggle-dark-icon");
+    const lightIcon = document.getElementById("theme-toggle-light-icon");
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.getItem(THEME_KEY) === "dark" || !(THEME_KEY in localStorage) && darkModeMedia.matches) {
+      document.documentElement.classList.add("dark");
+      lightIcon.classList.add("hidden");
+      darkIcon.classList.remove("hidden");
+    } else {
+      document.documentElement.classList.remove("dark");
+      darkIcon.classList.add("hidden");
+      lightIcon.classList.remove("hidden");
+    }
+  };
+  // Detect when user changes their system level preference to set theme.
+    darkModeMedia.addEventListener("change", setTheme);
+  // When the page loads, set theme. By default, uses the system preference.
+    document.addEventListener("DOMContentLoaded", setTheme);
+  // If user deletes the Local Storage key, then re-apply theme.
+    window.addEventListener("storage", (event => {
+    if (event.key === THEME_KEY) {
+      setTheme();
+    }
+  }));
+  const toggleDarkMode = () => {
+    if (localStorage.getItem(THEME_KEY) === "light") {
+      localStorage.setItem(THEME_KEY, "dark");
+    } else {
+      localStorage.setItem(THEME_KEY, "light");
+    }
+    setTheme();
+  };
+  Rails.delegate(document, ".dark-mode-toggle", "click", toggleDarkMode);
   const hasManyRemoveClick = function(event) {
     event.preventDefault();
     const oldGroup = this.closest("fieldset");
@@ -145,10 +180,4 @@
     window.location.search = params;
   };
   Rails.delegate(document, ".pagination-per-page", "change", setPerPage);
-  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (localStorage.getItem("color-theme") === "dark" || !("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
 }));
