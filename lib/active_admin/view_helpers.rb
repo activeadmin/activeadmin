4f5efd0f9ca6e1fbedfb0d68dc808ae892ff6d75
Javier Julio <jjfutbol@gmail.com>
2023-12-14 12:43:51 -0500

    Add layout helper from base page w/titles
    Should be able to handle the site, page and combined titles for both layout files.
    
    Consolidate title helper into layout helper.

--- lib/active_admin/view_helpers.rb
+++ lib/active_admin/view_helpers.rb
@@ -12,10 +12,10 @@ module ActiveAdmin
     include MethodOrProcHelper
     include SidebarHelper
     include FormHelper
-    include TitleHelper
     include ViewFactoryHelper
     include FlashHelper
     include ScopeNameHelper
+    include LayoutHelper
 
   end
 end
