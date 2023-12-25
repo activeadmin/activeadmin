0a05cf1cc42d1577289ff8ff6d342557f7207ccc
Javier Julio <jjfutbol@gmail.com>
2023-12-25 00:17:30 -0500

    Inline body styles

--- plugin.js
+++ plugin.js
@@ -304,9 +304,6 @@ module.exports = plugin(
       ['[role="tooltip"].invisible > [data-popper-arrow]:before']: {
         visibility: 'hidden',
       },
-      'body': {
-        '@apply bg-white dark:bg-gray-950 dark:text-white antialiased': {}
-      },
       '[type=checkbox]': {
         '@apply w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600': {}
       },
