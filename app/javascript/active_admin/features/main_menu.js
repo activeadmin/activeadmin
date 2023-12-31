682d83b4f262624e97d9484e975feae16faeaee6
Javier Julio <jjfutbol@gmail.com>
2023-12-30 22:14:04 -0500

    Simplify main menu JS and apply RTL support
    Originally, tried this but had some trouble. When applying RTL support for the nested menu icon, I looked up using group with data attribute and found this: https://github.com/tailwindlabs/tailwindcss/discussions/11768#discussioncomment-6663339 With this applied the group class approach was working. This is ideal since now our JS is just toggling a simple data attribute and has no classes to set/remove. This also works with the RTL modifier. No need for additional Ruby here to set the class if its current, since we can just base it off the data attribute now.

--- app/javascript/active_admin/features/main_menu.js
+++ app/javascript/active_admin/features/main_menu.js
@@ -1,17 +1,11 @@
 import Rails from '@rails/ujs';
-import { nextSibling } from 'active_admin/utils/dom'
 
 const toggleMenu = function(event) {
   const parent = this.parentNode
-  const menu = nextSibling(this, "[data-menu-list]")
   if (!("open" in parent.dataset)) {
     parent.dataset.open = ""
-    menu.classList.remove("hidden")
-    this.querySelector("[data-menu-icon]").classList.add("rotate-90")
   } else {
     delete parent.dataset.open
-    menu.classList.add("hidden")
-    this.querySelector("[data-menu-icon]").classList.remove("rotate-90")
   }
 }
 
