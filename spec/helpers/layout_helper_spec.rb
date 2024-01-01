7aa0a90ecb8e1625de826bc4ed08a2d59b1484a6
Javier Julio <jjfutbol@gmail.com>
2024-01-01 16:40:14 -0500

    Add tests for other helper methods
    These are already covered by feature specs but helps to have some simple unit tests in place.

--- spec/helpers/layout_helper_spec.rb
+++ spec/helpers/layout_helper_spec.rb
@@ -2,6 +2,39 @@
 require "rails_helper"
 
 RSpec.describe ActiveAdmin::LayoutHelper, type: :helper do
+  describe "active_admin_application" do
+    it "returns the application instance" do
+      expect(helper.active_admin_application).to eq ActiveAdmin.application
+    end
+  end
+
+  describe "set_page_title" do
+    it "sets the @page_title variable" do
+      helper.set_page_title("Sample Page")
+      expect(helper.instance_variable_get(:@page_title)).to eq "Sample Page"
+    end
+  end
+
+  describe "html_head_site_title" do
+    before do
+      expect(helper).to receive(:site_title).and_return("MyAdmin")
+      allow(helper).to receive(:page_title).and_return("Users")
+    end
+
+    it "returns title in default format" do
+      expect(helper.html_head_site_title).to eq "Users - MyAdmin"
+    end
+
+    it "returns title with custom separator" do
+      expect(helper.html_head_site_title(separator: "|")).to eq "Users | MyAdmin"
+    end
+
+    it "returns title with @page_title override" do
+      helper.set_page_title("Posts")
+      expect(helper.html_head_site_title).to eq "Posts - MyAdmin"
+    end
+  end
+
   describe "skip_sidebar?" do
     it "should return true if skipped" do
       helper.skip_sidebar!
