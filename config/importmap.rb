3f29ec3f184fcda8d45c4c9e930df35fdd9d976b
Javier Julio <JJfutbol@gmail.com>
2024-01-02 14:12:14 -0500

    Use defaults from importmap-rails v2 (#8204)
    * Preload all pins
    
    https://github.com/rails/importmap-rails/pull/218
    
    * Disable shim for importmaps
    
    Prep for importmap-rails v2 by using the new defaults now, even on v1.
    
    https://github.com/rails/importmap-rails/pull/216

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-pin "flowbite" # downloaded from https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javascripts/rails-ujs.esm.js"
+pin "flowbite", preload: true # downloaded from https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javascripts/rails-ujs.esm.js", preload: true
 pin "active_admin", to: "active_admin.js", preload: true
-pin_all_from File.expand_path("../app/javascript/active_admin", __dir__), under: "active_admin"
+pin_all_from File.expand_path("../app/javascript/active_admin", __dir__), under: "active_admin", preload: true
