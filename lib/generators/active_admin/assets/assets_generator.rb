35922ec0b10efe63fbb4700694fdf306333cb3dc
Javier Julio <jjfutbol@gmail.com>
2024-01-05 12:02:10 -0500

    Update assets gen to remove old SCSS/JS files

--- lib/generators/active_admin/assets/assets_generator.rb
+++ lib/generators/active_admin/assets/assets_generator.rb
@@ -5,6 +5,8 @@ module ActiveAdmin
       source_root File.expand_path("templates", __dir__)
 
       def install_assets
+        remove_file "app/assets/stylesheets/active_admin.scss"
+        remove_file "app/assets/javascripts/active_admin.js"
         template "active_admin.css", "app/assets/stylesheets/active_admin.css"
         template "tailwind.config.js", "tailwind-active_admin.config.js"
       end
