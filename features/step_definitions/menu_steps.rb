97efd31b9d978945dd1d1d71a8cad447b4a8ff36
Hamed Asghari <hasghari@gmail.com>
2024-01-09 10:14:18 -0700

    Add missing link to parent menu item
    Currently when rendering a nested menu, if the parent menu item
    links to a url, the link is not rendered for the parent item
    and there is no way to navigate to it.

--- features/step_definitions/menu_steps.rb
+++ features/step_definitions/menu_steps.rb
@@ -12,7 +12,7 @@ Then(/^the "([^"]*)" menu item should be hidden$/) do |name|
 end
 
 Then(/^I should see a menu parent for "([^"]*)"$/) do |name|
-  expect(page).to have_css "#main-menu li button", text: name
+  expect(page).to have_css "#main-menu li a", text: name
 end
 
 Then(/^I should see a nested menu item for "([^"]*)"$/) do |name|
