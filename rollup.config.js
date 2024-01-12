390927e127d78f0ef1c107c46f39cf8283d0fbad
Matias Grunberg <matias@yellowspot.dev>
2024-01-12 18:35:17 -0300

    format & lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { URL, fileURLToPath } from 'node:url';
 import { readFileSync } from 'node:fs';
 import alias from '@rollup/plugin-alias';
 
 const packageJson = JSON.parse(
-	readFileSync(new URL('./package.json', import.meta.url))
+  readFileSync(new URL('./package.json', import.meta.url))
 );
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
@@ -16,25 +16,25 @@ const assetsDir = path.resolve(projectRootDir, 'app/javascript');
  * @type {import('rollup').RollupOptions}
  */
 export default [
-	// build dist folder with all files from app/javascript using relative imports.
-	// let bundler tools like webpack or rollup to process our package
-	{
-		input: ['app/javascript/active_admin.js'],
-		output: {
-			format: 'es',
-			dir: 'dist',
-			preserveModules: true,
-		},
-		external: Object.keys(packageJson.dependencies),
-		plugins: [
-			alias({
-				entries: [
-					{
-						find: 'active_admin',
-						replacement: path.join(assetsDir, 'active_admin'),
-					},
-				]
-			})
-		]
-	}
+  // build dist folder with all files from app/javascript using relative imports.
+  // let bundler tools like webpack or rollup to process our package
+  {
+    input: ['app/javascript/active_admin.js'],
+    output: {
+      format: 'es',
+      dir: 'dist',
+      preserveModules: true,
+    },
+    external: Object.keys(packageJson.dependencies),
+    plugins: [
+      alias({
+        entries: [
+          {
+            find: 'active_admin',
+            replacement: path.join(assetsDir, 'active_admin'),
+          },
+        ]
+      })
+    ]
+  }
 ];
