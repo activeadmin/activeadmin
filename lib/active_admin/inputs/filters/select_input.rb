be0ec7e1311871b2ca17ba04f216b0e8d9171ec9
Yvan BARTHÉLEMY <223016+ybart@users.noreply.github.com>
2025-01-03 19:27:47 +0100

    Update lib/active_admin/inputs/filters/select_input.rb

--- lib/active_admin/inputs/filters/select_input.rb
+++ lib/active_admin/inputs/filters/select_input.rb
@@ -15,7 +15,7 @@ module ActiveAdmin
           if searchable_has_many_through?
             "#{reflection.through_reflection.name}_#{reflection.foreign_key}"
           else
-            reflection_searchable? ? "#{name}_#{reflection.association_primary_key}" : name
+            reflection_searchable? ? "#{method}_#{reflection.association_primary_key}" : method
           end
         end
 
