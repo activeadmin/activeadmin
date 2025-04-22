a3eaefb4d5b542a4f4c0c38f076c404d2325bf1a
Geremia Taglialatela <tagliala@users.noreply.github.com>
2025-04-22 10:16:52 +0200

    Update dependencies (#8686)
    Additionally, migrate RuboCop configuration to plugins and enable
    new cops, fixing a `Capybara/FindAllFirst` offense

--- features/step_definitions/attribute_steps.rb
+++ features/step_definitions/attribute_steps.rb
@@ -10,7 +10,7 @@ Then(/^I should( not)? see the attribute "([^"]*)" with "([^"]*)"$/) do |negate,
 end
 
 Then(/^I should see the attribute "([^"]*)" with a nicely formatted datetime$/) do |title|
-  text = all(".attributes-table th:contains('#{title}') ~ td").first.text
+  text = first(".attributes-table th:contains('#{title}') ~ td").text
   expect(text).to match(/\w+ \d{1,2}, \d{4} \d{2}:\d{2}/)
 end
 
