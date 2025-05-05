cf1d070ff57d8c0080171f1c8b1d4218cefa65e7
Geremia Taglialatela <tagliala.dev@gmail.com>
2025-05-05 14:31:38 +0200

    Add missing border colors for v4
    https://tailwindcss.com/docs/upgrade-guide#default-border-color

--- plugin.js
+++ plugin.js
@@ -241,7 +241,7 @@ export default plugin(
         '@apply w-full text-sm text-gray-800 dark:text-gray-300': {}
       },
       '.data-table :where(thead > tr > th)': {
-        '@apply px-3 py-3.5 whitespace-nowrap font-semibold text-start text-xs uppercase border-b text-gray-700 bg-gray-50 dark:bg-gray-950/50 dark:border-gray-800 dark:text-white': {}
+        '@apply px-3 py-3.5 whitespace-nowrap font-semibold text-start text-xs uppercase border-b border-gray-200 text-gray-700 bg-gray-50 dark:bg-gray-950/50 dark:border-gray-800 dark:text-white': {}
       },
       '.data-table :where(thead > tr > th > a)': {
         '@apply text-inherit no-underline inline-flex items-center gap-2': {}
@@ -256,7 +256,7 @@ export default plugin(
         '@apply rotate-180': {}
       },
       '.data-table :where(tbody > tr)': {
-        '@apply border-b dark:border-gray-800 last:border-b-0': {}
+        '@apply border-b border-gray-200 dark:border-gray-800 last:border-b-0': {}
       },
       '.data-table :where(td)': {
         '@apply px-3 py-4': {}
@@ -328,7 +328,7 @@ export default plugin(
         '@apply w-full text-sm text-gray-800 dark:text-gray-300': {}
       },
       '.attributes-table :where(tbody > tr)': {
-        '@apply border-b dark:border-gray-800 last:border-b-0 align-baseline': {}
+        '@apply border-b border-gray-200 dark:border-gray-800 last:border-b-0 align-baseline': {}
       },
       '.attributes-table :where(tbody > tr > th)': {
         '@apply w-32 sm:w-40 text-start text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-950/50 dark:text-gray-300': {}
@@ -350,7 +350,7 @@ export default plugin(
         '@apply text-sm': {}
       },
       '.formtastic :where(.fieldset-title, .has-many-fields-title)': {
-        '@apply block w-full mb-3 border-b font-bold text-lg': {}
+        '@apply block w-full mb-3 border-b border-gray-200 dark:border-gray-800 font-bold text-lg': {}
       },
       '.formtastic :where(.label)': {
         '@apply block mb-1.5': {}
