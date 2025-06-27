6218cfb271d8c2f900d83a718569c485f5253936
harunkumars <harunkumar.s@gmail.com>
2025-06-27 10:18:16 +0530

    Fix use of deprecated enum syntax

--- spec/support/templates/models/store.rb
+++ spec/support/templates/models/store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Store < ApplicationRecord
-  enum review_status: [:pending, :approved, :rejected]
-  enum reject_reason: {
+  enum :review_status, [:pending, :approved, :rejected]
+  enum :reject_reason, {
     not_a_good_fit: 0,
     other: 10
   }
